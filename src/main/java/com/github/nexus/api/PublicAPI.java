d7d3fd331db461b19b6ea0567acb85960c70be3b
SatpalSandhu61 <quorum@satpal.co.uk>
2018-05-21 16:21:25 +0100

    Creating initial REST API framework

---
+++ src/main/java/com/github/nexus/api/PublicAPI.java
@@ -0,0 +1,26 @@
+package com.github.nexus.api;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Public API entry points
+ */
+@Path("/")
+public class PublicAPI {
+
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent
+     * to the client as "text/plain" media type.
+     *
+     * @return String that will be returned as a text/plain response.
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("version")
+    public String getIt() {
+        return "No version defined yet!";
+    }
+}
