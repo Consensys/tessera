c14ecc6141f65c4db7fdf8676b9cfc3bb697feb4
Mark Lowe <melowe@gmail.com>
2018-05-22 05:04:54 +0100

    Add minimal logging config and catch all exception mapper

---
+++ nexus-app/src/main/java/com/github/nexus/app/DefaultExceptionMapper.java
@@ -0,0 +1,26 @@
+
+package com.github.nexus.app;
+
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class DefaultExceptionMapper implements ExceptionMapper<Exception> {
+    
+    private static final Logger LOGGER = Logger.getLogger(DefaultExceptionMapper.class.getName());
+    
+    @Override
+    public Response toResponse(Exception exception) {
+        
+        LOGGER.log(Level.SEVERE, "",exception);
+        
+        return Response.status(Status.INTERNAL_SERVER_ERROR)
+                .entity(exception.getMessage())
+                .build();
+    } 
+}
