b587e7ed4277c7e0a9f66d776f0f249a5486dd42
namtruong <nam.p.truong@outlook.com>
2018-05-25 12:03:22 +0100

    add some relevant objects for api

---
+++ nexus-app/src/main/java/com/github/nexus/api/model/ReceiveResponse.java
@@ -0,0 +1,14 @@
+package com.github.nexus.api.model;
+
+public class ReceiveResponse {
+
+    private String payload;
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+}
---
+++ nexus-app/src/main/java/com/github/nexus/api/model/SendRequest.java
@@ -0,0 +1,37 @@
+package com.github.nexus.api.model;
+
+import javax.validation.constraints.NotNull;
+
+public class SendRequest{
+
+    @NotNull
+    private String payload;
+    @NotNull
+    private String from;
+    @NotNull
+    private String[] to;
+
+    public String getPayload() {
+        return payload;
+    }
+
+    public void setPayload(String payload) {
+        this.payload = payload;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String[] getTo() {
+        return to;
+    }
+
+    public void setTo(String[] to) {
+        this.to = to;
+    }
+}
