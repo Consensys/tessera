85a4936870535b1e6a2efcad5a4a522c532f6dfc
namtruong <nam.p.truong@outlook.com>
2018-06-05 11:19:36 +0100

    model update

--- nexus-app/src/main/java/com/github/nexus/api/model/ReceiveResponse.java
+++ nexus-app/src/main/java/com/github/nexus/api/model/ReceiveResponse.java
@@ -4,6 +4,13 @@ public class ReceiveResponse {
 
     private String payload;
 
+    public ReceiveResponse(String payload) {
+        this.payload = payload;
+    }
+
+    public ReceiveResponse() {
+    }
+
     public String getPayload() {
         return payload;
     }
