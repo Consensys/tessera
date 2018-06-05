9ed00aadbd24625515f77e69d9b77bdd6d8864b0
Peter Fox <peter.rd.fox@gmail.com>
2018-06-05 14:24:45 +0100

    Add a delete method to the DAO that will delete by the unique hash

--- nexus-app/src/main/java/com/github/nexus/dao/EncryptedTransactionDAO.java
+++ nexus-app/src/main/java/com/github/nexus/dao/EncryptedTransactionDAO.java
@@ -1,13 +1,34 @@
 package com.github.nexus.dao;
 
+import com.github.nexus.enclave.model.MessageHash;
 import com.github.nexus.entity.EncryptedTransaction;
 
 import java.util.List;
 
 public interface EncryptedTransactionDAO {
 
+    /**
+     * Save a new Encrypted Transaction
+     * All fields are required to be non-null on the entity
+     *
+     * @param entity The entity to be persisted
+     * @return The entity that was persisted
+     */
     EncryptedTransaction save(EncryptedTransaction entity);
 
+    /**
+     * Retrieves a list of all transactions stored in the database
+     *
+     * @return The list of all rows in the database
+     */
     List<EncryptedTransaction> retrieveAllTransactions();
+
+    /**
+     * Deletes a transaction that has the given hash as its digest
+     *
+     * @param hash The hash of the message to be deleted
+     * @return whether an entity was deleted
+     */
+    boolean delete(MessageHash hash);
 
 }
