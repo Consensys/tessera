b828bb3c7c039f988fbc3ca96f7b7a629acca04c
namtruong <nam.p.truong@outlook.com>
2018-06-05 11:16:12 +0100

    exception classes

---
+++ nexus-app/src/main/java/com/github/nexus/api/exception/DecodingException.java
@@ -0,0 +1,16 @@
+package com.github.nexus.api.exception;
+
+public class DecodingException extends Exception {
+
+    public DecodingException(String message) {
+        super(message);
+    }
+
+    public DecodingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DecodingException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ nexus-app/src/main/java/com/github/nexus/api/exception/DecodingExceptionMapper.java
@@ -0,0 +1,25 @@
+package com.github.nexus.api.exception;
+
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Provider
+public class DecodingExceptionMapper implements ExceptionMapper<DecodingException> {
+
+    private static final Logger LOGGER = Logger.getLogger(DecodingExceptionMapper.class.getName());
+
+    @Override
+    public Response toResponse(DecodingException e) {
+        LOGGER.log(Level.SEVERE, "",e);
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(e.getMessage())
+                .header("Content-Type", MediaType.TEXT_PLAIN)
+                .build();
+    }
+}
