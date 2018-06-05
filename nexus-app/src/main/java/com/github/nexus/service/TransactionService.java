bf64818d1868e9ddc8d58c3c820979e7a6a05c13
namtruong <nam.p.truong@outlook.com>
2018-06-05 11:50:15 +0100

    update service

--- nexus-app/src/main/java/com/github/nexus/service/TransactionService.java
+++ nexus-app/src/main/java/com/github/nexus/service/TransactionService.java
@@ -3,25 +3,21 @@ package com.github.nexus.service;
 public interface TransactionService {
 
     /**
-     *
-     * @param from
-     * @param recipients
-     * @param payload
-     * @return
+     * Sends a new transaction to the enclave for storage and propagation to the provided list of recipients
+     * @param from sender node identification.
+     * @param recipients a list of the recipient nodes.
+     * @param payload transaction payload data we wish to store.
+     * @return a hash key. This key can be used to retrieve the submitted transaction
      */
     byte[] send(byte[] from, byte[][] recipients, byte[] payload);
 
 
     /**
-     *
-     * @param key
+     * Retrieve a particular transaction
+     * @param key hash key used to retrieve transaction
      * @param to
-     * @return
+     * @return the raw payload associated with the request.
      */
     byte[] receive(byte[] key, byte[] to);
-
-    void delete(byte[] key);
-
-    byte[] push(byte[] payload);
 
 }
