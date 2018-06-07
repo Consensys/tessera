be54de9deb476476f7dcc8129f1de88bef2528ef
Mark <melowe@me.com>
2018-06-07 17:05:35 +0100

    Change logger to slf4j and add assertions and verifications in test to reflect changes in resources.

--- nexus-app/src/test/java/com/github/nexus/api/PartyInfoResourceTest.java
+++ nexus-app/src/test/java/com/github/nexus/api/PartyInfoResourceTest.java
@@ -1,5 +1,7 @@
 package com.github.nexus.api;
 
+import com.github.nexus.node.PartyInfo;
+import com.github.nexus.node.PartyInfoParser;
 import com.github.nexus.node.PartyInfoService;
 import org.junit.After;
 import org.junit.Before;
@@ -8,11 +10,12 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import javax.ws.rs.core.Response;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.*;
 
 public class PartyInfoResourceTest {
 
@@ -21,24 +24,44 @@ public class PartyInfoResourceTest {
 
     private PartyInfoResource partyInfoResource;
 
+    @Mock
+    private PartyInfoParser partyInfoParser;
+
     @Before
     public void onSetup() {
         MockitoAnnotations.initMocks(this);
-        partyInfoResource = new PartyInfoResource(partyInfoService);
+        partyInfoResource = new PartyInfoResource(partyInfoService, partyInfoParser);
     }
 
     @After
     public void onTearDown() {
-        verifyNoMoreInteractions(partyInfoService);
+        verifyNoMoreInteractions(partyInfoService,partyInfoParser);
     }
 
     @Test
     public void testPartyInfo() throws IOException {
-
-        Response response = partyInfoResource.partyInfo(new ByteArrayInputStream("{}".getBytes()));
+        
+        byte[] data = "{}".getBytes();
+        
+        PartyInfo partyInfo = mock(PartyInfo.class);
+        when(partyInfoParser.from(data)).thenReturn(partyInfo);
+        when(partyInfoService.updatePartyInfo(partyInfo)).thenReturn(partyInfo);
+        
+        byte[] resultData = "I LOVE SPARROWS!!".getBytes();
+        
+        when(partyInfoParser.to(partyInfo)).thenReturn(resultData);
+        
+        Response response = partyInfoResource.partyInfo(data);
 
         assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(201);
+        assertThat(response.getEntity())
+                .isEqualTo(Entity.entity(resultData, MediaType.APPLICATION_OCTET_STREAM));
+        
+        verify(partyInfoParser).from(data);
+        verify(partyInfoService).updatePartyInfo(partyInfo);
+        verify(partyInfoParser).to(partyInfo);
+
     }
 
 }
