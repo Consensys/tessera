34b2cc946a2fbf8874c70c654799bdb524fa2b94
Mark <melowe@me.com>
2018-06-08 16:24:32 +0100

    Fixed coverage.. also ensure that interactions with mocks are correct.
    Requires more tests.

--- nexus-app/src/test/java/com/github/nexus/enclave/EnclaveImplTest.java
+++ nexus-app/src/test/java/com/github/nexus/enclave/EnclaveImplTest.java
@@ -3,6 +3,7 @@ package com.github.nexus.enclave;
 import com.github.nexus.enclave.model.MessageHash;
 import com.github.nexus.transaction.PayloadEncoder;
 import com.github.nexus.transaction.TransactionService;
+import com.github.nexus.transaction.model.EncodedPayloadWithRecipients;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,24 +17,25 @@ public class EnclaveImplTest {
 
     @Mock
     private TransactionService txService;
+
     @Mock
     private PayloadEncoder encoder;
 
-    private Enclave enclave;
+    private EnclaveImpl enclave;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         MockitoAnnotations.initMocks(this);
-        enclave = new EnclaveImpl(txService,encoder);
+        enclave = new EnclaveImpl(txService, encoder);
     }
 
     @After
-    public void tearDown(){
-
+    public void tearDown() {
+        verifyNoMoreInteractions(txService, encoder);
     }
 
     @Test
-    public void testDelete(){
+    public void testDelete() {
         doReturn(true).when(txService).delete(any(MessageHash.class));
 
         enclave.delete(new MessageHash(new byte[0]));
@@ -42,18 +44,43 @@ public class EnclaveImplTest {
     }
 
     @Test
-    public void testSend(){
+    public void testSend() {
         enclave.send(new byte[0], new byte[0][0], new byte[0]);
     }
 
     @Test
-    public void testReceive(){
+    public void testReceive() {
         enclave.receive(new byte[0], new byte[0]);
+        verify(txService).retrieveUnencryptedTransaction(any(), any());
     }
 
     @Test
-    public void testStore(){
+    public void testStore() {
+
+        EncodedPayloadWithRecipients payload = mock(EncodedPayloadWithRecipients.class);
+        when(txService.encryptPayload(any(), any(), any())).thenReturn(payload);
+
         enclave.store(new byte[0], new byte[0][0], new byte[0]);
+        
+        verify(txService).encryptPayload(any(), any(), any());
+        
+        verify(txService).storeEncodedPayload(payload);
+    }
+    
+        @Test
+    public void testStoreWithRecipientStuyff() {
+
+        EncodedPayloadWithRecipients payload = mock(EncodedPayloadWithRecipients.class);
+        when(txService.encryptPayload(any(), any(), any())).thenReturn(payload);
+
+        byte[][] recipients = new byte[1][1];
+        recipients[0]  = new byte[] {'P'};
+        
+        enclave.store(new byte[0],recipients, new byte[0]);
+        
+        verify(txService).encryptPayload(any(), any(), any());
+        
+        verify(txService).storeEncodedPayload(payload);
     }
 
 }
