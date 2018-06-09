ee318466261390759b3153470fcfeab7e7a101a8
Mark Lowe <melowe@gmail.com>
2018-06-09 12:52:47 +0100

    Add tests for coverage

--- nexus-encryption/src/test/java/com/github/nexus/nacl/KeyTest.java
+++ nexus-encryption/src/test/java/com/github/nexus/nacl/KeyTest.java
@@ -15,4 +15,40 @@ public class KeyTest {
         assertThat(isEqual).isFalse();
     }
 
+    @Test
+    public void sameInstanceIsEqual() {
+        Key key = new Key("bogus".getBytes());
+        assertThat(key).isEqualTo(key).isSameAs(key);
+    }
+
+    @Test
+    public void getKeyBytes() {
+        byte[] data = "bogus".getBytes();
+        final Key key = new Key(data);
+
+        
+        assertThat(key.getKeyBytes())
+                .isEqualTo(data)
+                .isNotSameAs(data);
+
+    }
+    
+    @Test
+    public void hashCodeTest() {
+        byte[] data = "bogus".getBytes();
+        final Key key = new Key(data);
+        assertThat(key)
+                .hasSameHashCodeAs(new Key(data));
+        
+    }
+    
+    @Test
+    public void toStringTest() {
+        byte[] data = "bogus".getBytes();
+        final Key key = new Key(data);
+        
+        assertThat(key.toString())
+                .isNotBlank();
+    }
+    
 }
