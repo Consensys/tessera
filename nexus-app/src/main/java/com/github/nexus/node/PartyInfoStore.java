f72c23e8067ea21786f7eb40e409fb9303a7ef3d
Peter Fox <peter.rd.fox@gmail.com>
2018-06-11 15:48:46 +0100

    Move PartyInfo model objects into own package
    Allow merging of two party info objects so that the result is all
    information is kept, instead of replacing the old info with the new.

--- nexus-app/src/main/java/com/github/nexus/node/PartyInfoStore.java
+++ nexus-app/src/main/java/com/github/nexus/node/PartyInfoStore.java
@@ -1,16 +1,37 @@
 package com.github.nexus.node;
 
-public enum PartyInfoStore {
+import com.github.nexus.configuration.Configuration;
+import com.github.nexus.node.model.Party;
+import com.github.nexus.node.model.PartyInfo;
+import com.github.nexus.node.model.Recipient;
 
-    INSTANCE;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.Set;
+
+public class PartyInfoStore {
 
     private PartyInfo partyInfo;
 
-    public void store(PartyInfo partyInfo) {
-        this.partyInfo  = partyInfo;
+    public PartyInfoStore(final Configuration configuration) {
+
+        final String advertisedUrl = configuration.url() + ":" + Objects.toString(configuration.port());
+
+        this.partyInfo = new PartyInfo(advertisedUrl, Collections.emptySet(), Collections.emptySet());
+    }
+
+    public void store(final PartyInfo partyInfo) {
+        final Set<Recipient> existingRecipients = this.partyInfo.getRecipients();
+        final Set<Recipient> newRecipients = partyInfo.getRecipients();
+        existingRecipients.addAll(newRecipients);
+
+        final Set<Party> existingParties = this.partyInfo.getParties();
+        final Set<Party> newParties = partyInfo.getParties();
+        existingParties.addAll(newParties);
     }
 
     public PartyInfo getPartyInfo() {
         return partyInfo;
     }
+
 }
