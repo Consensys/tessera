d06e6cec654673ce13439483d3aab3d79637acc5
Peter Fox <peter.rd.fox@gmail.com>
2018-06-12 09:22:06 +0100

    Add JavaDocs

--- nexus-configurator/src/main/java/com/github/nexus/configuration/ConfigurationInterceptor.java
+++ nexus-configurator/src/main/java/com/github/nexus/configuration/ConfigurationInterceptor.java
@@ -2,9 +2,20 @@ package com.github.nexus.configuration;
 
 import javax.el.ELContext;
 
+/**
+ * An interceptor provides a method reference to an ELContext that
+ * it can use to change values on configuration properties at the time
+ * of loading
+ */
 @FunctionalInterface
 public interface ConfigurationInterceptor {
 
+    /**
+     * Register intercepting functions on the provided context
+     * {@see FileLoadingInterceptor} for an example on how it works
+     *
+     * @param eLContext The context on which to provide mapping functions
+     */
     void register(ELContext eLContext);
 
 }
