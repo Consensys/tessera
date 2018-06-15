4111df55a96234019da4ee0089a62c29ec3b2ea4
Mark Lowe <melowe@gmail.com>
2018-06-15 01:23:15 +0100

    Remove -9 and add more logging

--- nexus-test/src/test/groovy/killNode.groovy
+++ nexus-test/src/test/groovy/killNode.groovy
@@ -4,12 +4,10 @@ def pid = new File(pidFile).text
 
 log.info "Found Pid file :" + pidFile
 
-def processDesc = "kill -9 $pid"
+def processDesc = "kill $pid"
 
 log.info "Executing: {}", processDesc
 def process = processDesc.execute()
 def exitCode = process.waitFor()
 log.info  "Executed: {} with exit code: {}", processDesc, exitCode
-if(exitCode != 0) {
-    log.error "{}",process.err.text
-}
+log.info "{}",process.text
