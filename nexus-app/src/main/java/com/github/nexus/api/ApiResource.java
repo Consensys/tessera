06f253dea1d9772d5d76caa6eecd859a3581a20a
namtruong <nam.p.truong@outlook.com>
2018-06-15 16:33:59 +0100

    unused imports....

--- nexus-app/src/main/java/com/github/nexus/api/ApiResource.java
+++ nexus-app/src/main/java/com/github/nexus/api/ApiResource.java
@@ -5,7 +5,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.*;
 import javax.ws.rs.core.Response.Status;
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
