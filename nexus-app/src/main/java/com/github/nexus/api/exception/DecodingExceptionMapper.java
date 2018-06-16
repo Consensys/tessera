51620d36d58ba3ed4fce0eb99c9b4ae82e66b984
Mark Lowe <melowe@gmail.com>
2018-06-16 09:36:12 +0100

    create file if it doesn't exist

--- nexus-app/src/main/java/com/github/nexus/api/exception/DecodingExceptionMapper.java
+++ nexus-app/src/main/java/com/github/nexus/api/exception/DecodingExceptionMapper.java
@@ -1,23 +1,21 @@
 package com.github.nexus.api.exception;
 
-
 import com.github.nexus.util.exception.DecodingException;
-
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class DecodingExceptionMapper implements ExceptionMapper<DecodingException> {
 
-    private static final Logger LOGGER = Logger.getLogger(DecodingExceptionMapper.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(DecodingExceptionMapper.class);
 
     @Override
     public Response toResponse(DecodingException e) {
-        LOGGER.log(Level.SEVERE, "",e);
+        LOGGER.error("",e);
 
         return Response.status(Response.Status.BAD_REQUEST)
                 .entity(e.getMessage())
--- nexus-app/src/main/java/com/github/nexus/api/exception/TransactionNotFoundExceptionMapper.java
+++ nexus-app/src/main/java/com/github/nexus/api/exception/TransactionNotFoundExceptionMapper.java
@@ -6,17 +6,17 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Provider
 public class TransactionNotFoundExceptionMapper implements ExceptionMapper<TransactionNotFoundException> {
 
-    private static final Logger LOGGER = Logger.getLogger(TransactionNotFoundExceptionMapper.class.getName());
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransactionNotFoundExceptionMapper.class);
 
     @Override
     public Response toResponse(TransactionNotFoundException e) {
-        LOGGER.log(Level.SEVERE, "",e);
+        LOGGER.error("",e);
 
         return Response.status(Response.Status.BAD_REQUEST)
             .entity(e.getMessage())
