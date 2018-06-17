7f6cbb9ee3e7cc7a33f3f873370659e795aa6b3f
SatpalSandhu61 <quorum@satpal.co.uk>
2018-06-17 02:22:46 +0100

    Merge branch 'master' of https://github.com/QuorumEngineering/Nexus into feature/unix-domain-socket

--- nexus-socket/src/test/java/com/github/nexus/HttpMessageUtilsTest.java
+++ nexus-socket/src/test/java/com/github/nexus/HttpMessageUtilsTest.java
@@ -1,12 +1,22 @@
 package com.github.nexus;
 
 import com.github.nexus.socket.HttpMessageUtils;
-import org.junit.Ignore;
+
 import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
-public class HttpProxyTest {
+public class HttpMessageUtilsTest {
 
     @Test
     public void testContentLengthParser() {
@@ -15,14 +25,55 @@ public class HttpProxyTest {
         assertEquals(42, HttpMessageUtils.getContentLength(headerLine));
     }
 
+    @Test
+    public void testGetHttpMessage() {
+        final String headerLine1 = "HTTP/1.1 200 OK";
+        final String headerLine2 = "Content-Type: text/plain";
+        final String headerLine3 = "Content-Length: 9";
+        final String headerLine4 = "";
+        final String dataLine1 = "some text";
 
-    @Ignore
-    public void testSomething() {
-        final String headerLine = "HTTP/1.1 200 OK\n" +
-            "Content-Type: text/plain\n" +
-            "Content-Length: 9\n" +
-            "\n" +
-            "some text";
+        BufferedReader mockReader = mock(BufferedReader.class);
+        try {
+            when(mockReader.readLine()).thenReturn(headerLine1, headerLine2, headerLine3, headerLine4);
 
+            char[] data = new char[9];
+            Mockito.doAnswer(new Answer<Object>() {
+                @Override
+                public Object answer(InvocationOnMock invocation) throws Throwable {
+                    Object[] args = invocation.getArguments();
+                    char[] cbuf = (char[]) args[0];
+                    int off = (int) args[1];
+                    int len = (int) args[2];
+                    dataLine1.getChars(0, len, cbuf, off);
+                    return 9;
+                }
+            }).when(mockReader).read(data, 0, 9);
+
+            String message = HttpMessageUtils.getHttpMessage(mockReader);
+
+            verify(mockReader).read(any(char[].class), any(int.class), any(int.class));
+            assertThat(message).isEqualTo(headerLine1 + "\n" + headerLine2 + "\n" + headerLine3 + "\n" + headerLine4 + "\n" + dataLine1);
+            verify(mockReader, times(4)).readLine();
+
+        } catch (IOException ex) {
+            fail("Unexpected exception thrown");
+        }
+    }
+
+    @Test
+    public void testGetHttpMessageException() {
+
+        try {
+            BufferedReader mockReader = mock(BufferedReader.class);
+
+            when(mockReader.readLine()).thenThrow(new IOException());
+
+            String message = HttpMessageUtils.getHttpMessage(mockReader);
+
+            failBecauseExceptionWasNotThrown(IOException.class);
+
+        } catch (Exception ex) {
+        }
     }
 }
--- nexus-socket/src/test/java/com/github/nexus/HttpProxyTest.java
+++ nexus-socket/src/test/java/com/github/nexus/HttpProxyTest.java
@@ -1,28 +1,41 @@
 package com.github.nexus;
 
-import com.github.nexus.socket.HttpMessageUtils;
+import com.github.nexus.socket.HttpProxy;
+import com.github.nexus.socket.SocketFactory;
 import org.junit.Ignore;
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.io.*;
+import java.net.Socket;
+import java.net.URI;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
 public class HttpProxyTest {
 
-    @Test
-    public void testContentLengthParser() {
-        final String headerLine = "Content-Length: 42";
-
-        assertEquals(42, HttpMessageUtils.getContentLength(headerLine));
-    }
-
-
     @Ignore
-    public void testSomething() {
-        final String headerLine = "HTTP/1.1 200 OK\n" +
-            "Content-Type: text/plain\n" +
-            "Content-Length: 9\n" +
-            "\n" +
-            "some text";
+    public void testConnection() {
+
+        try {
+            Socket mockSocket = mock(Socket.class);
+
+            SocketFactory mockSocketFactory = mock(SocketFactory.class);
+            when(mockSocketFactory.create(any(URI.class))).thenReturn(mockSocket);
+
+
+            URI uri = new URI("http://localhost:8080");
+            HttpProxy httpProxy = new HttpProxy(uri);
+
+            OutputStream outputStream = new ObjectOutputStream(new ByteArrayOutputStream());
+            when(mockSocket.getOutputStream()).thenReturn(outputStream);
+
+            boolean result = httpProxy.connect();
+
+            assertThat(result).isTrue();
+        } catch (Exception ex) {
+            fail("Unexpected exception thrown", ex);
+        }
 
     }
 }
