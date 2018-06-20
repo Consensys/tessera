7eee6c0fbd6e2e4223265dbff327d81d6b63848d
Peter Fox <peter.rd.fox@gmail.com>
2018-06-20 11:31:04 +0100

    Move the Logging Filter to the filter package

--- nexus-app/src/main/java/com/github/nexus/api/filter/Logged.java
+++ nexus-app/src/main/java/com/github/nexus/api/filter/Logged.java
@@ -1,10 +1,10 @@
-package com.github.nexus.api;
+package com.github.nexus.api.filter;
 
+import javax.ws.rs.NameBinding;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import javax.ws.rs.NameBinding;
 
 @Target({ElementType.TYPE, ElementType.METHOD})
 @Retention(value = RetentionPolicy.RUNTIME)
