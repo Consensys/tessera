1c7dfa725adf4d68ff9b3eb595a74615828fa3b6
Peter Fox <peter.rd.fox@gmail.com>
2018-06-23 22:56:32 +0100

    Fixed many formatting issues Re-wrote while loop to a do-while loop

--- nexus-socket/src/main/java/com/github/nexus/socket/InputStreamUtils.java
+++ nexus-socket/src/main/java/com/github/nexus/socket/InputStreamUtils.java
@@ -10,19 +10,20 @@ public class InputStreamUtils {
     //buffer size in bytes
     private static final int BUFFER_SIZE = 8192;
 
-    public static byte[] readAllBytes(final InputStream is) throws IOException {
+    static byte[] readAllBytes(final InputStream is) throws IOException {
 
         final ByteArrayOutputStream os = new ByteArrayOutputStream(BUFFER_SIZE);
 
-        int read = BUFFER_SIZE;
+        int read;
 
-        while(read == BUFFER_SIZE) {
+        do {
 
             final byte[] buff = new byte[BUFFER_SIZE];
             read = is.read(buff);
 
             os.write(Arrays.copyOf(buff, read));
-        }
+
+        } while (read == BUFFER_SIZE);
 
         return os.toByteArray();
     }
