de2696e6a8f9656d99399fb45063ff4f95cb63bc
namtruong <nam.p.truong@outlook.com>
2018-06-24 13:41:06 +0100

    unit tests refactored for client factory

--- nexus-app/src/test/java/com/github/nexus/node/ClientFactoryTest.java
+++ nexus-app/src/test/java/com/github/nexus/node/ClientFactoryTest.java
@@ -1,13 +1,13 @@
 package com.github.nexus.node;
 
-import org.junit.Test;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
 
 import javax.ws.rs.client.Client;
+import java.io.File;
 import java.io.IOException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.UnrecoverableKeyException;
+import java.security.*;
 import java.security.cert.CertificateException;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
@@ -16,22 +16,51 @@ public class ClientFactoryTest {
 
     ClientFactory factory = new ClientFactory();
 
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    static TemporaryFolder delegate;
+
+    File tmpKeyFile;
+
+    @Before
+    public void setUp() {
+        tmpKeyFile = new File(temporaryFolder.getRoot(), "test-keystore");
+    }
+
+    @After
+    public void after(){
+        delegate = temporaryFolder;
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        assertThat(delegate.getRoot().exists()).isFalse();
+    }
+
     @Test
-    public void testBuildInsecureClient() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
-        Client client = factory.buildClient("off","","","","","","");
+    public void testBuildInsecureClient() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, OperatorCreationException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Client client = factory.buildClient("somethingInvalid","","","","","","");
         assertThat(client).isNotNull();
     }
 
     @Test
-    public void testBuildSecureClientCAMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
-        Client client = factory.buildClient("strict","","","","","CA","");
+    public void testBuildSecureClientCAMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, OperatorCreationException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Client client = factory.buildClient(
+            "strict",
+            tmpKeyFile.getPath(),
+            "quorum",
+            "",
+            "",
+            "CA",
+            "");
         assertThat(client).isNotNull();
         assertThat(client.getSslContext()).isNotNull();
         assertThat(client.getSslContext().getProtocol().equals("TLS"));
     }
 
     @Test
-    public void testBuildSecureClientDefaultMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
+    public void testBuildSecureClientDefaultMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, OperatorCreationException, NoSuchProviderException, InvalidKeyException, SignatureException {
         Client client = factory.buildClient("strict","","","","","something invalid","");
         assertThat(client).isNotNull();
         assertThat(client.getSslContext()).isNotNull();
@@ -39,24 +68,24 @@ public class ClientFactoryTest {
     }
 
     @Test
-    public void testBuildSecureClientTOFUMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
-        Client client = factory.buildClient("strict","","","","","TOFU","");
+    public void testBuildSecureClientTOFUMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, OperatorCreationException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Client client = factory.buildClient("strict",tmpKeyFile.getPath(),"quorum","","","TOFU","");
         assertThat(client).isNotNull();
         assertThat(client.getSslContext()).isNotNull();
         assertThat(client.getSslContext().getProtocol().equals("TLS"));
     }
 
     @Test
-    public void testBuildSecureClientWhiteListMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
-        Client client = factory.buildClient("strict","","","","","WHITELIST","");
+    public void testBuildSecureClientWhiteListMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, OperatorCreationException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Client client = factory.buildClient("strict",tmpKeyFile.getPath(),"quorum","","","WHITELIST","");
         assertThat(client).isNotNull();
         assertThat(client.getSslContext()).isNotNull();
         assertThat(client.getSslContext().getProtocol().equals("TLS"));
     }
 
     @Test
-    public void testBuildSecureTrustAllMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
-        Client client = factory.buildClient("strict","","","","","NONE","");
+    public void testBuildSecureTrustAllMode() throws CertificateException, UnrecoverableKeyException, NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, OperatorCreationException, NoSuchProviderException, InvalidKeyException, SignatureException {
+        Client client = factory.buildClient("strict",tmpKeyFile.getPath(),"quorum","","","NONE","");
         assertThat(client).isNotNull();
         assertThat(client.getSslContext()).isNotNull();
         assertThat(client.getSslContext().getProtocol().equals("TLS"));
