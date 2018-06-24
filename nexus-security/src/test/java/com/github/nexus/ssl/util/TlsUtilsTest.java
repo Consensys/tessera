48dc2a9d5dc9630a7e0ec8e974cc2e45161a9710
namtruong <nam.p.truong@outlook.com>
2018-06-24 23:07:56 +0100

    updated unit tests for ssl

--- nexus-security/src/test/java/com/github/nexus/ssl/util/TlsUtilsTest.java
+++ nexus-security/src/test/java/com/github/nexus/ssl/util/TlsUtilsTest.java
@@ -17,15 +17,12 @@ public class TlsUtilsTest {
 
     @Test
     public void testGenerateKeys() throws OperatorCreationException, InvalidKeyException, NoSuchAlgorithmException, IOException, SignatureException, NoSuchProviderException, CertificateException, KeyStoreException {
-        TlsUtils tlsUtils = new TlsUtils();
 
-        File certificateFile = new File(tmpDir.getRoot(), "server.key");
-        File privateKeyFile = new File(tmpDir.getRoot(), "server.cer");
+        File privateKeyFile = new File(tmpDir.getRoot(),"./test-keystore");
 
-        tlsUtils.generateKeyStoreWithSelfSignedCertificate(privateKeyFile, certificateFile);
+        privateKeyFile.createNewFile();
 
-//        File resultPrivateKeyFile = new File(tmpDir.getRoot(), "server.key");
-//        assertTrue(resultPrivateKeyFile.exists());
+        TlsUtils.create().generateKeyStoreWithSelfSignedCertificate(privateKeyFile,"quorum");
 
     }
 }
