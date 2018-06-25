f0a7940e90094e3db2f073dcd2160949cc1a3fe2
Peter Fox <peter.rd.fox@gmail.com>
2018-06-25 15:20:05 +0100

    Move the IPC path to the static constructor so that it doesn't muddy the waters with the main socket object (it can remain config-agnostic)

--- nexus-socket/src/main/java/com/github/nexus/socket/SocketServerFactory.java
+++ nexus-socket/src/main/java/com/github/nexus/socket/SocketServerFactory.java
@@ -4,13 +4,14 @@ package com.github.nexus.socket;
 import com.github.nexus.configuration.Configuration;
 import com.github.nexus.junixsocket.adapter.UnixSocketFactory;
 
+import java.nio.file.Paths;
 import java.util.concurrent.Executors;
 
 public interface SocketServerFactory {
 
     static SocketServer createSocketServer(final Configuration config) {
         return new SocketServer(
-            config,
+            Paths.get(config.workdir(), config.socket()),
             new HttpProxyFactory(config.uri()),
             Executors.newCachedThreadPool(),
             UnixSocketFactory.create()
