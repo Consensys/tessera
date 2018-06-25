afea9b23b7564aa6531f12ae16962fde77483b9e
SatpalSandhu61 <quorum@satpal.co.uk>
2018-06-25 10:02:23 +0100

    Fix issue with PartyInfo class not being thread safe - we’re attempting to retrieve party info, whilst it’s being updated from another node. This occasionally results in garbage being returned to a node, resulting in out of memory at that node.

--- nexus-app/src/main/java/com/github/nexus/node/model/PartyInfo.java
+++ nexus-app/src/main/java/com/github/nexus/node/model/PartyInfo.java
@@ -10,6 +10,15 @@ public class PartyInfo {
 
     private Set<Party> parties;
 
+    /**
+     * Create a deep copy
+     */
+    public PartyInfo(final PartyInfo toCopy) {
+        this.url = toCopy.url;
+        this.recipients = Collections.synchronizedSet(new HashSet<>(toCopy.recipients));
+        this.parties = Collections.synchronizedSet(new HashSet<>(toCopy.parties));
+    }
+
     public PartyInfo(final String url, final Set<Recipient> recipients, final Set<Party> parties) {
         this.url = Objects.requireNonNull(url);
         this.recipients = Collections.synchronizedSet(new HashSet<>(recipients));
