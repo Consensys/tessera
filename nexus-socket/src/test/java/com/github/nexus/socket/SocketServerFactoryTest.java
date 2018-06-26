60604dd082b84c866ba40ed379ef48400be05dfd
Mark <melowe.quorum@gmail.com>
2018-06-26 22:36:20 +0100

    Untangled fixtures using Configuration object

--- nexus-socket/src/test/java/com/github/nexus/socket/SocketServerFactoryTest.java
+++ nexus-socket/src/test/java/com/github/nexus/socket/SocketServerFactoryTest.java
@@ -1,6 +1,8 @@
 package com.github.nexus.socket;
 
-import com.github.nexus.configuration.Configuration;
+
+import com.github.nexus.config.Config;
+import com.github.nexus.config.ServerConfig;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -8,10 +10,10 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -23,12 +25,14 @@ public class SocketServerFactoryTest {
         final Path socketDir = Files.createTempDirectory(UUID.randomUUID().toString());
         final String socketFile = "junit.txt";
         
-        final Configuration config = mock(Configuration.class);
-        when(config.uri()).thenReturn(new URI("http://bogos.com"));
-        when(config.workdir()).thenReturn(socketDir.toString());
-        when(config.socket()).thenReturn(socketFile);
-        doReturn("OFF").when(config).tls();
+        final Config config = mock(Config.class);
+        ServerConfig serverConfig = mock(ServerConfig.class);
+        when(serverConfig.getServerUri()).thenReturn(new URI("http://bogos.com"));
         
+        when(config.getUnixSocketFile())
+                .thenReturn(Paths.get(socketDir.toString(), socketFile));
+
+        when(config.getServerConfig()).thenReturn(serverConfig);
         final SocketServer result = SocketServerFactory.createSocketServer(config);
         assertThat(result).isNotNull();
 
