6275ea7f19a7785a3551bfb35efa5ba5ee18b23b
Peter Fox <peter.rd.fox@gmail.com>
2018-06-26 09:49:11 +0100

    Initialise the unix socket server in the constructor so that it fails immediately if not valid. Move the accepting of socket connections into the socket server so that multiple connections can potentially be accepted at the same time.

--- nexus-socket/src/main/java/com/github/nexus/socket/SocketServer.java
+++ nexus-socket/src/main/java/com/github/nexus/socket/SocketServer.java
@@ -4,28 +4,22 @@ import com.github.nexus.junixsocket.adapter.UnixSocketFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Path;
+import java.util.Objects;
 import java.util.concurrent.ExecutorService;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Create a server listening on a Unix Domain Socket for http requests. We
  * create a connection to an HTTP server, and act as a proxy between the socket
- * and the HTTP server. TODO: should possibly support connections from multiple
- * clients
- * <p>
- * FIXME: This object has far too many internal dependencies, making it
- * resistant to effective testing.
+ * and the HTTP server.
+ * TODO: should possibly support connections from multiple clients
  */
 public class SocketServer implements Runnable {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SocketServer.class);
-
-    private UnixDomainServerSocket serverUds;
 
     private final HttpProxyFactory httpProxyFactory;
 
@@ -33,15 +27,7 @@ public class SocketServer implements Runnable {
 
     private final ExecutorService executor;
 
-    ////
-
-    private final Path socketFile;
-
-    private final UnixSocketFactory unixSocketFactory;
-
-    private ServerSocket server;
-
-    ////
+    private final ServerSocket server;
 
     /**
      * Create the unix domain socket and start the listener thread.
@@ -51,28 +37,20 @@ public class SocketServer implements Runnable {
                         final ExecutorService executor,
                         final UnixSocketFactory unixSocketFactory) {
 
-        this.unixSocketFactory = requireNonNull(unixSocketFactory);
+        this.httpProxyFactory = Objects.requireNonNull(httpProxyFactory);
 
-        this.httpProxyFactory = httpProxyFactory;
-        requireNonNull(unixSocketFactory);
+        this.executor = Objects.requireNonNull(executor, "Executor service is required");
 
-        this.executor = requireNonNull(executor, "Executor service is required");
-
-        this.socketFile = requireNonNull(socketFile);
-    }
-
-    @PostConstruct
-    public void init() {
         try {
-            server = unixSocketFactory.createServerSocket(socketFile);
-            LOGGER.info("server: {}", server);
+            this.server = unixSocketFactory.createServerSocket(socketFile);
 
-            serverUds = new UnixDomainServerSocket(server);
+            LOGGER.info("IPC server: {}", server);
 
         } catch (final IOException ex) {
-            LOGGER.error("Failed to create Unix Domain Socket: {}/{}", socketFile.toString());
+            LOGGER.error("Failed to create Unix Domain Socket: {}", socketFile.toString());
             throw new NexusSocketException(ex);
         }
+
     }
 
     /**
@@ -83,22 +61,32 @@ public class SocketServer implements Runnable {
      */
     @Override
     public void run() {
+        final UnixDomainServerSocket udss;
+
         LOGGER.info("Waiting for client connection on unix domain socket...");
-        serverUds.connect();
+        try {
+            final Socket accept = server.accept();
+
+            udss = new UnixDomainServerSocket(accept);
+
+        } catch (IOException ex) {
+            LOGGER.error("Failed to create Socket");
+            throw new NexusSocketException(ex);
+        }
         LOGGER.info("Client connection received");
 
         //Get a connection to the HTTP server.
         if (createHttpServerConnection()) {
 
             //Read the request from the socket and send it to the HTTP server
-            byte[] message = serverUds.read();
+            byte[] message = udss.read();
             LOGGER.info("Received message on socket: {}", message);
             httpProxy.sendRequest(message);
 
             //Return the HTTP response to the socket
             byte[] response = httpProxy.getResponse();
             LOGGER.info("Received http response: {}", response);
-            serverUds.write(response);
+            udss.write(response);
 
             httpProxy.disconnect();
         }
