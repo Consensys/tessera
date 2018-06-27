8e7f5a4624492bb2ce63eea7e7e298f0883c20c7
Mark <melowe@me.com>
2018-06-27 17:55:41 +0100

    Use ServiceLaoder to resolve SSLCOntextFactory

--- nexus-security/src/main/java/com/github/nexus/ssl/SSLContextFactory.java
+++ nexus-security/src/main/java/com/github/nexus/ssl/SSLContextFactory.java
@@ -1,46 +1,16 @@
 package com.github.nexus.ssl;
 
 import com.github.nexus.config.SslConfig;
-import com.github.nexus.config.SslTrustMode;
-import com.github.nexus.ssl.strategy.TrustMode;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SignatureException;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
+import java.util.ServiceLoader;
 import javax.net.ssl.SSLContext;
-import org.bouncycastle.operator.OperatorCreationException;
 
 public interface SSLContextFactory {
     
-    static SSLContext from(SslConfig sslConfig) {
-
-        SslTrustMode sslTrustMode = Optional.ofNullable(sslConfig.getServerTrustMode())
-                .orElse(SslTrustMode.NONE);
-        
-        TrustMode trustMode = Stream.of(TrustMode.values())
-                .filter(tm -> Objects.equals(tm.name(),sslTrustMode.name()))
-                .findAny().get();
-
-        String keyStore = sslConfig.getServerKeyStore().toString();
-        String keyStorePassword = sslConfig.getServerKeyStorePassword();
-        String trustStore = sslConfig.getServerTrustStore().toString();
-        String trustStorePassword = sslConfig.getServerTrustStorePassword();
-        String knownHostsFile = sslConfig.getKnownServersFile().toString();
-
-        try {
-            return trustMode
-                .createSSLContext(keyStore, keyStorePassword, trustStore, trustStorePassword, knownHostsFile);
-        } catch (NoSuchAlgorithmException | KeyManagementException | UnrecoverableKeyException | CertificateException | KeyStoreException | IOException | OperatorCreationException | NoSuchProviderException | InvalidKeyException | SignatureException ex) {
-           throw new NexusSecurityException(ex);
-        } 
+    static SSLContextFactory create() {
+        return ServiceLoader.load(SSLContextFactory.class).iterator().next();
     }
+    
+    
+     SSLContext from(SslConfig sslConfig);
 
 }
