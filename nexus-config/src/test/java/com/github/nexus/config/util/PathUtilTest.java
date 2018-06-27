e1ef3e629ad1f314433264755e7ed27452968828
Mark <melowe@me.com>
2018-06-27 14:28:59 +0100

    Load paths into values during object creation

---
+++ nexus-config/src/test/java/com/github/nexus/config/util/PathUtilTest.java
@@ -0,0 +1,51 @@
+package com.github.nexus.config.util;
+
+import com.github.nexus.config.ConfigException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+
+public class PathUtilTest {
+
+    @Test
+    public void readDataValueAndPathAreNotNull() {
+
+        Path path = mock(Path.class);
+
+        String value = "HELLOW";
+
+        assertThat(PathUtil.readData(path, value)).isEqualTo(value);
+
+    }
+
+    @Test
+    public void readDataValueIsNotNullAndPathIsNull() {
+
+        String value = "HELLOW";
+
+        assertThat(PathUtil.readData(null, value)).isEqualTo(value);
+
+    }
+
+    @Test
+    public void readDataValueIsNullAndPathIsValid() throws URISyntaxException {
+
+        Path path = Paths.get(getClass().getResource("/keyfile.txt").toURI());
+
+        assertThat(PathUtil.readData(path, null)).isEqualTo("SOMEDATA");
+
+    }
+
+    @Test(expected = ConfigException.class)
+    public void readPathThrowsIOException() throws URISyntaxException {
+
+        Path path = Paths.get("bogus");
+        
+
+        PathUtil.readData(path, null);
+
+    }
+}
