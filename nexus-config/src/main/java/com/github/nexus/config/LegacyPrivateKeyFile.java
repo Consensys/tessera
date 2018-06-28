1864465a584832a8e4d43cb37d8ee141d90ee6c3
Peter Fox <peter.rd.fox@gmail.com>
2018-06-28 16:30:32 +0100

    Add reading the path from file on JAXB instantiated classes (that bypass the constructor)

--- nexus-config/src/main/java/com/github/nexus/config/LegacyPrivateKeyFile.java
+++ nexus-config/src/main/java/com/github/nexus/config/LegacyPrivateKeyFile.java
@@ -5,14 +5,53 @@ package com.github.nexus.config;
 //{"data":{"aopts":{"variant":"id","memory":1048576,"iterations":10,"parallelism":4,"version":"1.3"},"snonce":"x3HUNXH6LQldKtEv3q0h0hR4S12Ur9pC","asalt":"7Sem2tc6fjEfW3yYUDN/kSslKEW0e1zqKnBCWbZu2Zw=","sbox":"d0CmRus0rP0bdc7P7d/wnOyEW14pwFJmcLbdu2W3HmDNRWVJtoNpHrauA/Sr5Vxc"},"type":"argon2sbox"}
 public class LegacyPrivateKeyFile {
     
-    private String path;
+    private final String bytes;
     
-    private String bytes;
-    
-    private String type;
-    
-    
-    
-    
-    
+    private final PrivateKeyType type;
+
+    private final String snonce;
+
+    private final String asalt;
+
+    private final String sbox;
+
+    private final ArgonOptions argonOptions;
+
+    public LegacyPrivateKeyFile(final String bytes,
+                                final PrivateKeyType type,
+                                final ArgonOptions argonOptions,
+                                final String snonce,
+                                final String asalt,
+                                final String sbox) {
+        this.bytes = bytes;
+        this.type = type;
+        this.argonOptions = argonOptions;
+        this.snonce = snonce;
+        this.asalt = asalt;
+        this.sbox = sbox;
+    }
+
+    public String getBytes() {
+        return bytes;
+    }
+
+    public PrivateKeyType getType() {
+        return type;
+    }
+
+    public ArgonOptions getArgonOptions() {
+        return argonOptions;
+    }
+
+    public String getSnonce() {
+        return snonce;
+    }
+
+    public String getAsalt() {
+        return asalt;
+    }
+
+    public String getSbox() {
+        return sbox;
+    }
 }
--- nexus-config/src/test/java/com/github/nexus/config/OpenPojoTest.java
+++ nexus-config/src/test/java/com/github/nexus/config/OpenPojoTest.java
@@ -3,7 +3,6 @@ package com.github.nexus.config;
 import com.openpojo.validation.Validator;
 import com.openpojo.validation.ValidatorBuilder;
 import com.openpojo.validation.rule.impl.GetterMustExistRule;
-import com.openpojo.validation.test.impl.DefaultValuesNullTester;
 import com.openpojo.validation.test.impl.GetterTester;
 import org.junit.Test;
 
@@ -18,7 +17,7 @@ public class OpenPojoTest {
         Validator pojoValidator = ValidatorBuilder.create()
                 .with(new GetterMustExistRule())
                 .with(new GetterTester())
-                .with(new DefaultValuesNullTester())
+//                .with(new DefaultValuesNullTester())
                 .build();
 
         
