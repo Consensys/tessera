02260cdc7022aaaa5033c7b2c356128b9d0ee604
Peter Fox <peter.rd.fox@gmail.com>
2018-07-03 17:07:21 +0100

    Checkstyle issues

--- nexus-socket-parent/junixsocket-adapter/src/main/java/com/github/nexus/junixsocket/adapter/DependencyInstaller.java
+++ nexus-socket-parent/junixsocket-adapter/src/main/java/com/github/nexus/junixsocket/adapter/DependencyInstaller.java
@@ -6,7 +6,6 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Stream;
 
 public class DependencyInstaller {
 
