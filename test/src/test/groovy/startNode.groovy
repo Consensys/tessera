b621db0ec515b8da8192033461f9317df9299d91
namtruong <nam.p.truong@outlook.com>
2018-07-09 12:22:13 +0100

    test module

---
+++ test/src/test/groovy/startNode.groovy
@@ -0,0 +1,37 @@
+
+def jarfile = properties['jarfile']
+
+def configFile = properties['configFile']
+
+def pidFile = properties['pidFile']
+
+def logbackConfigFile = properties['logbackConfigFile']
+
+def nodeNumber = properties['nodeNumber']
+
+log.info "$jarfile"
+// -Dspring.profiles.active=disable-unixsocket
+def processDesc = "java -Dspring.profiles.active=disable-unixsocket -Dnode.number=$nodeNumber -Dlogback.configurationFile=$logbackConfigFile -jar $jarfile -configfile $configFile -pidfile $pidFile "
+
+log.info "$processDesc"
+
+def countdownLatch = new java.util.concurrent.CountDownLatch(1)
+
+def process = "$processDesc".execute()
+
+def t = new Thread({
+    def exitCode = process.waitFor()
+    log.info "Exit code: {}",exitCode
+    if(exitCode != 0) {
+        log.error process.err.text
+            
+    }
+    countdownLatch.countDown()
+})
+
+t.start()
+
+countdownLatch.await(10,java.util.concurrent.TimeUnit.SECONDS)
+
+
+
