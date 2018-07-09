a238dbbb85d6a8df08254ef98076aa35abc7950c
namtruong <nam.p.truong@outlook.com>
2018-07-09 16:42:01 +0100

    remove dead code

--- socket-parent/socket/src/main/java/com/github/tessera/socket/NexusSocketException.java
+++
@@ -1,9 +0,0 @@
-package com.github.tessera.socket;
-
-public class NexusSocketException extends RuntimeException {
-
-    public NexusSocketException(final Throwable cause) {
-        super(cause);
-    }
-
-}
