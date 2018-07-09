91760841095efb951199bf37825086113dcf5ce3
Peter Fox <peter.rd.fox@gmail.com>
2018-07-09 14:21:38 +0100

    Added test for non equal objects.

--- nexus-app/src/test/java/com/github/nexus/transaction/model/EncryptedTransactionTest.java
+++ nexus-app/src/test/java/com/github/nexus/transaction/model/EncryptedTransactionTest.java
@@ -9,6 +9,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class EncryptedTransactionTest {
 
     @Test
+    public void subclassesEqual() {
+
+        class OtherClass extends EncryptedTransaction {
+        }
+
+        final OtherClass other = new OtherClass();
+        final EncryptedTransaction et = new EncryptedTransaction();
+
+        other.setId(1L);
+        et.setId(1L);
+
+        final boolean equal = Objects.equals(et, other);
+
+        assertThat(equal).isTrue();
+
+    }
+
+    @Test
     public void differentClassesNotEqual() {
 
         final Object other = "OTHER";
