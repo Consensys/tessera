2ef908d2a1a62088273005a2fad66446d073566a
Mark <melowe_quorum@gmail.com>
2018-07-12 16:33:03 +0100

    Coverage gaps revealed on codcov

--- config/src/test/java/com/github/tessera/config/constraints/PathValidatorTest.java
+++ config/src/test/java/com/github/tessera/config/constraints/PathValidatorTest.java
@@ -1,10 +1,13 @@
 package com.github.tessera.config.constraints;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import org.junit.Test;
 
 import javax.validation.ConstraintValidatorContext;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -46,4 +49,23 @@ public class PathValidatorTest {
 
     }
 
+    @Test
+    public void validateFileExistsWhenFileDoesExist() throws IOException {
+
+        ConstraintValidatorContext context = mock(ConstraintValidatorContext.class);
+
+        ValidPath validPath = mock(ValidPath.class);
+        when(validPath.checkExists()).thenReturn(true);
+
+        PathValidator pathValidator = new PathValidator();
+        pathValidator.initialize(validPath);
+
+        Path tempDir = Paths.get(System.getProperty("java.io.tmpdir"));
+        Path actualFile = Files.createTempFile(tempDir, UUID.randomUUID().toString(), ".txt");
+
+        assertThat(pathValidator.isValid(actualFile, context)).isTrue();
+
+        Files.deleteIfExists(actualFile);
+
+    }
 }
