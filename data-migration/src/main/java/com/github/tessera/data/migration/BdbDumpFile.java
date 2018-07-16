17ee2d7bf512069d8c5ae29d24a8e7eed652f0f7
Mark <melowe.quorum@gmail.com>
2018-07-16 17:37:05 +0100

    java doc notes.

---
+++ data-migration/src/main/java/com/github/tessera/data/migration/BdbDumpFile.java
@@ -0,0 +1,58 @@
+package com.github.tessera.data.migration;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Objects;
+import java.util.stream.Stream;
+
+/**
+ * Assumes that user has exported data from bdb using db_dump
+ * <pre>
+ *  db_dump -f exported.txt c1/cn§.db/playload.db
+ * </pre>
+ *
+ */
+public class BdbDumpFile {
+
+    private final Path inputFile;
+
+    public BdbDumpFile(Path inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    public void execute(OutputStream outputStream) throws IOException {
+
+        String sql = "INSERT INTO ENCRYPTED_TRANSACTION (ENC_TX_SEQ,HASH,ENCODED_PAYLOAD) VALUES (ENC_TX_SEQ.NEXTVAL,'%s','%s');";
+
+        try (BufferedReader reader = Files.newBufferedReader(inputFile);
+                BufferedWriter writer = Stream.of(outputStream)
+                        .map(OutputStreamWriter::new)
+                        .map(BufferedWriter::new)
+                        .findAny().get()) {
+
+            while (true) {
+                String line = reader.readLine();
+                if (Objects.isNull(line)) {
+                    break;
+                }
+
+                if (!line.startsWith(" ")) {
+                    continue;
+                }
+                final String key = line;
+                final String value = reader.readLine();
+
+                final String insertLine = String.format(sql, key.trim(), value.trim());
+                writer.write(insertLine);
+                writer.newLine();
+
+            }
+        }
+    }
+
+}
