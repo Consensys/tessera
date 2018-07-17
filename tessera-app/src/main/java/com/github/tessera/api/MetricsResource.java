e44f8d9c64df148f199f307f9194b6df42ce2063
chris-j-h <chris.hounsom@gmail.com>
2018-07-17 10:46:51 +0100

    Expose some Jersey request data on /metrics endpoint

--- tessera-app/src/main/java/com/github/tessera/api/MetricsResource.java
+++ tessera-app/src/main/java/com/github/tessera/api/MetricsResource.java
@@ -1,40 +1,35 @@
 package com.github.tessera.api;
 
-import javax.management.remote.JMXConnector;
-import javax.management.remote.JMXServiceURL;
+import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import java.net.MalformedURLException;
+
+import org.glassfish.jersey.server.monitoring.MonitoringStatistics;
+import org.glassfish.jersey.server.monitoring.ResourceStatistics;
+import org.glassfish.jersey.server.monitoring.TimeWindowStatistics;
 
 @Path("/metrics")
 public class MetricsResource {
 
-    JMXServiceURL url;
-    JMXConnector jmxc;
-
-    public MetricsResource() {
-        String hostName = "localhost";
-        String portNum = "8080";
-
-        try {
-            url = new JMXServiceURL("service:jmx:rmi:///jndi/rmi://" + hostName + ":" + portNum +  "/jmxrmi");
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-    }
+    @Inject
+    Provider<MonitoringStatistics> monitoringStatisticsProvider;
 
     @GET
     @Produces("text/plain")
-    public String metrics() {
+    public String getSomething() {
+        final MonitoringStatistics monitoringStatistics = monitoringStatisticsProvider.get();
 
-//        try {
-//            jmxc = JMXConnectorFactory.connect(url);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        final TimeWindowStatistics timeWindowStatistics = monitoringStatistics.getRequestStatistics().getTimeWindowStatistics().get(0L);
+        final TimeWindowStatistics sendRawExecutionStatistics = monitoringStatistics.getUriStatistics().get("/sendraw").getRequestExecutionStatistics().getTimeWindowStatistics().get(0L);
+        final TimeWindowStatistics sendExecutionStatistics = monitoringStatistics.getUriStatistics().get("/send").getRequestExecutionStatistics().getTimeWindowStatistics().get(0L);
 
-        return "Hello World";
+        return "total_request_count " + timeWindowStatistics.getRequestCount() + "\n" +
+               "sendraw_request_count " + sendRawExecutionStatistics.getRequestCount() + "\n" +
+               "send_request_count " + sendExecutionStatistics.getRequestCount() + "\n" +
+               "total_avg_request_processing_ms " + timeWindowStatistics.getAverageDuration() + "\n" +
+               "sendraw_request_processing_ms " + sendRawExecutionStatistics.getAverageDuration() + "\n" +
+               "send_request_processing_ms " + sendExecutionStatistics.getAverageDuration();
     }
 }
