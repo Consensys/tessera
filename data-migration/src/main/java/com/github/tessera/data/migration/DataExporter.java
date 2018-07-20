81b75763ad81c0b87677118c0aa9bec55f585541
Mark <melowe.quorum@gmail.com>
2018-07-20 17:31:26 +0100

    Add test and service loader resources

--- data-migration/src/main/java/com/github/tessera/data/migration/DataExporter.java
+++ data-migration/src/main/java/com/github/tessera/data/migration/DataExporter.java
@@ -1,6 +1,5 @@
 package com.github.tessera.data.migration;
 
-import java.util.Iterator;
 import java.util.Map;
 import java.util.ServiceLoader;
 
@@ -9,11 +8,7 @@ public interface DataExporter {
     void export(Map<byte[], byte[]> data);
 
     static DataExporter create() {
-        Iterator<DataExporter> it = ServiceLoader.load(DataExporter.class).iterator();
-        if(it.hasNext()) {
-            return it.next();
-        }
-        return new DefaultDataExporter();
+        return ServiceLoader.load(DataExporter.class).iterator().next();
     }
     
 }
