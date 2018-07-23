43e0e4ac8155f5379a4aa28bf87d56b8d39de3b7
Mark <melowe.quorum@gmail.com>
2018-07-23 18:43:20 +0100

    Add FIXME tags for known issues.

--- config-cli/src/main/java/com/github/tessera/config/cli/TomlConfigFactory.java
+++ config-cli/src/main/java/com/github/tessera/config/cli/TomlConfigFactory.java
@@ -2,18 +2,28 @@ package com.github.tessera.config.cli;
 
 import com.github.tessera.config.Config;
 import com.github.tessera.config.ConfigFactory;
+import com.github.tessera.config.PrivateKeyData;
 import com.github.tessera.config.SslAuthenticationMode;
 import com.github.tessera.config.SslTrustMode;
+import com.github.tessera.config.util.JaxbUtil;
 import com.github.tessera.io.FilesDelegate;
+import com.github.tessera.io.IOCallback;
 import com.moandjiezana.toml.Toml;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,14 +35,12 @@ public class TomlConfigFactory implements ConfigFactory {
 
     public TomlConfigFactory() {
         this(FilesDelegate.create());
-    }    
-    
+    }
+
     public TomlConfigFactory(FilesDelegate filesDelegate) {
         this.filesDelegate = Objects.requireNonNull(filesDelegate);
     }
-    
-    
-    
+
     @Override
     public Config create(InputStream configData, InputStream... keyConfigData) {
         if (keyConfigData.length != 0) {
@@ -56,17 +64,17 @@ public class TomlConfigFactory implements ConfigFactory {
         //??
         String workdir = toml.getString("workdir", ".");
 
-        List<String> othernodes = toml.getList("othernodes", Collections.EMPTY_LIST);
+        final List<String> othernodes = toml.getList("othernodes", Collections.EMPTY_LIST);
 
-        List<String> publicKeyList = toml.getList("publickeys", Collections.EMPTY_LIST);
+        final List<String> publicKeyList = toml.getList("publickeys", Collections.EMPTY_LIST);
 
-        List<String> privateKeyList = toml.getList("privatekeys", Collections.EMPTY_LIST);
+        final List<String> privateKeyList = toml.getList("privatekeys", Collections.EMPTY_LIST);
 
         //String privateKeyPasswordFile = toml.getString("passwords")
         final List<String> privateKeyPasswords;
         if (toml.contains("passwords")) {
             String privateKeyPasswordFile = toml.getString("passwords");
-            
+
             Path privateKeyPasswordFilePath = Paths.get(privateKeyPasswordFile);
             privateKeyPasswords = filesDelegate
                     .lines(privateKeyPasswordFilePath)
@@ -75,8 +83,7 @@ public class TomlConfigFactory implements ConfigFactory {
         } else {
             privateKeyPasswords = Collections.unmodifiableList(Collections.EMPTY_LIST);
         }
-       
-        
+
         List<String> alwayssendtoList = toml.getList("alwayssendto", Collections.EMPTY_LIST);
 
         String tlsserverkey = toml.getString("tlsserverkey", "tls-server-key.pem");
@@ -86,12 +93,16 @@ public class TomlConfigFactory implements ConfigFactory {
         String storage = toml.getString("storage", "dir:storage");
 
         //verbosity
-        String tlsservercert = toml.getString("tlsservercert", "tls-server-cert.pem");
+        final String tlsservercert = toml.getString("tlsservercert", "tls-server-cert.pem");
 
-        String tlsservertrust = toml.getString("tlsservertrust", "tofu");
+        final String tlsservertrust = toml.getString("tlsservertrust", "tofu");
 
-        String tlsclienttrust = toml.getString("tlsclienttrust", "ca-or-tofu");
+        final String tlsclienttrust = toml.getString("tlsclienttrust", "ca-or-tofu");
 
+        final String tlsknownservers = toml.getString("tlsknownservers", "tls-known-servers");
+        
+        final String tlsknownclients = toml.getString("tlsknownclients","tls-known-clients");
+        
         ConfigBuilder configBuilder = ConfigBuilder.create()
                 .serverHostname(url)
                 .unixSocketFile(socket)
@@ -99,18 +110,46 @@ public class TomlConfigFactory implements ConfigFactory {
                 .sslServerKeyStorePath(tlsserverkey)
                 .sslServerTrustMode(resolve(tlsservertrust))
                 .sslServerTrustStorePath(tlsservertrust)
-
                 .sslClientTrustMode(resolve(tlsclienttrust))
-                .sslClientKeyStorePath("FIXME")
+                .sslClientKeyStorePath(tlsserverkey)
                 .sslClientKeyStorePassword("FIXME")
-                .sslClientTrustStorePath("FIXME")
-                .knownClientsFile("FIXME")
-                .knownServersFile("FIXME")
+                .sslClientTrustStorePath(tlsservercert)
+                .knownClientsFile(tlsknownclients)
+                .knownServersFile(tlsknownservers)
                 .peers(othernodes);
 
         return configBuilder.build();
     }
 
+    static List<PrivateKeyData> createPrivateKeyData(List<String> privateKeys, List<String> privateKeyPasswords) {
+
+        //Populate null values assume that they arent private
+        List<String> passwordList = new ArrayList<>(privateKeyPasswords);
+        for(int i = privateKeyPasswords.size() - 1; i < privateKeys.size();i++) {
+            passwordList.add(null);
+        }
+
+        List<JsonObject> priavteKeyJson = privateKeys.stream()
+                .map(s -> Paths.get(s))
+                .map(path -> IOCallback.execute(() -> Files.newInputStream(path)))
+                .map(is -> Json.createReader(is))
+                .map(JsonReader::readObject)
+                        .collect(Collectors.toList());
+        
+        
+        List<PrivateKeyData> privateKeyData = IntStream.range(0, priavteKeyJson.size())
+                //FIXME: Canyt set to null value.. need to use addNull("password")
+                .mapToObj(i -> Json.createObjectBuilder(priavteKeyJson.get(i)).add("password",privateKeyPasswords.get(i)).build())
+                .map(JsonObject::toString)
+                .map(String::getBytes)
+                .map(ByteArrayInputStream::new)
+                .map(inputStream -> JaxbUtil.unmarshal(inputStream, PrivateKeyData.class))
+                .collect(Collectors.toList());
+
+        return Collections.unmodifiableList(privateKeyData);
+    }
+
+    //FIXME: CA-OR-TOFU 
     static SslTrustMode resolve(String value) {
         return Stream.of(SslTrustMode.values())
                 .filter(s -> Objects.equals(s.name(), Objects.toString(value).toUpperCase()))
