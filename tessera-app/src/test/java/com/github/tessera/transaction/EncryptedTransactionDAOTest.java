182beab538eb7da5500a6a628bf712ddc7bdf61e
namtruong <nam.p.truong@outlook.com>
2018-07-23 16:18:12 +0100

    add sqlite dependency

--- tessera-app/src/test/java/com/github/tessera/transaction/EncryptedTransactionDAOTest.java
+++ tessera-app/src/test/java/com/github/tessera/transaction/EncryptedTransactionDAOTest.java
@@ -2,6 +2,7 @@ package com.github.tessera.transaction;
 
 import com.github.tessera.dao.JpaH2Config;
 import com.github.tessera.dao.JpaHsqlConfig;
+import com.github.tessera.dao.JpaSqliteConfig;
 import com.github.tessera.enclave.model.MessageHash;
 import com.github.tessera.transaction.model.EncryptedTransaction;
 import org.junit.Test;
@@ -28,7 +29,8 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
     {EncryptedTransactionDAOTest.H2Test.class,
-        EncryptedTransactionDAOTest.HsqlTest.class})
+        EncryptedTransactionDAOTest.HsqlTest.class,
+        EncryptedTransactionDAOTest.SqliteTest.class})
 public class EncryptedTransactionDAOTest {
 
     @Transactional
@@ -36,6 +38,7 @@ public class EncryptedTransactionDAOTest {
     @ContextConfiguration(classes = JpaH2Config.class)
     @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
     public static class H2Test {
+
         @PersistenceContext
         private EntityManager entityManager;
 
@@ -255,6 +258,165 @@ public class EncryptedTransactionDAOTest {
                 .isInstanceOf(PersistenceException.class)
                 .hasMessageContaining("unique constraint or index violation")
                 .hasMessageContaining("ENCRYPTED_TRANSACTION");
+
+        }
+
+        @Test
+        public void validEncryptedTransactionCanBePersisted() {
+
+            final EncryptedTransaction encryptedTransaction = new EncryptedTransaction();
+            encryptedTransaction.setEncodedPayload(new byte[]{5});
+            encryptedTransaction.setHash(new MessageHash(new byte[]{1}));
+            encryptedTransactionDAO.save(encryptedTransaction);
+
+            final EncryptedTransaction retrieved
+                = entityManager.find(EncryptedTransaction.class, encryptedTransaction.getId());
+
+            assertThat(retrieved).isNotNull().isEqualToComparingFieldByField(encryptedTransaction);
+
+        }
+
+        @Test
+        public void fetchingAllTransactionsReturnsAll() {
+
+            final List<EncryptedTransaction> payloads = IntStream.range(0, 50)
+                .mapToObj(i -> new EncryptedTransaction(
+                        new MessageHash(new byte[]{(byte) i}),
+                        new byte[]{(byte) i}
+                    )
+                ).peek(entityManager::persist)
+                .collect(Collectors.toList());
+
+            final List<EncryptedTransaction> retrievedList = encryptedTransactionDAO.retrieveAllTransactions();
+
+            assertThat(retrievedList).hasSameSizeAs(payloads);
+            assertThat(retrievedList).hasSameElementsAs(payloads);
+
+        }
+
+        @Test
+        public void deleteTransactionRemovesFromDatabaseAndReturnsTrue() {
+
+            //put a transaction in the database
+            final EncryptedTransaction encryptedTransaction = new EncryptedTransaction();
+            encryptedTransaction.setEncodedPayload(new byte[]{5});
+            encryptedTransaction.setHash(new MessageHash(new byte[]{1}));
+            encryptedTransactionDAO.save(encryptedTransaction);
+
+            //check that it is actually in the database
+            final EncryptedTransaction retrieved
+                = entityManager.find(EncryptedTransaction.class, encryptedTransaction.getId());
+            assertThat(retrieved).isNotNull();
+
+            //delete the transaction
+            encryptedTransactionDAO.delete(new MessageHash(new byte[]{1}));
+
+            //check it is not longer in the database
+            final EncryptedTransaction deleted
+                = entityManager.find(EncryptedTransaction.class, encryptedTransaction.getId());
+            assertThat(deleted).isNull();
+        }
+
+        @Test(expected = EntityNotFoundException.class)
+        public void deleteThrowsEntityNotFoundExceptionForNonExistentHash() {
+            //delete the transaction
+            encryptedTransactionDAO.delete(new MessageHash(new byte[]{1}));
+        }
+
+        @Test
+        public void retrieveByHashFindsTransactionThatIsPresent() {
+            //put a transaction in the database
+            final EncryptedTransaction encryptedTransaction = new EncryptedTransaction();
+            encryptedTransaction.setEncodedPayload(new byte[]{5});
+            encryptedTransaction.setHash(new MessageHash(new byte[]{1}));
+            encryptedTransactionDAO.save(encryptedTransaction);
+
+            final MessageHash searchHash = new MessageHash(new byte[]{1});
+
+            final Optional<EncryptedTransaction> retrieved = encryptedTransactionDAO.retrieveByHash(searchHash);
+
+            assertThat(retrieved.isPresent()).isTrue();
+            assertThat(retrieved.get()).isEqualToComparingFieldByField(encryptedTransaction);
+        }
+
+        @Test
+        public void retrieveByHashThrowsExceptionWhenNotPresent() {
+            final MessageHash searchHash = new MessageHash(new byte[]{1});
+
+            final Optional<EncryptedTransaction> retrieved = encryptedTransactionDAO.retrieveByHash(searchHash);
+
+            assertThat(retrieved.isPresent()).isFalse();
+        }
+    }
+
+
+    @Transactional
+    @RunWith(SpringRunner.class)
+    @ContextConfiguration(classes = JpaSqliteConfig.class)
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+    public static class SqliteTest {
+
+        @PersistenceContext
+        private EntityManager entityManager;
+
+        @Inject
+        private EncryptedTransactionDAO encryptedTransactionDAO;
+
+        @Test
+        public void saveDoesntAllowNullEncodedPayload() {
+
+            EncryptedTransaction encryptedTransaction = new EncryptedTransaction();
+            encryptedTransaction.setHash(new MessageHash(new byte[]{5}));
+
+            final Throwable throwable = catchThrowable(() -> {
+                encryptedTransactionDAO.save(encryptedTransaction);
+                entityManager.flush();
+            });
+
+            assertThat(throwable)
+                .isInstanceOf(PersistenceException.class)
+                .hasMessageContaining("NOT NULL constraint failed");
+
+        }
+
+        @Test
+        public void saveDoesntAllowNullHash() {
+
+            EncryptedTransaction encryptedTransaction = new EncryptedTransaction();
+            encryptedTransaction.setEncodedPayload(new byte[]{5});
+
+            final Throwable throwable = catchThrowable(() -> {
+                encryptedTransactionDAO.save(encryptedTransaction);
+                entityManager.flush();
+            });
+
+            assertThat(throwable)
+                .isInstanceOf(PersistenceException.class)
+                .hasMessageContaining("NOT NULL constraint failed");
+
+        }
+
+        @Test
+        public void cannotPersistMultipleOfSameHash() {
+
+            final EncryptedTransaction encryptedTransaction = new EncryptedTransaction();
+            encryptedTransaction.setEncodedPayload(new byte[]{5});
+            encryptedTransaction.setHash(new MessageHash(new byte[]{1}));
+            encryptedTransactionDAO.save(encryptedTransaction);
+
+            final EncryptedTransaction duplicateTransaction = new EncryptedTransaction();
+            duplicateTransaction.setEncodedPayload(new byte[]{6});
+            duplicateTransaction.setHash(new MessageHash(new byte[]{1}));
+
+            final Throwable throwable = catchThrowable(() -> {
+                encryptedTransactionDAO.save(duplicateTransaction);
+                entityManager.flush();
+            });
+
+            assertThat(throwable)
+                .isInstanceOf(PersistenceException.class)
+                .hasMessageContaining("UNIQUE constraint failed")
+                .hasMessageContaining("ENCRYPTED_TRANSACTION.HASH");
 
         }
 
