6793556ebb510145c44413ebb9820861b1c762ff
namtruong <nam.p.truong@outlook.com>
2018-07-24 12:08:01 +0100

    add jdbc driver to config - load relevant driver class name based on url

---
+++ config/src/main/java/com/github/tessera/config/util/JdbcDriverClassName.java
@@ -0,0 +1,35 @@
+package com.github.tessera.config.util;
+
+public enum JdbcDriverClassName {
+
+    H2 {
+        @Override
+        public String toString() {
+            return "org.h2.Driver";
+        }
+    },
+
+    HSQL {
+        @Override
+        public String toString() {
+            return "org.hsqldb.jdbc.JDBCDriver";
+        }
+    },
+
+    SQLITE {
+        @Override
+        public String toString() {
+            return "org.sqlite.JDBC";
+        }
+    };
+
+    public static String fromUrl(final String url) {
+        if (url.contains("hsql")) {
+            return HSQL.toString();
+        }
+        if (url.contains("sqlite")) {
+            return SQLITE.toString();
+        }
+        return H2.toString();
+    }
+}
