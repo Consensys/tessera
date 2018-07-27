47bef8074e18bb8466f5eb71c3a1881ecc640384
Mark <melowe.quorum@gmail.com>
2018-07-27 19:51:28 +0100

    Suppress logging and print validation violations to console.

--- config-migration/src/main/java/com/quorum/tessera/config/migration/Main.java
+++ config-migration/src/main/java/com/quorum/tessera/config/migration/Main.java
@@ -1,6 +1,8 @@
 package com.quorum.tessera.config.migration;
 
 import com.quorum.tessera.config.cli.CliResult;
+import com.quorum.tessera.config.util.JaxbUtil;
+import javax.validation.ConstraintViolationException;
 
 public class Main {
     
@@ -8,7 +10,12 @@ public class Main {
         LegacyCliAdapter adapter = new LegacyCliAdapter();
         try {
             CliResult result = adapter.execute(args);
+            result.getConfig().ifPresent(c -> JaxbUtil.marshal(c,  System.out));
             System.exit(result.getStatus());
+ 
+        } catch(ConstraintViolationException ex) {
+            ex.getConstraintViolations().forEach(v -> System.err.println(v));
+            System.exit(1);
         } catch(Exception ex) {
             System.err.println(ex.getMessage());
             System.exit(1);
