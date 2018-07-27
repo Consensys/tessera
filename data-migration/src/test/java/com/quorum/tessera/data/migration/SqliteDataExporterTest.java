5e2925d4887a083906bf11d1d381d07a8e2ef343
namtruong <nam.p.truong@outlook.com>
2018-07-27 12:59:03 +0100

    fix tests for SQliteDataExporter

--- data-migration/src/test/java/com/quorum/tessera/data/migration/SqliteDataExporterTest.java
+++ data-migration/src/test/java/com/quorum/tessera/data/migration/SqliteDataExporterTest.java
@@ -1,6 +1,10 @@
 
 package com.quorum.tessera.data.migration;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,10 +16,8 @@ import java.sql.SQLException;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
 
 
 public class SqliteDataExporterTest {
@@ -50,10 +52,10 @@ public class SqliteDataExporterTest {
 
         try (Connection conn = DriverManager.getConnection(connectionString)) {
             try (ResultSet rs = conn.createStatement().executeQuery("SELECT * FROM ENCRYPTED_TRANSACTION")) {
-
-                assertThat(rs.getLong("ID")).isEqualTo(1);
-                assertThat(rs.getString("HASH")).isEqualTo("HASH");
-                assertThat(rs.getString("ENCODED_PAYLOAD")).isEqualTo("VALUE");
+                while (rs.next()) {
+                    assertThat(rs.getString("HASH")).isEqualTo("HASH");
+                    assertThat(rs.getString("ENCODED_PAYLOAD")).isEqualTo("VALUE");
+                }
 
             }
 
