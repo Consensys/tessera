4820e6a70720b7cf3d4542c4825339a6419e6cea
Mark <melowe.quorum@gmail.com>
2018-07-27 10:53:28 +0100

    inital code for exporting to sqlite and/or h2

---
+++ data-migration/src/main/java/com/quorum/tessera/data/migration/DataExporterException.java
@@ -0,0 +1,11 @@
+
+package com.quorum.tessera.data.migration;
+
+
+public class DataExporterException extends RuntimeException {
+
+    public DataExporterException(Throwable cause) {
+        super(cause);
+    }
+    
+}
--- data-migration/src/main/java/com/quorum/tessera/data/migration/DefaultDataExporter.java
+++ data-migration/src/main/java/com/quorum/tessera/data/migration/DefaultDataExporter.java
@@ -1,14 +1,21 @@
 
 package com.quorum.tessera.data.migration;
 
+import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Map;
 
 
 public class DefaultDataExporter implements DataExporter {
 
     @Override
-    public void export(Map<byte[], byte[]> data) {
+    public void export(Map<byte[], byte[]> data)  throws SQLException,IOException  {
        
+    }
+
+    @Override
+    public String dbId() {
+        return "default";
     }
     
 }
