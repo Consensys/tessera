f0bf11b15092062c4e0852acc86712dd5d5b00df
Mark <melowe.quorum@gmail.com>
2018-07-28 13:47:22 +0100

    Reactive test and capture of constraint violations

--- config-migration/src/main/java/com/quorum/tessera/config/migration/Main.java
+++ config-migration/src/main/java/com/quorum/tessera/config/migration/Main.java
@@ -2,7 +2,7 @@ package com.quorum.tessera.config.migration;
 
 import com.quorum.tessera.config.cli.CliResult;
 import com.quorum.tessera.config.util.JaxbUtil;
-//import javax.validation.ConstraintViolationException;
+import javax.validation.ConstraintViolationException;
 
 public class Main {
     
@@ -12,10 +12,10 @@ public class Main {
             CliResult result = adapter.execute(args);
             result.getConfig().ifPresent(c -> JaxbUtil.marshal(c,  System.out));
             System.exit(result.getStatus());
-// 
-//        } catch(ConstraintViolationException ex) {
-//            ex.getConstraintViolations().forEach(v -> System.err.println(v));
-//            System.exit(2);
+ 
+        } catch(ConstraintViolationException ex) {
+            ex.getConstraintViolations().forEach(v -> System.err.println(v));
+            System.exit(2);
         } catch(Exception ex) {
             System.err.println(ex.getMessage());
             System.exit(1);
