a1e70d5d10c4f4873197b81467b110ef1dd83e8b
Mark <melowe.quorum@gmail.com>
2018-07-30 12:07:51 +0100

    Add support to migrate from sqlite

---
+++ data-migration/src/main/java/com/quorum/tessera/data/migration/StoreLoaderException.java
@@ -0,0 +1,11 @@
+
+package com.quorum.tessera.data.migration;
+
+
+public class StoreLoaderException extends RuntimeException {
+
+    public StoreLoaderException(Throwable cause) {
+        super(cause);
+    }
+    
+}
