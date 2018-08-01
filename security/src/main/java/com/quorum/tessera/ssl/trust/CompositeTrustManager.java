e2bfbde97c848e003bcf1b0e10f119d6b7808ba0
namtruong <nam.p.truong@outlook.com>
2018-08-01 15:20:03 +0100

    add support for legacy ca_or_tofu trust mode

---
+++ security/src/main/java/com/quorum/tessera/ssl/trust/CompositeTrustManager.java
@@ -0,0 +1,56 @@
+package com.quorum.tessera.ssl.trust;
+
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+/**
+ * If the certificate is trusted by one of the given trust managers,
+ * the connection will be accepted.
+ */
+public class CompositeTrustManager extends AbstractTrustManager {
+
+    private TrustManager[] trustManagers;
+
+    public CompositeTrustManager(TrustManager[] trustManagers) {
+        this.trustManagers = trustManagers.clone();
+    }
+
+    @Override
+    public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        for (TrustManager trustManager : trustManagers) {
+            try {
+                X509TrustManager x509TrustManager = (X509TrustManager) trustManager;
+                x509TrustManager.checkClientTrusted(x509Certificates, s);
+                return;
+            }
+            catch(CertificateException ex) {
+                //Ignore and move on to the next trust manager
+            }
+        }
+        throw new CertificateException("Certificate is not trusted by any of the trust managers");
+    }
+
+    @Override
+    public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+        for (TrustManager trustManager : trustManagers) {
+            try {
+                final X509TrustManager x509TrustManager = (X509TrustManager) trustManager;
+                x509TrustManager.checkServerTrusted(x509Certificates, s);
+                return;
+            }
+            catch (CertificateException ex) {
+                //Ignore and move on to the next trust manager
+            }
+        }
+        throw new CertificateException("Certificate is not trusted by any of the trust managers");
+    }
+
+    @Override
+    public X509Certificate[] getAcceptedIssuers() {
+        return new X509Certificate[0];
+    }
+
+
+}
