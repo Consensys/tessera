6866c0f02ba77726bcccb70ae3a853e106d7f443
Mark <melowe_quorum@gmail.com>
2018-08-01 12:24:26 +0100

    Handles simple collections and nested objects. All identified simple types Added util functions for testability and uncheck exceptions. log config for tests

--- config-cli/src/main/java/com/quorum/tessera/config/cli/OverrideUtil.java
+++ config-cli/src/main/java/com/quorum/tessera/config/cli/OverrideUtil.java
@@ -2,11 +2,13 @@ package com.quorum.tessera.config.cli;
 
 import com.quorum.tessera.config.Config;
 import com.quorum.tessera.reflect.ReflectCallback;
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
@@ -14,10 +16,15 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.xml.bind.annotation.XmlElement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface OverrideUtil {
+
+    Logger LOGGER = LoggerFactory.getLogger(OverrideUtil.class);
 
     static Map<String, Class> buildConfigOptions() {
         return fields(null, Config.class);
@@ -46,21 +53,20 @@ public interface OverrideUtil {
         return isSimple(field.getType());
     }
 
+    List<Class> SIMPLE_TYPES = Arrays.asList(String.class,
+            Path.class, Integer.class, Boolean.class, Long.class);
+
     static boolean isSimple(Class type) {
+
+        if (type.isPrimitive()) {
+            return true;
+        }
 
         if (type.getPackage() == null) {
             return true;
         }
 
-        if (type.equals(String.class)) {
-            return true;
-        }
-
-        if (type.equals(Path.class)) {
-            return true;
-        }
-
-        if (type.equals(Integer.class)) {
+        if (SIMPLE_TYPES.contains(type)) {
             return true;
         }
 
@@ -94,12 +100,9 @@ public interface OverrideUtil {
                 Class t = resolveCollectionParameter(field.getGenericType());
 
                 if (isSimple(t)) {
-                    if (t.equals(String.class)) {
-                        list.put(amendedPrefix + resolveName(field), String[].class);
-                    } else if(t.equals(Path.class)) {
-                        list.put(amendedPrefix + resolveName(field), Path[].class);
-                    } 
-                    
+                    final Class arrayType = toArrayType(t);
+                    list.put(amendedPrefix + resolveName(field), arrayType);
+
                 } else {
                     list.putAll(fields(amendedPrefix + resolveName(field), t));
                 }
@@ -108,6 +111,10 @@ public interface OverrideUtil {
 
         return list;
 
+    }
+
+    static <T> Class<T[]> toArrayType(Class<T> t) {
+        return (Class<T[]>) Array.newInstance(t, 0).getClass();
     }
 
     /*
@@ -119,67 +126,138 @@ public interface OverrideUtil {
         final String[] pathTokens = propertyPath.split("\\.");
 
         Object obj = config;
-        for (int i = 0; i < pathTokens.length; i++) {
-            String fieldName = pathTokens[i];
-            obj = checkFieldIsInitialed(obj, fieldName, value).orElse(null);
+        for (String fieldName : pathTokens) {
+            obj = setProperty(obj, fieldName, Arrays.asList(value)).orElse(null);
         }
     }
 
-    /**
-     *
-     * @param <T>
-     * @param obj
-     * @param fieldName
-     * @param value
-     * @return object with fieldName initialised and populated if the target
-     * property
-     */
-    static <T> Optional<T> checkFieldIsInitialed(T obj, String fieldName, String[] value) {
+    static Field resolveField(Class type, String name) {
+        return Stream.of(type.getDeclaredFields())
+                .filter(f -> f.isAnnotationPresent(XmlElement.class))
+                .filter(f -> f.getAnnotation(XmlElement.class).name().equals(name))
+                .findAny()
+                .orElseGet(() -> ReflectCallback.execute(() -> type.getDeclaredField(name)));
+    }
 
-        ReflectCallback<Optional<T>> reflectCallback = () -> {
-            Class type = obj.getClass();
-            List<Field> fields = Arrays.asList(type.getDeclaredFields());
+    static Optional<Object> setProperty(Object obj, String fieldName, List<String> values) {
 
-            Field field = fields
-                    .stream()
-                    .filter(f -> f.isAnnotationPresent(XmlElement.class))
-                    .filter(f -> f.getAnnotation(XmlElement.class).name().equals(fieldName))
-                    .findAny()
-                    .orElseGet(() -> ReflectCallback.execute(() -> type.getDeclaredField(fieldName)));
+        ReflectCallback<Optional<Object>> reflectCallback = () -> {
+            final Class type = obj.getClass();
 
+            LOGGER.debug("Looking up field name {} in object: {}", fieldName, type);
+
+            final Field field = resolveField(type, fieldName);
             field.setAccessible(true);
+
+            LOGGER.debug("Found field name {} from {} in object: {}", field.getName(), fieldName, type);
 
             final Class fieldType = field.getType();
 
+            LOGGER.debug("Resolved field name: {}#{}, type: {}", type, field.getName(), fieldType.getName());
+
             if (isSimple(fieldType)) {
-                if (fieldType.equals(String.class)) {
-                    field.set(obj, value[0]);
+                final String value = values.get(0);
+                if (String.class.isAssignableFrom(fieldType)) {
+                    field.set(obj, value);
+                }
+                if (Integer.class.isAssignableFrom(fieldType)) {
+                    field.set(obj, Integer.valueOf(value));
+                }
+                if (Long.class.isAssignableFrom(fieldType)) {
+                    field.set(obj, Long.valueOf(value));
+                }
+                if (boolean.class.isAssignableFrom(fieldType)) {
+                    field.set(obj, Boolean.valueOf(value));
+                }
+
+                if (Path.class.isAssignableFrom(fieldType)) {
+                    field.set(obj, Paths.get(value));
+                }
+
+                if (fieldType.isEnum()) {
+                    field.set(obj, Enum.valueOf(fieldType, value));
+                }
+
+                return Optional.of(obj);
+            }
+
+            boolean isNestedConfigObject
+                    = fieldType.getPackage().getName().startsWith("com.quorum.tessera");
+
+            if (Collection.class.isAssignableFrom(fieldType)) {
+                final Class genericType = resolveCollectionParameter(field.getGenericType());
+                if (isSimple(genericType)) {
+                    if (String.class.equals(genericType)) {
+                        field.set(obj, values);
+                    }
+                    if (Path.class.equals(genericType)) {
+                        List<Path> paths = values.stream()
+                                .map(s -> Paths.get(s))
+                                .collect(Collectors.toList());
+
+                        field.set(obj, paths);
+                    }
+                } else {
+
+                    
+                    //TODO: Handle collectiosn of nested objects
+                    Object nestedObject = createInstance(genericType);
+                    List<Object> nestedObjects = Arrays.asList(nestedObject);
+                    field.set(obj, nestedObject);
+                    //return 
+  
                 }
                 return Optional.of(obj);
             }
 
-            if (Collection.class.isAssignableFrom(fieldType)) {
-                Class collectionParamType = resolveCollectionParameter(field.getGenericType());
-                if (isSimple(collectionParamType)) {
+            Object existingValue = field.get(obj);
+            boolean isInitialised = Objects.nonNull(existingValue) || isSimple(fieldType);
 
-                    if (String.class.equals(collectionParamType)) {
-                        field.set(obj, Arrays.asList(value));
-                    }
-
-                }
-
+            if (isInitialised && isNestedConfigObject) {
+                LOGGER.debug("Value {}#{} already initialised", type, field.getName());
+                return Optional.of(existingValue);
             }
 
-            if (Objects.isNull(field.get(obj))) {
-                Method createMethod = field.getType().getDeclaredMethod("create");
-                createMethod.setAccessible(true);
-                Object nested = createMethod.invoke(null);
-                field.set(obj, nested);
-                return Optional.of((T) nested);
-            }
+//            if (isInitialised) {
+//                LOGGER.debug("Field {}#{} already populated. ", type, field.getName());
+//                return Optional.of(obj);
+//            }
+//
+//            LOGGER.debug("{} isisNestedConfigObject? {}", fieldType.toString(), isNestedConfigObject);
+//
+//            if (isNestedConfigObject) {
+//                LOGGER.debug("Looking up factory method for {}", fieldType);
+//                Method createMethod = field.getType().getDeclaredMethod("create");
+//                createMethod.setAccessible(true);
+//                LOGGER.debug("Looked up factory method for {}", fieldType);
+//
+//                LOGGER.debug("Creating instance of {}", fieldType);
+//                Object nested = createMethod.invoke(null);
+//
+//                LOGGER.debug("Created instance of {}", fieldType);
+//                field.set(obj, nested);
+//                return Optional.of(nested);
+//            }
             return Optional.empty();
         };
         return ReflectCallback.execute(reflectCallback);
+    }
+
+    static <T> T createInstance(Class<T> type) {
+
+        return ReflectCallback.execute(() -> {
+            Method factoryMethod = type.getDeclaredMethod("create");
+            factoryMethod.setAccessible(true);
+            
+            return (T) factoryMethod.invoke(null);
+        });
+
+
+    }
+    
+    
+    static Class classForName(String classname) {
+     return ReflectCallback.execute(() -> Class.forName(classname));
     }
 
 }
