a01bbc87a4689ee26c41f4bd75ea998302164a94
Mark <melowe.quorum@gmail.com>
2018-08-03 10:39:17 +0100

    Use try-with-resources or close this "AFUNIXServerSocket" in a "finally" clause

--- socket/junixsocket-adapter/src/main/java/com/quorum/tessera/junixsocket/adapter/DefaultUnixSocketFactory.java
+++ socket/junixsocket-adapter/src/main/java/com/quorum/tessera/junixsocket/adapter/DefaultUnixSocketFactory.java
@@ -18,16 +18,18 @@ public class DefaultUnixSocketFactory implements UnixSocketFactory {
 
     @Override
     public ServerSocket createServerSocket(final Path socketFile) throws IOException {
-        final ServerSocket server = AFUNIXServerSocket.newInstance();
-        server.bind(new AFUNIXSocketAddress(socketFile.toFile()));
-        return server;
+        try (final ServerSocket server = AFUNIXServerSocket.newInstance()) {
+            server.bind(new AFUNIXSocketAddress(socketFile.toFile()));
+            return server;
+        }
     }
 
     @Override
     public Socket createSocket(final Path socketFile) throws IOException {
-        final Socket socket = AFUNIXSocket.newInstance();
-        socket.connect(new AFUNIXSocketAddress(socketFile.toFile()));
-        return socket;
+        try (final Socket socket = AFUNIXSocket.newInstance()) {
+            socket.connect(new AFUNIXSocketAddress(socketFile.toFile()));
+            return socket;
+        }
     }
 
 }
