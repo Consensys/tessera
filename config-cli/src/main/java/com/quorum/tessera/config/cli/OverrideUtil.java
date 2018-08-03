a6facd4d585b529569e70b56adde958fbc7fb560
Mark <melowe_quorum@gmail.com>
2018-08-03 11:08:29 +0100

    SONAR: Move the contents of this initializer to a standard constructor or to field initializers. MAke map iunmodifyable, rendering it immutable.

--- config-cli/src/main/java/com/quorum/tessera/config/cli/OverrideUtil.java
+++ config-cli/src/main/java/com/quorum/tessera/config/cli/OverrideUtil.java
@@ -12,6 +12,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
@@ -28,7 +29,7 @@ public interface OverrideUtil {
 
     Logger LOGGER = LoggerFactory.getLogger(OverrideUtil.class);
 
-    Map<Class<?>, Class<?>> PRIMATIVE_LOOKUP = new HashMap<Class<?>, Class<?>>() {
+    Map<Class<?>, Class<?>> PRIMATIVE_LOOKUP = Collections.unmodifiableMap(new HashMap<Class<?>, Class<?>>() {
         {
             put(Boolean.TYPE, Boolean.class);
             put(Byte.TYPE, Byte.class);
@@ -40,7 +41,7 @@ public interface OverrideUtil {
             put(Float.TYPE, Float.class);
             put(Void.TYPE, Void.TYPE);
         }
-    };
+    });
 
     static Map<String, Class> buildConfigOptions() {
         return fields(null, Config.class);
