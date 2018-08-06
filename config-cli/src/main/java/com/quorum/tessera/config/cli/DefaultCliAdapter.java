d6facc8f673e6e125722c03bba2d1ecda939ce04
namtruong <nam.p.truong@outlook.com>
2018-08-06 20:39:27 +0100

    unused import

--- config-cli/src/main/java/com/quorum/tessera/config/cli/DefaultCliAdapter.java
+++ config-cli/src/main/java/com/quorum/tessera/config/cli/DefaultCliAdapter.java
@@ -36,7 +36,6 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
