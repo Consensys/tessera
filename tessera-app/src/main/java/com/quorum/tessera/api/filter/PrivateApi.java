7301d2a35c8a9b31c2cab9c03dac2c4693d5835d
Peter Fox <peter.rd.fox@gmail.com>
2018-08-07 10:59:32 +0100

    Add a filter to reject requests if the remote address is not the same as the one soecified in the configuration

---
+++ tessera-app/src/main/java/com/quorum/tessera/api/filter/PrivateApi.java
@@ -0,0 +1,16 @@
+package com.quorum.tessera.api.filter;
+
+import javax.ws.rs.NameBinding;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A name binding that is to be applied to endpoints under the Private API
+ */
+@NameBinding
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD})
+public @interface PrivateApi {
+}
