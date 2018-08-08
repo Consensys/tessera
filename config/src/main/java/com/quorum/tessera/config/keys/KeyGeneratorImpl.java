83e0a59fcfe82726f22e02dcb368ca899a24504f
Peter Fox <peter.rd.fox@gmail.com>
2018-08-08 15:20:11 +0100

    Add logging output to indicate to the user a private key has been locked (encrypted).

--- config/src/main/java/com/quorum/tessera/config/keys/KeyGeneratorImpl.java
+++ config/src/main/java/com/quorum/tessera/config/keys/KeyGeneratorImpl.java
@@ -6,6 +6,8 @@ import com.quorum.tessera.config.util.JaxbUtil;
 import com.quorum.tessera.config.util.PasswordReader;
 import com.quorum.tessera.nacl.KeyPair;
 import com.quorum.tessera.nacl.NaclFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.file.Files;
@@ -20,6 +22,8 @@ import static com.quorum.tessera.config.PrivateKeyType.UNLOCKED;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class KeyGeneratorImpl implements KeyGenerator {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KeyGeneratorImpl.class);
 
     private static final String EMPTY_FILENAME = "";
 
@@ -76,6 +80,8 @@ public class KeyGeneratorImpl implements KeyGenerator {
                 null,
                 null
             );
+
+            LOGGER.info("Newly generated private key has been encrypted");
 
         } else {
 
