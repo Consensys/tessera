30c0c7f47cbedc80f8d08045857732597ec75396
Mark <melowe.quorum@gmail.com>
2018-08-10 10:01:17 +0100

    Path check group for seelctive validation of paths

---
+++ config/src/main/java/com/quorum/tessera/config/constraints/checks/PathCheck.java
@@ -0,0 +1,10 @@
+package com.quorum.tessera.config.constraints.checks;
+
+/**
+ * 
+ * Java bean validation group/check used to 
+ * apply io validations once a config object has been created. 
+ */
+public interface PathCheck {
+    
+}
