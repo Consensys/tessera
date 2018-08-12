27de0d3809629d7119cbae863c270b7c68dfeb8e
Mark <melowe.quorum@gmail.com>
2018-08-12 08:58:28 +0100

    Validate unixDomainFIle not wait for exception during startup

--- config/src/test/java/com/quorum/tessera/config/ValidationTest.java
+++ config/src/test/java/com/quorum/tessera/config/ValidationTest.java
@@ -1,11 +1,19 @@
 package com.quorum.tessera.config;
 
+import com.quorum.tessera.config.util.FilesDelegate;
+import com.quorum.tessera.config.util.MockFilesDelegate;
+import java.io.UncheckedIOException;
+import java.nio.file.Path;
 import java.util.Set;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class ValidationTest {
 
@@ -43,6 +51,52 @@ public class ValidationTest {
 
         assertThat(violations).hasSize(3);
         assertThat(options.getAlgorithm()).isEqualTo("id");
+
+    }
+
+    @Test
+    public void validUnixDomainPath() {
+
+        Path unixSocketFile = mock(Path.class);
+
+        FilesDelegate filesDelegate = MockFilesDelegate.setUpMock();
+        when(filesDelegate.notExists(unixSocketFile)).thenReturn(true);
+
+        Config config = new Config(null, null, null, null, null, unixSocketFile, true);
+
+        Set<ConstraintViolation<Config>> violations = validator.validateProperty(config, "unixSocketFile");
+
+        assertThat(violations).isEmpty();
+
+        verify(filesDelegate).createFile(unixSocketFile);
+        verify(filesDelegate).deleteIfExists(unixSocketFile);
+        verify(filesDelegate).notExists(unixSocketFile);
+        verifyNoMoreInteractions(filesDelegate);
+        MockFilesDelegate.tearDownMock();
+
+    }
+
+    @Test
+    public void invalidCannotCreateUnixDomainPath() {
+
+        Path unixSocketFile = mock(Path.class);
+
+        FilesDelegate filesDelegate = MockFilesDelegate.setUpMock();
+        when(filesDelegate.notExists(unixSocketFile)).thenReturn(true);
+        when(filesDelegate.createFile(unixSocketFile)).thenThrow(UncheckedIOException.class);
+
+        Config config = new Config(null, null, null, null, null, unixSocketFile, true);
+
+        Set<ConstraintViolation<Config>> violations = validator.validateProperty(config, "unixSocketFile");
+
+        assertThat(violations).hasSize(1);
+        assertThat(violations.iterator().next().getPropertyPath().toString()).isEqualTo("unixSocketFile");
+
+        verify(filesDelegate).createFile(unixSocketFile);
+        verify(filesDelegate).deleteIfExists(unixSocketFile);
+        verify(filesDelegate).notExists(unixSocketFile);
+        verifyNoMoreInteractions(filesDelegate);
+        MockFilesDelegate.tearDownMock();
 
     }
 
