37f33618f173cfcc8ac31608026a6f05ae522324
namtruong <nam.p.truong@outlook.com>
2018-08-14 13:46:39 +0100

    Provide default filename for knownClients and knownServers file if these values are null

--- security/src/test/java/com/quorum/tessera/ssl/context/ClientSSLContextFactoryTest.java
+++ security/src/test/java/com/quorum/tessera/ssl/context/ClientSSLContextFactoryTest.java
@@ -3,9 +3,11 @@ package com.quorum.tessera.ssl.context;
 import com.quorum.tessera.config.SslConfig;
 import com.quorum.tessera.config.SslTrustMode;
 import com.quorum.tessera.ssl.exception.TesseraSecurityException;
+import com.quorum.tessera.ssl.trust.TrustOnFirstUseManager;
 import org.junit.Test;
 
 import javax.net.ssl.SSLContext;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -34,6 +36,27 @@ public class ClientSSLContextFactoryTest {
 
         assertThat(result).isNotNull();
 
+    }
+
+    @Test
+    public void createFromConfigWithDefaultKnownServers() throws URISyntaxException {
+        SslConfig config = mock(SslConfig.class);
+
+        Path keyStore = Paths.get(getClass().getResource("/trust.jks").toURI());
+
+        when(config.getClientTrustMode()).thenReturn(SslTrustMode.TOFU);
+        when(config.getClientKeyStore()).thenReturn(keyStore);
+        when(config.getClientKeyStorePassword()).thenReturn("password");
+        when(config.getKnownServersFile()).thenReturn(null);
+
+        SSLContext result = ClientSSLContextFactory.create().from("localhost",config);
+
+        assertThat(result).isNotNull()
+            .extracting("contextSpi").isNotNull()
+            .extracting("trustManager").isNotNull()
+            .extracting("tm").isNotNull()
+            .hasAtLeastOneElementOfType(TrustOnFirstUseManager.class)
+            .extracting("knownHostsFile").asList().first().isEqualTo(Paths.get("knownServers"));
     }
 
     @Test(expected = TesseraSecurityException.class)
