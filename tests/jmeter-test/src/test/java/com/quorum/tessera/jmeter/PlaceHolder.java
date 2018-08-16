021b79b2e2a53dc385ee75a45b355ea1b45cf9c6
Mark <melowe.quorum@gmail.com>
2018-08-16 14:20:11 +0100

    Add placeholder java file as quick means to ensure that jmeter module is included in reactor when versioning.

--- tests/jmeter-test/src/test/java/com/quorum/tessera/jmeter/PlaceHolder.java
+++ tests/jmeter-test/src/test/java/com/quorum/tessera/jmeter/PlaceHolder.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.quorum.tessera.jmeter;
 
 /**
  *
- * @author mark
+ *Place holder file so the jmeter module is included in maven reactor. 
  */
 public class PlaceHolder {
     
