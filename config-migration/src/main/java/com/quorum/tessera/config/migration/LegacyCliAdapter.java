ad6d6f4a2bd664bfbfd90a9c42f0ba3cb3a6f79d
Mark <melowe.quorum@gmail.com>
2018-08-17 17:00:36 +0100

    Dont mask config for now.

--- config-migration/src/main/java/com/quorum/tessera/config/migration/LegacyCliAdapter.java
+++ config-migration/src/main/java/com/quorum/tessera/config/migration/LegacyCliAdapter.java
@@ -79,7 +79,7 @@ public class LegacyCliAdapter implements CliAdapter {
 
         System.out.printf("Saving config to  %s", outputPath);
         System.out.println();
-        JaxbUtil.marshalMasked(config, System.out);
+        JaxbUtil.marshal(config, System.out);
         System.out.println();
 
         try (OutputStream outputStream = Files.newOutputStream(outputPath)) {
