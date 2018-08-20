ba9059c11b3b3204cbec5a383fe3f409ebe9428a
Mark <melowe.quorum@gmail.com>
2018-08-20 17:11:25 +0100

    Fix typo

--- config/src/main/java/com/quorum/tessera/config/keys/KeysConverter.java
+++ config/src/main/java/com/quorum/tessera/config/keys/KeysConverter.java
@@ -6,15 +6,15 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+public interface KeysConverter {
 
-public interface KeysConverter  {
 
     static List<Key> convert(List<String> values) {
-        return  Objects.requireNonNull(values, "Key values cannot me null")
+        return Objects.requireNonNull(values, "Key values cannot be null")
                 .stream()
                 .map(v -> Base64.getDecoder().decode(v))
                 .map(Key::new)
                 .collect(Collectors.toList());
     }
-    
+
 }
