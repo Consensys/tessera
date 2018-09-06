58ae4a10dbf303d8417c6f32f3a627e8b7ce6711
Mark <melowe.quorum@gmail.com>
2018-09-06 19:10:29 +0100

    Introduce suites for communication types and move process execution into specialised java object rather than doing this in groovy.

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/grpc/SendGrpcIT.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/grpc/SendGrpcIT.java
@@ -5,7 +5,10 @@ import com.quorum.tessera.api.grpc.model.SendRequest;
 import com.quorum.tessera.api.grpc.model.SendResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.failBecauseExceptionWasNotThrown;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -65,44 +68,21 @@ public class SendGrpcIT {
 
     }
 
-//    @Test
-//    public void missingPayloadFails() throws Exception {
-//        
-//        SendRequest request = SendRequest.newBuilder()
-//                .setFrom("/+UuD63zItL1EbjxkKUljMgG8Z1w0AJ8pNOR4iq2yQc=")
-//                .addTo("yGcjkFyZklTTXrn8+WIkYwicA2EGBn9wZFkctAad4X0=")
-//                .build();
-//        try {
-//        SendResponse result = blockingStub.send(request);
-//        } catch(StatusRuntimeException ex) {
-//            ex.getStatus().
-//        }
-//        assertThat(result).isNotNull();
-//        result.getAllFields().forEach((k, v) -> System.out.println(k + " " + v));
-//        assertThat(result.getKey()).isNotNull().isNotBlank();
-//        
-//    }
+    //@Test
+    public void missingPayloadFails() throws Exception {
+        
+        SendRequest request = SendRequest.newBuilder()
+                .setFrom("/+UuD63zItL1EbjxkKUljMgG8Z1w0AJ8pNOR4iq2yQc=")
+                .addTo("yGcjkFyZklTTXrn8+WIkYwicA2EGBn9wZFkctAad4X0=")
+                .build();
+        try {
+            SendResponse result = blockingStub.send(request);
+            failBecauseExceptionWasNotThrown(StatusRuntimeException.class);
+        } catch(StatusRuntimeException ex) {
+            assertThat(ex.getStatus()).isEqualTo(Status.INVALID_ARGUMENT);
+        }
 
-    /*
-        @Test
-    public void missingPayloadFails() {
-
-        final String sendRequest = Json.createObjectBuilder()
-            .add("from", "/+UuD63zItL1EbjxkKUljMgG8Z1w0AJ8pNOR4iq2yQc=")
-            .add("to", Json.createArrayBuilder().add("yGcjkFyZklTTXrn8+WIkYwicA2EGBn9wZFkctAad4X0="))
-            .build().toString();
-
-        LOGGER.info("sendRequest: {}", sendRequest);
-
-        final Response response = client.target(SERVER_URI)
-            .path(SEND_PATH)
-            .request()
-            .post(Entity.entity(sendRequest, MediaType.APPLICATION_JSON));
-
-        //validate result
-
-        assertThat(response).isNotNull();
-        assertThat(response.getStatus()).isEqualTo(400);
+        
     }
-     */
+
 }
