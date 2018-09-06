89b6bb247a5592e4a9e57ea0425848d68c2d6ba8
namtruong <nam.p.truong@outlook.com>
2018-09-06 13:48:25 +0100

    unit test for GrpcClientFactory

---
+++ tessera-app/src/test/java/com/quorum/tessera/node/GrpcClientFactoryTest.java
@@ -0,0 +1,22 @@
+package com.quorum.tessera.node;
+
+import com.quorum.tessera.node.grpc.GrpcClient;
+import com.quorum.tessera.node.grpc.GrpcClientFactory;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GrpcClientFactoryTest {
+
+
+    @Test
+    public void testCreateNewClient() {
+        GrpcClient client1 = GrpcClientFactory.getClient("bogus.com");
+        assertThat(client1).isNotNull();
+        GrpcClient client2 = GrpcClientFactory.getClient("morebogus.com");
+        assertThat(client2).isNotNull();
+
+        assertThat(GrpcClientFactory.getClient("bogus.com")).isEqualTo(client1);
+        assertThat(GrpcClientFactory.getClient("morebogus.com")).isEqualTo(client2);
+    }
+}
