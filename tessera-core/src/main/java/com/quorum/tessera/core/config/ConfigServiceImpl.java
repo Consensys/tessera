beab8fcfb9fd706a77ec585eb4dea70c56af11ec
Mark <melowe.quorum@gmail.com>
2018-09-26 11:20:27 +0100

    Remove volatile key word as we do not need to syncronise access for now.

--- tessera-core/src/main/java/com/quorum/tessera/core/config/ConfigServiceImpl.java
+++ tessera-core/src/main/java/com/quorum/tessera/core/config/ConfigServiceImpl.java
@@ -1,19 +1,20 @@
 package com.quorum.tessera.core.config;
 
 import com.quorum.tessera.config.Config;
+import java.util.Objects;
 
 
 public class ConfigServiceImpl implements ConfigService {
     
-    private volatile Config config;
+    private final Config config;
 
     public ConfigServiceImpl(Config initialConfig) {
-        this.config = initialConfig;
+        this.config = Objects.requireNonNull(initialConfig);
     }
 
     @Override
     public Config getConfig() {
         return config;
     }
-    
+
 }
