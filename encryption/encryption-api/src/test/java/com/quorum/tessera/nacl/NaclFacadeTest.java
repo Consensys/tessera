d17653701a0dff094c2d8a2f76b2350f92a29c08
Mark <melowe.quorum@gmail.com>
2018-10-10 13:59:34 +0100

    Test for default function defined on interface

--- encryption/encryption-api/src/test/java/com/quorum/tessera/nacl/NaclFacadeTest.java
+++ encryption/encryption-api/src/test/java/com/quorum/tessera/nacl/NaclFacadeTest.java
@@ -1,4 +1,3 @@
-
 package com.quorum.tessera.nacl;
 
 import com.quorum.tessera.encryption.KeyPair;
@@ -11,53 +10,63 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.mockito.Mockito.mock;
 
-
 public class NaclFacadeTest {
-    
-    private NaclFacade naclFacade;
 
+    private NaclFacade naclFacade;
 
     @Before
     public void onSetUp() {
         naclFacade = new MockNaclFacade();
     }
-    
-    
+
     @Test
     public void sealAfterPrecomputationWithMasterKey() {
         byte[] message = "MESSAGE".getBytes();
         Nonce nonce = mock(Nonce.class);
         MasterKey masterKey = mock(MasterKey.class);
-        
+
         byte[] outcome = "sealAfterPrecomputationWithSharedKey".getBytes();
 
         byte[] result = naclFacade.sealAfterPrecomputation(message, nonce, masterKey);
-        
+
         assertThat(result).isEqualTo(outcome);
     }
-    
+
+    @Test
+    public void openAfterPrecomputationWithMasterKey() {
+        byte[] message = "MESSAGE".getBytes();
+        Nonce nonce = mock(Nonce.class);
+        MasterKey masterKey = mock(MasterKey.class);
+
+        byte[] outcome = "openAfterPrecomputationWithSharedKey".getBytes();
+
+        byte[] result = naclFacade.openAfterPrecomputation(message, nonce, masterKey);
+
+        assertThat(result).isEqualTo(outcome);
+    }
+
     @Test
     public void createMasterKey() {
-    
+
         MasterKey result = naclFacade.createMasterKey();
         assertThat(result.getKeyBytes()).isEqualTo("createSingleKey".getBytes());
     }
-    
+
     static class MockNaclFacade implements NaclFacade {
 
         @Override
         public SharedKey computeSharedKey(PublicKey publicKey, PrivateKey privateKey) {
-            throw new UnsupportedOperationException("Not supported yet."); 
+            throw new UnsupportedOperationException("Not supported yet.");
         }
 
         @Override
         public byte[] seal(byte[] message, Nonce nonce, PublicKey publicKey, PrivateKey privateKey) {
-            throw new UnsupportedOperationException("Not supported yet."); 
+            throw new UnsupportedOperationException("Not supported yet.");
         }
 
         @Override
         public byte[] open(byte[] cipherText, Nonce nonce, PublicKey publicKey, PrivateKey privateKey) {
-            throw new UnsupportedOperationException("Not supported yet."); 
+            throw new UnsupportedOperationException("Not supported yet.");
         }
 
         @Override
@@ -67,25 +76,24 @@ public class NaclFacadeTest {
 
         @Override
         public byte[] openAfterPrecomputation(byte[] cipherText, Nonce nonce, SharedKey sharedKey) {
-            throw new UnsupportedOperationException("Not supported yet."); 
+            return "openAfterPrecomputationWithSharedKey".getBytes();
         }
 
         @Override
         public Nonce randomNonce() {
-            throw new UnsupportedOperationException("Not supported yet."); 
+            throw new UnsupportedOperationException("Not supported yet.");
         }
 
         @Override
         public KeyPair generateNewKeys() {
-            throw new UnsupportedOperationException("Not supported yet."); 
+            throw new UnsupportedOperationException("Not supported yet.");
         }
 
         @Override
         public SharedKey createSingleKey() {
             return SharedKey.from("createSingleKey".getBytes());
         }
-    
+
     }
-            
-    
+
 }
