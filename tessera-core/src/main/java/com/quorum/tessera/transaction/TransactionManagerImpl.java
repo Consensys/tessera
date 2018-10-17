e6f4ed5e4a91440c54d5eb65ff5dab39c58c8877
Mark <melowe.quorum@gmail.com>
2018-10-17 13:53:57 +0100

    Always included sender key to recipients list ensuring that transaction is always accessible by the sender.

--- tessera-core/src/main/java/com/quorum/tessera/transaction/TransactionManagerImpl.java
+++ tessera-core/src/main/java/com/quorum/tessera/transaction/TransactionManagerImpl.java
@@ -87,7 +87,9 @@ public class TransactionManagerImpl implements TransactionManager {
                 .of(recipients)
                 .map(PublicKey::from)
                 .collect(Collectors.toList());
-
+        
+        recipientList.add(senderPublicKey);
+        
         recipientList.addAll(enclave.getForwardingKeys());
 
         final byte[] payload = sendRequest.getPayload();
@@ -107,7 +109,7 @@ public class TransactionManagerImpl implements TransactionManager {
 
         this.encryptedTransactionDAO.save(newTransaction);
 
-        recipientList.forEach(recipient -> payloadPublisher.publishPayload(encodedPayloadWithRecipients, recipient));
+        recipientList.stream().forEach(recipient -> payloadPublisher.publishPayload(encodedPayloadWithRecipients, recipient));
 
         final byte[] key = transactionHash.getHashBytes();
 
@@ -228,7 +230,7 @@ public class TransactionManagerImpl implements TransactionManager {
                         = payloadEncoder.decodePayloadWithRecipients(encryptedTransaction.getEncodedPayload());
                 enclave.unencryptTransaction(payloadWithRecipients, potentialMatchingKey);
                 return Optional.of(potentialMatchingKey);
-            } catch (final NaclException ex) {
+            } catch (IndexOutOfBoundsException | NaclException ex) {
                 LOGGER.debug("Attempted payload decryption using wrong key, discarding.");
             }
         }
