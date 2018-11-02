2bed2722e83f8a9aa19ddfbd42b80432de9a0e6e
Mark <melowe.quorum@gmail.com>
2018-11-02 11:43:46 +0000

    Happy non exception cases added

--- config/src/test/java/com/quorum/tessera/config/DeprecatedServerConfigTest.java
+++ config/src/test/java/com/quorum/tessera/config/DeprecatedServerConfigTest.java
@@ -1,5 +1,7 @@
 package com.quorum.tessera.config;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Optional;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,12 +13,10 @@ public class DeprecatedServerConfigTest {
     public void createConfigsFromDeprecatedServerConfig() {
 
         DeprecatedServerConfig deprecatedServerConfig = new DeprecatedServerConfig();
-        deprecatedServerConfig.setCommunicationType(CommunicationType.REST);
         deprecatedServerConfig.setHostName("somehost");
         deprecatedServerConfig.setPort(99);
 
         List<ServerConfig> results = DeprecatedServerConfig.from(deprecatedServerConfig, Optional.empty());
-
 
         assertThat(results)
             .hasSize(3)
@@ -27,10 +27,52 @@ public class DeprecatedServerConfigTest {
             .extracting(InetServerSocket.class::cast)
             .allMatch(s -> s.getHostName().equals("somehost"))
             .allMatch(s -> s.getPort() == 99);
-        
-        
-        
-        
-    }
 
+    }
+    
+    
+    @Test
+    public void createConfigsFromDeprecatedServerConfigWithGprcPort() {
+
+        DeprecatedServerConfig deprecatedServerConfig = new DeprecatedServerConfig();
+        deprecatedServerConfig.setHostName("somehost");
+        deprecatedServerConfig.setGrpcPort(99);
+
+        List<ServerConfig> results = DeprecatedServerConfig.from(deprecatedServerConfig, Optional.empty());
+
+
+        assertThat(results)
+            .hasSize(3)
+            .allMatch(s -> s.getCommunicationType() == CommunicationType.GRPC)
+            .allMatch(ServerConfig::isEnabled)
+            .extracting(ServerConfig::getServerSocket)
+            .allMatch(InetServerSocket.class::isInstance)
+            .extracting(InetServerSocket.class::cast)
+            .allMatch(s -> s.getHostName().equals("somehost"))
+            .allMatch(s -> s.getPort() == 99);
+
+    }
+    
+    
+        @Test
+    public void createConfigsFromDeprecatedServerConfigWithUnixSocketFile() {
+
+        DeprecatedServerConfig deprecatedServerConfig = new DeprecatedServerConfig();
+        
+        Path unixSocketFile = Paths.get("unixSocketFile");
+        
+        List<ServerConfig> results = DeprecatedServerConfig.from(deprecatedServerConfig, 
+            Optional.of(unixSocketFile));
+
+
+        assertThat(results)
+            .hasSize(3)
+            .allMatch(s -> s.getCommunicationType() == CommunicationType.UNIX_SOCKET)
+            .allMatch(ServerConfig::isEnabled)
+            .extracting(ServerConfig::getServerSocket)
+            .allMatch(UnixServerSocket.class::isInstance)
+            .extracting(UnixServerSocket.class::cast)
+            .allMatch(s -> s.getPath().equals("unixSocketFile"));
+
+    }
 }
