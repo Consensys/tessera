5fee10ffec83389f37915a012a0edec7edf192ad
Mark <melowe.quorum@gmail.com>
2018-11-15 14:46:10 +0000

    Fix for Unauthenticated Denial of Service #57

---
+++ tessera-core/src/main/java/com/quorum/tessera/node/PartyInfoParserException.java
@@ -0,0 +1,13 @@
+
+package com.quorum.tessera.node;
+
+import com.quorum.tessera.exception.TesseraException;
+
+
+public class PartyInfoParserException extends TesseraException {
+
+    public PartyInfoParserException(String message) {
+        super(message);
+    }
+    
+}
