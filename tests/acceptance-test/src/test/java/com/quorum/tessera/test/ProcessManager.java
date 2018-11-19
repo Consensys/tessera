8d6fa2a2370c183da4e13c95c6024f9e6a306ee5
Mark <melowe.quorum@gmail.com>
2018-11-19 14:12:48 +0000

    Check url connection to assume startup state. for both grpc and rest endpoints

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/ProcessManager.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/ProcessManager.java
@@ -1,6 +1,8 @@
 package com.quorum.tessera.test;
 
 import com.quorum.tessera.config.CommunicationType;
+import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.util.JaxbUtil;
 import com.quorum.tessera.io.FilesDelegate;
 import com.quorum.tessera.test.util.ElUtil;
 
@@ -9,6 +11,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UncheckedIOException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -24,6 +27,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
+import javax.ws.rs.core.UriBuilder;
 
 public class ProcessManager {
 
@@ -52,13 +56,13 @@ public class ProcessManager {
 
     public String findJarFilePath() {
         return Objects.requireNonNull(System.getProperty("application.jar", null),
-            "System property application.jar is undefined.");
+                "System property application.jar is undefined.");
     }
 
     public void startNodes() throws Exception {
         List<String> nodeAliases = Arrays.asList(configFiles.keySet().toArray(new String[0]));
         Collections.shuffle(nodeAliases);
-        
+
         for (String nodeAlias : nodeAliases) {
             start(nodeAlias);
         }
@@ -69,16 +73,16 @@ public class ProcessManager {
         args.add("kill");
 
         pids.values().stream()
-            .flatMap(p -> {
-                try {
-                    return Files.readAllLines(p).stream();
-                } catch (IOException ex) {
-                    throw new UncheckedIOException(ex);
-                }
-            })
-            .filter(Objects::nonNull)
-            .filter(s -> !Objects.equals("", s))
-            .forEach(args::add);
+                .flatMap(p -> {
+                    try {
+                        return Files.readAllLines(p).stream();
+                    } catch (IOException ex) {
+                        throw new UncheckedIOException(ex);
+                    }
+                })
+                .filter(Objects::nonNull)
+                .filter(s -> !Objects.equals("", s))
+                .forEach(args::add);
 
         ProcessBuilder processBuilder = new ProcessBuilder(args);
         Process process = processBuilder.start();
@@ -92,23 +96,23 @@ public class ProcessManager {
 
         URL configFile = configFiles.get(nodeAlias);
         Path pid = Paths.get(System.getProperty("java.io.tmpdir"), "pid" + nodeAlias + ".pid");
-        
+
         pids.put(nodeAlias, pid);
-        
+
         List<String> args = Arrays.asList(
-            "java",
-            "-Dspring.profiles.active=disable-unixsocket,disable-sync-poller",
-            "-Dnode.number=" + nodeAlias,
-            "-Dlogback.configurationFile=" + logbackConfigFile.getFile(),
-            "-Ddebug=true",
-            "-jar",
-            jarfile,
-            "-configfile",
-            ElUtil.createAndPopulatePaths(configFile).toAbsolutePath().toString(),
-            "-pidfile",
-            pid.toAbsolutePath().toString(),
-            "-server.communicationType",
-            communicationType.name()
+                "java",
+                "-Dspring.profiles.active=disable-unixsocket,disable-sync-poller",
+                "-Dnode.number=" + nodeAlias,
+                "-Dlogback.configurationFile=" + logbackConfigFile.getFile(),
+                "-Ddebug=true",
+                "-jar",
+                jarfile,
+                "-configfile",
+                ElUtil.createAndPopulatePaths(configFile).toAbsolutePath().toString(),
+                "-pidfile",
+                pid.toAbsolutePath().toString(),
+                "-server.communicationType",
+                communicationType.name()
         );
         System.out.println(String.join(" ", args));
 
@@ -119,9 +123,9 @@ public class ProcessManager {
         executorService.submit(() -> {
 
             try (BufferedReader reader = Stream.of(process.getInputStream())
-                .map(InputStreamReader::new)
-                .map(BufferedReader::new)
-                .findAny().get()) {
+                    .map(InputStreamReader::new)
+                    .map(BufferedReader::new)
+                    .findAny().get()) {
 
                 String line = null;
                 while ((line = reader.readLine()) != null) {
@@ -133,21 +137,74 @@ public class ProcessManager {
             }
         });
 
-        CountDownLatch startUpLatch = new CountDownLatch(1);
         final String nodeId = nodeAlias;
+
+        final Config config = JaxbUtil.unmarshal(configFile.openStream(), Config.class);
+
+        final URL bindingUrl = config.getServerConfig().getBindingUri().toURL();
+
+        CountDownLatch startUpLatch = new CountDownLatch(communicationType == CommunicationType.GRPC ? 2 : 1);
+
+        if (communicationType == CommunicationType.GRPC) {
+            URL grpcUrl = UriBuilder.fromUri(config.getServerConfig().getBindingUri())
+                    .port(config.getServerConfig().getGrpcPort())
+                    .build().toURL();
+
+            executorService.submit(() -> {
+
+                while (true) {
+                    try {
+                        URLConnection conn = grpcUrl.openConnection();
+                        conn.connect();
+                        System.out.println(grpcUrl + " started. ");
+                        startUpLatch.countDown();
+                        return;
+                    } catch (IOException ex) {
+                        try {
+                            TimeUnit.MILLISECONDS.sleep(200L);
+                        } catch (InterruptedException ex1) {
+                        }
+                    }
+                }
+
+            });
+        }
+
+        executorService.submit(() -> {
+
+            while (true) {
+                try {
+                    URLConnection conn = bindingUrl.openConnection();
+                    conn.connect();
+                    System.out.println(bindingUrl + " started. ");
+                    startUpLatch.countDown();
+                    return;
+                } catch (IOException ex) {
+                    try {
+                        TimeUnit.MILLISECONDS.sleep(200L);
+                    } catch (InterruptedException ex1) {
+                    }
+                }
+            }
+
+        });
+
+        boolean started = startUpLatch.await(2, TimeUnit.MINUTES);
+
+        if (!started) {
+            System.err.println(bindingUrl + " Not started. ");
+        }
+
         executorService.submit(() -> {
             try {
                 int exitCode = process.waitFor();
                 if (0 != exitCode) {
                     System.err.println("Node " + nodeId + " exited with code " + exitCode);
                 }
-                startUpLatch.countDown();
             } catch (InterruptedException ex) {
                 ex.printStackTrace();
             }
         });
-
-        startUpLatch.await(15, TimeUnit.SECONDS);
 
     }
 
@@ -167,7 +224,7 @@ public class ProcessManager {
     }
 
     public static void main(String[] args) throws Exception {
-        System.setProperty("application.jar", "/Users/mark/Projects/tessera/tessera-app/target/tessera-app-0.7-SNAPSHOT-app.jar");
+        System.setProperty("application.jar", "/Users/mark/Projects/tessera/tessera-app/target/tessera-app-0.8-SNAPSHOT-app.jar");
 
         ProcessManager pm = new ProcessManager(CommunicationType.REST);
         pm.startNodes();
