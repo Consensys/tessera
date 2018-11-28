d54a432a6778a5a41d79d7ee62f8a2e8489a6c8c
chris-j-h <chris.hounsom@gmail.com>
2018-11-28 11:59:43 +0000

    Add validation to require Hashicorp config if providing Hashicorp keys

--- config/src/main/java/com/quorum/tessera/config/constraints/KeyVaultConfigurationValidator.java
+++ config/src/main/java/com/quorum/tessera/config/constraints/KeyVaultConfigurationValidator.java
@@ -2,6 +2,7 @@ package com.quorum.tessera.config.constraints;
 
 import com.quorum.tessera.config.KeyConfiguration;
 import com.quorum.tessera.config.keypairs.AzureVaultKeyPair;
+import com.quorum.tessera.config.keypairs.HashicorpVaultKeyPair;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -28,6 +29,22 @@ public class KeyVaultConfigurationValidator implements ConstraintValidator<Valid
             .anyMatch(keyPair -> keyPair instanceof AzureVaultKeyPair);
 
         if(isUsingAzureVaultKeys && keyConfiguration.getAzureKeyVaultConfig() == null) {
+            cvc.disableDefaultConstraintViolation();
+            cvc.buildConstraintViolationWithTemplate("{ValidKeyVaultConfiguration.azure.message}")
+                .addConstraintViolation();
+
+            return false;
+        }
+
+        boolean isUsingHashicorpVaultKeys = keyConfiguration.getKeyData()
+            .stream()
+            .anyMatch(keyPair -> keyPair instanceof HashicorpVaultKeyPair);
+
+        if(isUsingHashicorpVaultKeys && keyConfiguration.getHashicorpKeyVaultConfig() == null) {
+            cvc.disableDefaultConstraintViolation();
+            cvc.buildConstraintViolationWithTemplate("{ValidKeyVaultConfiguration.hashicorp.message}")
+                .addConstraintViolation();
+
             return false;
         }
 
--- config/src/test/java/com/quorum/tessera/config/ValidationTest.java
+++ config/src/test/java/com/quorum/tessera/config/ValidationTest.java
@@ -1,10 +1,6 @@
 package com.quorum.tessera.config;
 
-import com.quorum.tessera.config.keypairs.AzureVaultKeyPair;
-import com.quorum.tessera.config.keypairs.ConfigKeyPair;
-import com.quorum.tessera.config.keypairs.DirectKeyPair;
-import com.quorum.tessera.config.keypairs.FilesystemKeyPair;
-import com.quorum.tessera.config.keypairs.InlineKeypair;
+import com.quorum.tessera.config.keypairs.*;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -275,11 +271,70 @@ public class ValidationTest {
         assertThat(violations).hasSize(1);
 
         ConstraintViolation<Config> violation = violations.iterator().next();
-        assertThat(violation.getMessageTemplate()).isEqualTo("{ValidKeyVaultConfiguration.message}");
+        assertThat(violation.getMessageTemplate()).isEqualTo("{ValidKeyVaultConfiguration.azure.message}");
     }
 
     @Test
-    public void nonKeyVaultPairProvidedWithoutKeyVaultConfigDoesNotCreateViolation() {
+    public void azureKeyPairProvidedWithHashicorpKeyVaultConfigCreatesViolation() {
+        AzureVaultKeyPair keyPair = new AzureVaultKeyPair("publicVauldId", "privateVaultId");
+
+        KeyConfiguration keyConfiguration = new KeyConfiguration();
+        keyConfiguration.setKeyData(singletonList(keyPair));
+
+        HashicorpKeyVaultConfig hashicorpConfig = new HashicorpKeyVaultConfig();
+        keyConfiguration.setHashicorpKeyVaultConfig(hashicorpConfig);
+
+        Config config = new Config();
+        config.setKeys(keyConfiguration);
+
+        Set<ConstraintViolation<Config>> violations = validator.validateProperty(config, "keys");
+        assertThat(violations).hasSize(1);
+
+        ConstraintViolation<Config> violation = violations.iterator().next();
+        assertThat(violation.getMessageTemplate()).isEqualTo("{ValidKeyVaultConfiguration.azure.message}");
+    }
+
+    @Test
+    public void hashicorpKeyPairProvidedWithoutKeyVaultConfigCreatesViolation() {
+        HashicorpVaultKeyPair keyPair = new HashicorpVaultKeyPair("pubId", "privdId", "secretPath");
+
+        KeyConfiguration keyConfiguration = new KeyConfiguration();
+        keyConfiguration.setKeyData(singletonList(keyPair));
+        keyConfiguration.setHashicorpKeyVaultConfig(null);
+
+        Config config = new Config();
+        config.setKeys(keyConfiguration);
+
+        Set<ConstraintViolation<Config>> violations = validator.validateProperty(config, "keys");
+        assertThat(violations).hasSize(1);
+
+        ConstraintViolation<Config> violation = violations.iterator().next();
+        assertThat(violation.getMessageTemplate()).isEqualTo("{ValidKeyVaultConfiguration.hashicorp.message}");
+    }
+
+    @Test
+    public void hashicorpKeyPairProvidedWithAzureKeyVaultConfigCreatesViolation() {
+        HashicorpVaultKeyPair keyPair = new HashicorpVaultKeyPair("pubId", "privdId", "secretPath");
+
+        KeyConfiguration keyConfiguration = new KeyConfiguration();
+        keyConfiguration.setKeyData(singletonList(keyPair));
+        keyConfiguration.setHashicorpKeyVaultConfig(null);
+
+        AzureKeyVaultConfig azureConfig = new AzureKeyVaultConfig();
+        keyConfiguration.setAzureKeyVaultConfig(azureConfig);
+
+        Config config = new Config();
+        config.setKeys(keyConfiguration);
+
+        Set<ConstraintViolation<Config>> violations = validator.validateProperty(config, "keys");
+        assertThat(violations).hasSize(1);
+
+        ConstraintViolation<Config> violation = violations.iterator().next();
+        assertThat(violation.getMessageTemplate()).isEqualTo("{ValidKeyVaultConfiguration.hashicorp.message}");
+    }
+
+    @Test
+    public void nonKeyVaultPairProvidedWithoutAzureAndHashicorpKeyVaultConfigDoesNotCreateViolation() {
         DirectKeyPair keyPair = new DirectKeyPair("pub", "priv");
 
         KeyConfiguration keyConfiguration = new KeyConfiguration(null, null, singletonList(keyPair), null, null);
@@ -288,7 +343,6 @@ public class ValidationTest {
         Set<ConstraintViolation<Config>> violations = validator.validateProperty(config, "keys");
         assertThat(violations).hasSize(0);
     }
-
 
     @Test
     public void keyConfigurationIsNullCreatesNotNullViolation() {
@@ -314,7 +368,7 @@ public class ValidationTest {
     }
 
     @Test
-    public void azureVaultKeyPairProvidedButKeyVaultConfigHasNullUrlCreatesNullViolation() {
+    public void azureVaultKeyPairProvidedButKeyVaultConfigHasNullUrlCreatesNotNullViolation() {
         AzureVaultKeyPair keyPair = new AzureVaultKeyPair("pubId", "privId");
         AzureKeyVaultConfig keyVaultConfig = new AzureKeyVaultConfig(null);
         KeyConfiguration keyConfiguration = new KeyConfiguration(null, null, singletonList(keyPair), keyVaultConfig, null);
@@ -325,5 +379,30 @@ public class ValidationTest {
         ConstraintViolation<KeyConfiguration> violation = violations.iterator().next();
         assertThat(violation.getMessageTemplate()).isEqualTo("{javax.validation.constraints.NotNull.message}");
         assertThat(violation.getPropertyPath().toString()).isEqualTo("azureKeyVaultConfig.url");
+    }
+
+    @Test
+    public void hashicorpVaultConfigWithNoUrlCreatesNotNullViolation() {
+        HashicorpKeyVaultConfig keyVaultConfig = new HashicorpKeyVaultConfig(null);
+
+        Set<ConstraintViolation<HashicorpKeyVaultConfig>> violations = validator.validate(keyVaultConfig);
+        assertThat(violations).hasSize(1);
+
+        ConstraintViolation<HashicorpKeyVaultConfig> violation = violations.iterator().next();
+        assertThat(violation.getMessageTemplate()).isEqualTo("{javax.validation.constraints.NotNull.message}");
+    }
+
+    @Test
+    public void hashicorpVaultKeyPairProvidedButKeyVaultConfigHasNullUrlCreatesNotNullViolation() {
+        HashicorpVaultKeyPair keyPair = new HashicorpVaultKeyPair("pubId", "privId", "secretPath");
+        HashicorpKeyVaultConfig keyVaultConfig = new HashicorpKeyVaultConfig(null);
+        KeyConfiguration keyConfiguration = new KeyConfiguration(null, null, singletonList(keyPair), null, keyVaultConfig);
+
+        Set<ConstraintViolation<KeyConfiguration>> violations = validator.validate(keyConfiguration);
+        assertThat(violations).hasSize(1);
+
+        ConstraintViolation<KeyConfiguration> violation = violations.iterator().next();
+        assertThat(violation.getMessageTemplate()).isEqualTo("{javax.validation.constraints.NotNull.message}");
+        assertThat(violation.getPropertyPath().toString()).isEqualTo("hashicorpKeyVaultConfig.url");
     }
 }
