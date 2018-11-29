9a1db3a96866d2e305a07640a3063dc43fe68b2a
Mark <melowe.quorum@gmail.com>
2018-11-29 17:20:20 +0000

    attach ddl create scripts as a dependency so can me used in acceptance tests to setup data. Stop creating tables using jpa. acceptance tests unpacks ddls and use PartyHelper uses script to create tables.

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/PartyHelper.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/PartyHelper.java
@@ -1,5 +1,12 @@
 package com.quorum.tessera.test;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public interface PartyHelper {
@@ -20,5 +27,31 @@ public interface PartyHelper {
             .orElseThrow(() -> new RuntimeException("No party found with publicKey " + publicKey));
 
     }
+    
+    static void initialiseData(List<Party> parties) {
+        
+       String createTableSql = Stream.of(PartyHelper.class.getResourceAsStream("/ddls/h2-ddl.sql"))
+               .map(InputStreamReader::new)
+               .map(BufferedReader::new)
+               .flatMap(BufferedReader::lines)
+               .collect(Collectors.joining(System.lineSeparator()));
+
+         parties.forEach(party -> {
+        
+            try (Connection conn = party.getDatabaseConnection()) {
+                try(Statement statement = conn.createStatement()) {
+                    statement.execute(createTableSql);
+                }
+            } catch(SQLException ex) {
+                //IGNORE Assume table already exists
+          //      throw new UncheckedSQLException(ex);
+            }
+        });
+        
+                
+        
+        
+    }
+    
 
 }
