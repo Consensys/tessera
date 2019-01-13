ddaad0d2507f5cd40fc9dec2c8d3e267cd28ead1
Peter Fox <peter.rd.fox@gmail.com>
2019-01-13 00:22:37 +0000

    Remove duplicated files Remove redundant methods & associated tests Streamline existing methods

--- encryption/encryption-api/src/main/java/com/quorum/tessera/encryption/PayloadEncoderImpl.java
+++ encryption/encryption-api/src/main/java/com/quorum/tessera/encryption/PayloadEncoderImpl.java
@@ -1,90 +1,38 @@
 package com.quorum.tessera.encryption;
 
-
 import com.quorum.tessera.nacl.Nonce;
+
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
-import static java.util.Collections.emptyList;
-import static java.util.Collections.singletonList;
 import java.util.List;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
 import static java.util.stream.Collectors.toList;
 
 public class PayloadEncoderImpl implements PayloadEncoder, BinaryEncoder {
 
     @Override
-    public byte[] encode(final EncodedPayload encodedPayload) {
-        final byte[] senderKey = encodeField(encodedPayload.getSenderKey().getKeyBytes());
-        final byte[] cipherText = encodeField(encodedPayload.getCipherText());
-        final byte[] nonce = encodeField(encodedPayload.getCipherTextNonce().getNonceBytes());
-        final byte[] recipientNonce = encodeField(encodedPayload.getRecipientNonce().getNonceBytes());
-        final byte[] recipients = encodeArray(encodedPayload.getRecipientBoxes());
+    public byte[] encode(final EncodedPayloadWithRecipients outer) {
+        final EncodedPayload inner = outer.getEncodedPayload();
 
-        return ByteBuffer.allocate(senderKey.length + cipherText.length + nonce.length + recipients.length + recipientNonce.length)
-                .put(senderKey)
-                .put(cipherText)
-                .put(nonce)
-                .put(recipients)
-                .put(recipientNonce)
-                .array();
-    }
+        final byte[] senderKey = encodeField(inner.getSenderKey().getKeyBytes());
+        final byte[] cipherText = encodeField(inner.getCipherText());
+        final byte[] nonce = encodeField(inner.getCipherTextNonce().getNonceBytes());
+        final byte[] recipientNonce = encodeField(inner.getRecipientNonce().getNonceBytes());
+        final byte[] recipients = encodeArray(inner.getRecipientBoxes());
+        final byte[] recipientBytes
+            = encodeArray(outer.getRecipientKeys().stream().map(PublicKey::getKeyBytes).collect(toList()));
 
-    @Override
-    public EncodedPayload decode(final byte[] input) {
-        final ByteBuffer buffer = ByteBuffer.wrap(input);
-
-        final long senderSize = buffer.getLong();
-        final byte[] senderKey = new byte[Math.toIntExact(senderSize)];
-        buffer.get(senderKey);
-
-        final long cipherTextSize = buffer.getLong();
-        final byte[] cipherText = new byte[Math.toIntExact(cipherTextSize)];
-        buffer.get(cipherText);
-
-        final long nonceSize = buffer.getLong();
-        final byte[] nonce = new byte[Math.toIntExact(nonceSize)];
-        buffer.get(nonce);
-
-        final long numberOfRecipients = buffer.getLong();
-        final List<byte[]> recipientBoxes = new ArrayList<>();
-        for (long i = 0; i < numberOfRecipients; i++) {
-            final long boxSize = buffer.getLong();
-            final byte[] box = new byte[Math.toIntExact(boxSize)];
-            buffer.get(box);
-            recipientBoxes.add(box);
-        }
-
-        final long recipientNonceSize = buffer.getLong();
-        final byte[] recipientNonce = new byte[Math.toIntExact(recipientNonceSize)];
-        buffer.get(recipientNonce);
-
-        return new EncodedPayload(
-                PublicKey.from(senderKey),
-                cipherText,
-                new Nonce(nonce),
-                recipientBoxes,
-                new Nonce(recipientNonce)
-        );
-    }
-
-    @Override
-    public byte[] encode(final EncodedPayloadWithRecipients encodedPayloadWithRecipients) {
-        final byte[] payloadBytes = encode(encodedPayloadWithRecipients.getEncodedPayload());
-
-        final List<byte[]> keysAsBytes = encodedPayloadWithRecipients
-                .getRecipientKeys()
-                .stream()
-                .map(PublicKey::getKeyBytes)
-                .collect(toList());
-
-        final byte[] recipientBytes = encodeArray(keysAsBytes);
-
-        final byte[] returnValue = new byte[payloadBytes.length + recipientBytes.length];
-
-        System.arraycopy(payloadBytes, 0, returnValue, 0, payloadBytes.length);
-        System.arraycopy(recipientBytes, 0, returnValue, payloadBytes.length, recipientBytes.length);
-
-        return returnValue;
+        return ByteBuffer
+            .allocate(senderKey.length + cipherText.length + nonce.length + recipients.length + recipientNonce.length + recipientBytes.length)
+            .put(senderKey)
+            .put(cipherText)
+            .put(nonce)
+            .put(recipients)
+            .put(recipientNonce)
+            .put(recipientBytes)
+            .array();
     }
 
     @Override
@@ -117,11 +65,11 @@ public class PayloadEncoderImpl implements PayloadEncoder, BinaryEncoder {
         buffer.get(recipientNonce);
 
         EncodedPayload payload = new EncodedPayload(
-                PublicKey.from(senderKey),
-                cipherText,
-                new Nonce(nonce),
-                recipientBoxes,
-                new Nonce(recipientNonce)
+            PublicKey.from(senderKey),
+            cipherText,
+            new Nonce(nonce),
+            recipientBoxes,
+            new Nonce(recipientNonce)
         );
 
         //this means there are no recipients in the payload (which we receive when we are a participant)
@@ -139,12 +87,7 @@ public class PayloadEncoderImpl implements PayloadEncoder, BinaryEncoder {
             recipientKeys.add(box);
         }
 
-        return new EncodedPayloadWithRecipients(
-                payload,
-                recipientKeys.stream()
-                        .map(PublicKey::from)
-                        .collect(toList())
-        );
+        return new EncodedPayloadWithRecipients(payload, recipientKeys.stream().map(PublicKey::from).collect(toList()));
     }
 
     @Override
