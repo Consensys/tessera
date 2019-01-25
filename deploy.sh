0130be6e29599dd82f6b989ef84e0efc6d49f567
Mark <melowe.quorum@gmail.com>
2019-01-25 11:02:24 +0000

    assume that switcher default is the version we want to use for deploying and releasing

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,27 @@ echo "TRAVIS_PULL_REQUEST $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_BUILD_NUMBER $TRAVIS_BUILD_NUMBER"
 echo "TRAVIS_BUILD_STAGE_NAME $TRAVIS_BUILD_STAGE_NAME"
 echo "TRAVIS_JDK_VERSION $TRAVIS_JDK_VERSION"
-echi "JDK_SWITCHER_DEFAULT $JDK_SWITCHER_DEFAULT"
+echo "JDK_SWITCHER_DEFAULT $JDK_SWITCHER_DEFAULT"
 
+defaultJdk=${JDK_SWITCHER_DEFAULT:-"undefined"}
+travisJdk=${TRAVIS_JDK_VERSION:-"undefined"}
 
-if[ $TRAVIS_JDK_VERSION = $JDK_SWITCHER_DEFAULT ]
-then 
-    echo "JAVA8"
-else 
-    echo "NOT JAVA8"
+if [ "${defaultJdk}" == "undefined" ] 
+then
+echo "No JDK_SWITCHER_DEFAULT environment variable found"
+exit 1
 fi
+
+if [ "${travisJdk}" == "undefined" ] 
+then
+echo "No TRAVIS_JDK_VERSION environment variable found"
+exit 1
+fi
+
+if [ "${defaultJdk}" != "${travisJdk}" ]
+then
+    echo "Ignoring deploy for ${travisJdk}"
+    exit 0
+fi
+
+# Use branch name, job name and event type to infer if release or a snapshot deploy
