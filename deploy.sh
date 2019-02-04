61f1cc942453319d8bdc295f62846e88ee5f144f
SatpalSandhu61 <quorum@satpal.co.uk>
2019-02-04 13:21:49 +0000

    Writing deploy script for performing releases via travis.

--- deploy.sh
+++ deploy.sh
@@ -29,14 +29,14 @@ travisJdk=${TRAVIS_JDK_VERSION:-"undefined"}
 
 if [ "${defaultJdk}" == "undefined" ] 
 then
-echo "No JDK_SWITCHER_DEFAULT environment variable found"
-exit 1
+    echo "No JDK_SWITCHER_DEFAULT environment variable found"
+    exit 1
 fi
 
-if [ "${travisJdk}" == "undefined" ] 
+if [ "${travisJdk}" == "undefined" ]
 then
-echo "No TRAVIS_JDK_VERSION environment variable found"
-exit 1
+    echo "No TRAVIS_JDK_VERSION environment variable found"
+    exit 1
 fi
 
 if [ "${defaultJdk}" != "${travisJdk}" ]
@@ -45,4 +45,18 @@ then
     exit 0
 fi
 
-# Use branch name, job name and event type to infer if release or a snapshot deploy
+# Use branch name, job name and event type to infer whether release or a snapshot deploy
+if [ "$TRAVIS_EVENT_TYPE" == "api" ] && [ "$TRAVIS_JOB_NAME" == "release" ]; then
+    echo "Performing release build"
+    #TODO: should ideally extract the version number from the pom and use that in the release branch name
+    branch_name=`date +"release-%H%M%S-%d%m%y"`
+    mvn -B release:branch -DbranchName=${branch_name}
+    mvn -B -DpushChanges=false release:prepare release:perform
+    echo "TODO: The release branch must be manually merged back to master. (This could be automated in the future.)"
+
+elif [ "$TRAVIS_EVENT_TYPE" == "push" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "Deploying snapshot release to central"
+    mvn deploy -P release
+fi
+
+exit 0
