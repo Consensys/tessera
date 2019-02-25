3e71f791730e179f98320c95dda0f319bd22afac
Mark <melowe.quorum@gmail.com>
2019-02-25 12:54:20 +0000

    Deprecate UNIX_SOCKET as communication type as its a transport rather than protocol. Added log warning when used to create server. Delete unixsocket server module.

--- server/server-api/src/main/java/com/quorum/tessera/server/TesseraServerFactory.java
+++ server/server-api/src/main/java/com/quorum/tessera/server/TesseraServerFactory.java
@@ -7,13 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.Set;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public interface TesseraServerFactory<T> {
 
     TesseraServer createServer(ServerConfig config, Set<T> services);
 
+    Logger LOGGER = LoggerFactory.getLogger(TesseraServerFactory.class);
+    
     static TesseraServerFactory create(CommunicationType communicationType) {
-        CommunicationType ct = communicationType == CommunicationType.UNIX_SOCKET ? CommunicationType.REST : communicationType;
+        final CommunicationType ct;
+        if(communicationType == CommunicationType.UNIX_SOCKET) {
+           LOGGER.warn("UNIX_SOCKET communication type is deprecated it will "
+                   + "be removed for furture releases. "
+                   + "Use REST commnications type with UnixServerSocket serverSocket");
+           ct = CommunicationType.REST;
+        } else {
+            ct = communicationType;
+        }
+
         List<TesseraServerFactory> all = new ArrayList<>();
         ServiceLoader.load(TesseraServerFactory.class).forEach(all::add);
         return all.stream()
