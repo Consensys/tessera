0bad3403252d00a4c4c22c54e53b3c2fd7af68c9
Mark <melowe.quorum@gmail.com>
2019-03-08 12:30:00 +0000

    Add system adapter to proxy access to System.out and System.err

---
+++ shared/src/test/java/com/quorum/tessera/io/MockSystemAdapter.java
@@ -0,0 +1,30 @@
+package com.quorum.tessera.io;
+
+import java.io.PrintStream;
+
+
+public class MockSystemAdapter implements SystemAdapter {
+    
+    private PrintStream outPrintStream;
+    
+    private PrintStream errPrintStream;
+
+    public void setOutPrintStream(PrintStream outPrintStream) {
+        this.outPrintStream = outPrintStream;
+    }
+
+    public void setErrPrintStream(PrintStream errPrintStream) {
+        this.errPrintStream = errPrintStream;
+    }
+    
+    @Override
+    public PrintStream out() {
+        return outPrintStream;
+    }
+
+    @Override
+    public PrintStream err() {
+        return errPrintStream;
+    }
+    
+}
