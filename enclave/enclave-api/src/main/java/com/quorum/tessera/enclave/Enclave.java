02b56550358c93e92d6aa181efdc3afa11b0172c
Mark <melowe.quorum@gmail.com>
2019-03-14 17:09:44 +0000

    Deprecate methods that are node configuration focused rather than enclave.

--- enclave/enclave-api/src/main/java/com/quorum/tessera/enclave/Enclave.java
+++ enclave/enclave-api/src/main/java/com/quorum/tessera/enclave/Enclave.java
@@ -7,33 +7,41 @@ import java.util.List;
 import java.util.Set;
 
 /**
- * An {@link Enclave} provides encryption/decryption functions and keeps hold
- * of all the nodes private keys so the do not leak into other services.
+ * An {@link Enclave} provides encryption/decryption functions and keeps hold of
+ * all the nodes private keys so the do not leak into other services.
  */
 public interface Enclave extends Service {
 
     /**
-     * Retrieves the public key to use if no key is specified for an operation
-     * There is no guarantee this key remains the same between runs of the Enclave.
+     * @deprecated This needs moving out of enclave as is basically a local node
+     * config property Retrieves the public key to use if no key is specified
+     * for an operation There is no guarantee this key remains the same between
+     * runs of the Enclave.
      *
      * @return the public key that has been assigned as the default
      */
+    @Deprecated
     PublicKey defaultPublicKey();
 
     /**
-     * Returns a set of public keys that should be included as recipients to
-     * all transactions produced by this node. The keys are not be managed by
-     * this node.
+     * @deprecated This needs moving out of enclave as is basically a local node
+     * config property Returns a set of public keys that should be included as
+     * recipients to all transactions produced by this node. The keys are not be
+     * managed by this node.
      *
      * @return the set of public keys to be added to transactions
      */
+    @Deprecated
     Set<PublicKey> getForwardingKeys();
 
     /**
-     * Returns all the public keys that are managed by this Enclave.
+     * @deprecated This needs moving out of enclave as is basically a local node
+     * config property Returns all the public keys that are managed by this
+     * Enclave.
      *
      * @return all public keys managed by this Enclave
      */
+    @Deprecated
     Set<PublicKey> getPublicKeys();
 
     /**
@@ -41,10 +49,11 @@ public interface Enclave extends Service {
      * Returns a {@link EncodedPayload} which contains all the encrypted
      * information, including the recipients and their encrypted master keys.
      *
-     * @param message             the message to be encrypted
-     * @param senderPublicKey     the public key which this enclave manages
+     * @param message the message to be encrypted
+     * @param senderPublicKey the public key which this enclave manages
      * @param recipientPublicKeys the recipients to encrypt this message for
-     * @return the encrypted information, represented by an {@link EncodedPayload}
+     * @return the encrypted information, represented by an
+     * {@link EncodedPayload}
      */
     EncodedPayload encryptPayload(byte[] message, PublicKey senderPublicKey, List<PublicKey> recipientPublicKeys);
 
@@ -52,19 +61,21 @@ public interface Enclave extends Service {
      * Decrypts a {@link RawTransaction} so that it can be re-encrypted into a
      * {@link EncodedPayload} with the given recipient list
      *
-     * @param rawTransaction      the transactiopn to decrypt and re-encrypt with recipients
+     * @param rawTransaction the transactiopn to decrypt and re-encrypt with
+     * recipients
      * @param recipientPublicKeys the recipients to encrypt the transaction for
-     * @return the encrypted information, represented by an {@link EncodedPayload}
+     * @return the encrypted information, represented by an
+     * {@link EncodedPayload}
      */
     EncodedPayload encryptPayload(RawTransaction rawTransaction, List<PublicKey> recipientPublicKeys);
 
     /**
-     * Encrypt a payload without any recipients that can be retrieved later.
-     * The payload is encrypted using the private key that is related to the
-     * given public key.
+     * Encrypt a payload without any recipients that can be retrieved later. The
+     * payload is encrypted using the private key that is related to the given
+     * public key.
      *
      * @param message the message to be encrypted
-     * @param sender  the sender's public key to encrypt the transaction with
+     * @param sender the sender's public key to encrypt the transaction with
      * @return the encrypted transaction
      */
     RawTransaction encryptRawPayload(byte[] message, PublicKey sender);
@@ -75,17 +86,19 @@ public interface Enclave extends Service {
      * the provided public key OR one of the Enclave's managed keys cannot be
      * used to decrypt the payload
      *
-     * @param payload     the encrypted payload
-     * @param providedKey the key to use for decryption, if the payload wasn't sent by this Enclave
+     * @param payload the encrypted payload
+     * @param providedKey the key to use for decryption, if the payload wasn't
+     * sent by this Enclave
      * @return the original, decrypted message
      */
     byte[] unencryptTransaction(EncodedPayload payload, PublicKey providedKey);
 
     /**
-     * Creates a new recipient box for the payload, for which we must be the originator.
-     * At least one recipient must already be available to be able to decrypt the master key.
+     * Creates a new recipient box for the payload, for which we must be the
+     * originator. At least one recipient must already be available to be able
+     * to decrypt the master key.
      *
-     * @param payload      the payload to add a recipient to
+     * @param payload the payload to add a recipient to
      * @param recipientKey the new recipient key to add
      */
     byte[] createNewRecipientBox(EncodedPayload payload, PublicKey recipientKey);
