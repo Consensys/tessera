5892c5fd8db6d3fa5d99282c522630a6e044dbd5
Mark <melowe.quorum@gmail.com>
2019-03-20 15:37:19 +0000

    Commiting current failing tests with debug logging.

---
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuite.java
@@ -0,0 +1,23 @@
+
+package com.quorum.tessera.test.rest;
+
+import org.junit.runners.Suite;
+
+@Suite.SuiteClasses({
+//    MultipleKeyNodeIT.class,
+//    DeleteIT.class,
+//    PushIT.class,
+//    ReceiveIT.class,
+//    ReceiveRawIT.class,
+    ResendAllIT.class,
+//    ResendIndividualIT.class,
+//    SendIT.class,
+//    SendRawIT.class,
+//    P2PRestAppIT.class,
+//    TransactionForwardingIT.class,
+//    CucumberRestIT.class,
+//    CucumberRawIT.class
+})
+public abstract class RestSuite {
+    
+}
--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuiteRemoteEnclaveH2.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuiteRemoteEnclaveH2.java
@@ -4,7 +4,6 @@ package com.quorum.tessera.test.rest;
 import com.quorum.tessera.config.CommunicationType;
 import com.quorum.tessera.test.*;
 import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import suite.EnclaveType;
 import suite.SocketType;
 import suite.TestSuite;
@@ -16,21 +15,7 @@ import suite.TestSuite;
         socketType = SocketType.HTTP,
         enclaveType = EnclaveType.REMOTE)
 
-@Suite.SuiteClasses({
-    MultipleKeyNodeIT.class,
-    DeleteIT.class,
-    PushIT.class,
-    ReceiveIT.class,
-    ReceiveRawIT.class,
-    ResendAllIT.class,
-    ResendIndividualIT.class,
-    SendIT.class,
-    SendRawIT.class,
-    P2PRestAppIT.class,
-    TransactionForwardingIT.class,
-    CucumberRestIT.class,
-    CucumberRawIT.class
-})
-public class RestSuiteRemoteEnclaveH2 {
+
+public class RestSuiteRemoteEnclaveH2 extends RestSuite {
 
 }
