439a2b0d2be6cccf51a5ff090e0760eab67bea90
Mark <melowe.quorum@gmail.com>
2019-03-21 18:10:03 +0000

    Define all tests in super class

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/grpc/GrpcSuiteHSQL.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/grpc/GrpcSuiteHSQL.java
@@ -1,7 +1,6 @@
 package com.quorum.tessera.test.grpc;
 
 import com.quorum.tessera.config.CommunicationType;
-import com.quorum.tessera.test.CucumberGprcIT;
 import com.quorum.tessera.test.DBType;
 import org.junit.runner.RunWith;
 import suite.SocketType;
@@ -12,14 +11,7 @@ import suite.TestSuite;
         communicationType = CommunicationType.GRPC,
         dbType = DBType.HSQL,
         socketType = SocketType.HTTP)
-
-@TestSuite.SuiteClasses({
-    SendGrpcIT.class,
-    PartyInfoGrpcIT.class,
-    TesseraGrpcIT.class,
-    CucumberGprcIT.class
-})
-public class GrpcSuiteHSQL {
+public class GrpcSuiteHSQL extends GrpcSuite {
 
 
 }
