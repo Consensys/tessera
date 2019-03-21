21b28f064687c0895dda5599936df20ab76d53be
Mark <melowe.quorum@gmail.com>
2019-03-21 15:07:30 +0000

    Reactivate tests suites and remove version from enclave-jaxrs server dependency

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuite.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuite.java
@@ -1,22 +1,25 @@
 
 package com.quorum.tessera.test.rest;
 
-import org.junit.runners.Suite;
 
-@Suite.SuiteClasses({
-//    MultipleKeyNodeIT.class,
-//    DeleteIT.class,
-//    PushIT.class,
-//    ReceiveIT.class,
-//    ReceiveRawIT.class,
+import com.quorum.tessera.test.CucumberRawIT;
+import com.quorum.tessera.test.CucumberRestIT;
+
+
+@suite.TestSuite.SuiteClasses({
+    MultipleKeyNodeIT.class,
+    DeleteIT.class,
+    PushIT.class,
+    ReceiveIT.class,
+    ReceiveRawIT.class,
     ResendAllIT.class,
-//    ResendIndividualIT.class,
-//    SendIT.class,
-//    SendRawIT.class,
-//    P2PRestAppIT.class,
-//    TransactionForwardingIT.class,
-//    CucumberRestIT.class,
-//    CucumberRawIT.class
+    ResendIndividualIT.class,
+    SendIT.class,
+    SendRawIT.class,
+    P2PRestAppIT.class,
+    TransactionForwardingIT.class,
+    CucumberRestIT.class,
+    CucumberRawIT.class
 })
 public abstract class RestSuite {
     
