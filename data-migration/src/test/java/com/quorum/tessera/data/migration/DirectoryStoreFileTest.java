622de59183287970813773136113bd0d895b38ec
Mark Lowe <melowe.quorum@gmail.com>
2019-04-23 15:50:28 +0100

    Encode value as base64 just for consistency.

--- data-migration/src/test/java/com/quorum/tessera/data/migration/DirectoryStoreFileTest.java
+++ data-migration/src/test/java/com/quorum/tessera/data/migration/DirectoryStoreFileTest.java
@@ -1,28 +1,37 @@
 package com.quorum.tessera.data.migration;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.UncheckedIOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Map;
+import java.util.UUID;
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
+import org.junit.Ignore;
 
 public class DirectoryStoreFileTest {
-    
+
     @Test
     public void load() throws Exception {
-    
+
         Path directory = Paths.get(getClass().getResource("/dir/").toURI());
-        
+
         DirectoryStoreFile directoryStoreFile = new DirectoryStoreFile();
 
         Map<byte[], InputStream> results = directoryStoreFile.load(directory);
 
         assertThat(results).hasSize(22);
-        
+
     }
 
+    @Ignore
     @Test
     public void loadLarge() throws Exception {
 
@@ -34,14 +43,26 @@ public class DirectoryStoreFileTest {
 
         Path largeFile = Paths.get(directory.toAbsolutePath().toString(), "loadLarge");
 
-        Files.copy(getClass().getResourceAsStream("/loadLarge.sample"), largeFile);
+        try (java.io.BufferedWriter writer = Files.newBufferedWriter(largeFile)) {
+            IntStream.range(1, 5000000)
+                .mapToObj(i -> UUID.randomUUID())
+                .map(UUID::toString)
+                .map(String::getBytes)
+                .map(Base64.getEncoder()::encodeToString)
+                .forEach(s -> {
+                    try {
+                        writer.write(s);
+                    } catch (IOException ex) {
+                        throw new UncheckedIOException(ex);
+                    }
+                });
+        }
 
         DirectoryStoreFile directoryStoreFile = new DirectoryStoreFile();
 
-        Map<byte[], InputStream> results = directoryStoreFile.load(directory);
+        Map results = directoryStoreFile.load(directory);
 
         assertThat(results).hasSize(1);
-
     }
 
 }
