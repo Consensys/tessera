118ba194e349ad41bc792d372db8f3d4f90399bd
Mark Lowe <melowe.quorum@gmail.com>
2019-04-23 13:10:33 +0100

    Add test that replicated user bug

--- data-migration/src/test/java/com/quorum/tessera/data/migration/DirectoryStoreFileTest.java
+++ data-migration/src/test/java/com/quorum/tessera/data/migration/DirectoryStoreFileTest.java
@@ -1,24 +1,47 @@
 package com.quorum.tessera.data.migration;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 
 public class DirectoryStoreFileTest {
-    
+
     @Test
     public void load() throws Exception {
-    
+
         Path directory = Paths.get(getClass().getResource("/dir/").toURI());
-        
+
         DirectoryStoreFile directoryStoreFile = new DirectoryStoreFile();
-        
-        Map<byte[],byte[]> results = directoryStoreFile.load(directory);
-        
+
+        Map<byte[], byte[]> results = directoryStoreFile.load(directory);
+
         assertThat(results).hasSize(22);
-        
+
     }
-    
+
+    @Test
+    public void loadLarge() throws Exception {
+
+        Path baseDir = Paths.get(getClass().getResource("/").toURI());
+
+        Path directory = baseDir.resolve(UUID.randomUUID().toString());
+
+        Files.createDirectories(directory);
+
+        Path largeFile = Paths.get(directory.toAbsolutePath().toString(), "loadLarge");
+
+        Files.copy(getClass().getResourceAsStream("/loadLarge.sample"), largeFile);
+
+        DirectoryStoreFile directoryStoreFile = new DirectoryStoreFile();
+
+        Map<byte[], byte[]> results = directoryStoreFile.load(directory);
+
+        assertThat(results).hasSize(1);
+
+    }
+
 }
