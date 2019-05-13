0bf57b5cceed2fa6241e8f7e456054790e8eddad
chris-j-h <39617426+chris-j-h@users.noreply.github.com>
2019-05-13 12:55:12 +0100

    Monitoring fixes/improvements (#744)
    * Update Jersey metrics to work with multi-app server config
    
    * Add server app type to name of InfluxDB metric measurements
    
    * Add Influx monitoring to Q2T app if using legacy config
    
    * Throw exception and exit if influx config given for unsupported app type
    
    * Fix monitoring tests and add more test cases
    
    * Update Prometheus metrics endpoint to return metrics for configured apps
    
    * Checkstyle fix
    
    * Track Enclave API metrics
    
    * Add Enclave API data to Prometheus metrics endpoint
    
    * String builder clean up

---
+++ server/jersey-server/src/main/java/com/quorum/tessera/server/monitoring/MonitoringNotSupportedException.java
@@ -0,0 +1,11 @@
+package com.quorum.tessera.server.monitoring;
+
+import com.quorum.tessera.config.AppType;
+
+public class MonitoringNotSupportedException extends RuntimeException {
+
+    public MonitoringNotSupportedException(final AppType appType) {
+        super(appType + " app does not support monitoring Jersey metrics");
+    }
+
+}
