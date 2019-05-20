3393ce6a80206e95cded3faeeba7e3dc8027c5d0
chris-j-h <chris.hounsom@gmail.com>
2019-05-20 19:08:14 +0100

    Mock CliAdapter for CliDelegate tests

--- cli/cli-api/src/test/java/com/quorum/tessera/cli/CliDelegateTest.java
+++ cli/cli-api/src/test/java/com/quorum/tessera/cli/CliDelegateTest.java
@@ -1,13 +1,12 @@
 package com.quorum.tessera.cli;
 
-import com.quorum.tessera.test.util.ElUtil;
+import com.quorum.tessera.config.Config;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Path;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class CliDelegateTest {
 
@@ -22,26 +21,66 @@ public class CliDelegateTest {
 
     @Test
     public void adminCliOptionCreatesAdminInstance() throws Exception {
-        Path configFile = ElUtil.createAndPopulatePaths(getClass().getResource("/sample-config.json"));
-        CliResult result = instance.execute("admin", "-configfile", configFile.toString());
+        MockCliAdapter.setType(CliType.ADMIN);
+        int status = 111;
+        MockCliAdapter.setResult(new CliResult(status, true, null));
+
+        CliResult result = instance.execute("admin", "-configfile", "/path/to/file");
 
         assertThat(result).isNotNull();
-        // MockAdminCliAdapter should be loaded by ServiceLoader and returns status 101
-        assertThat(result.getStatus()).isEqualTo(101);
+        assertThat(result.getStatus()).isEqualTo(status);
+    }
+
+    @Test
+    public void adminCliOptionButNoAdminCliAvailableThrowsException() throws Exception {
+        MockCliAdapter.setType(CliType.CONFIG);
+
+        Throwable ex = catchThrowable(() -> instance.execute("admin"));
+
+        assertThat(ex).isNotNull();
+        assertThat(ex).isExactlyInstanceOf(CliException.class);
+        assertThat(ex).hasMessage("No valid implementation of CliAdapter found on the classpath");
     }
 
     @Test
     public void standardCliOptionsCreatesConfigInstance() throws Exception {
-        Path configFile = ElUtil.createAndPopulatePaths(getClass().getResource("/sample-config.json"));
-        CliResult result = instance.execute("-configfile", configFile.toString());
+        MockCliAdapter.setType(CliType.CONFIG);
+        int status = 111;
+        Config config = new Config();
+        MockCliAdapter.setResult(new CliResult(status, true, config));
+
+        CliResult result = instance.execute("-configfile", "path/to/file");
 
         assertThat(result).isNotNull();
-        // MockDefaultCliAdapter should be loaded by ServiceLoader and returns status 100
-        assertThat(result.getStatus()).isEqualTo(100);
+        assertThat(result.getStatus()).isEqualTo(status);
+        assertThat(result.getConfig().get()).isEqualTo(config);
+    }
+
+    @Test
+    public void standardCliOptionsButNoNonAdminCliAvailableThrowsException() throws Exception {
+        MockCliAdapter.setType(CliType.ADMIN);
+
+        Throwable ex = catchThrowable(() -> instance.execute("-configfile", "/path/to/file"));
+
+        assertThat(ex).isNotNull();
+        assertThat(ex).isExactlyInstanceOf(CliException.class);
+        assertThat(ex).hasMessage("No valid implementation of CliAdapter found on the classpath");
+    }
+
+    @Test
+    public void configFieldUpdatedAfterExecution() throws Exception {
+        MockCliAdapter.setType(CliType.CONFIG);
+        int status = 111;
+        Config config = new Config();
+        MockCliAdapter.setResult(new CliResult(status, false, config));
+
+        instance.execute("-configfile", "/path/to/file");
+
+        assertThat(instance.getConfig()).isEqualTo(new Config());
     }
 
     @Test(expected = IllegalStateException.class)
-    public void fetchWithoutExecution() {
+    public void fetchConfigWithoutExecution() {
         instance.getConfig();
     }
 }
