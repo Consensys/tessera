708305d834f67b30ac26cef35b7362df18fa686b
Peter Fox <31221824+prd-fox@users.noreply.github.com>
2019-07-19 11:12:38 +0100

    Undo the removal of the deprecated configuration until it needs to be (#822)
    released

--- config-migration/src/test/java/com/quorum/tessera/config/migration/TomlConfigFactoryTest.java
+++ config-migration/src/test/java/com/quorum/tessera/config/migration/TomlConfigFactoryTest.java
@@ -36,28 +36,30 @@ public class TomlConfigFactoryTest {
         try (InputStream configData = ElUtil.process(template, params)) {
             Config result = tomlConfigFactory.create(configData, null).build();
             assertThat(result).isNotNull();
+            assertThat(result.getUnixSocketFile()).isEqualTo(Paths.get("data", "myipcfile.ipc"));
+            assertThat(result.getServer()).isNotNull();
+            assertThat(result.getServer().getSslConfig()).isNotNull();
 
-            final ServerConfig q2tConfig = result.getServerConfigs().stream().filter(ServerConfig::isUnixSocket).findAny().get();
-            assertThat(q2tConfig.getServerAddress()).isEqualTo("unix:data/myipcfile.ipc");
+            SslConfig sslConfig = result.getServer().getSslConfig();
 
-            assertThat(result.getP2PServerConfig()).isNotNull();
-            assertThat(result.getP2PServerConfig().getServerAddress()).isEqualTo("http://127.0.0.1:9001");
-            assertThat(result.getP2PServerConfig().getBindingAddress()).isEqualTo("http://127.0.0.1:9001");
+            assertThat(result.getServer().getHostName()).isEqualTo("http://127.0.0.1");
+            assertThat(result.getServer().getPort()).isEqualTo(9001);
+            assertThat(result.getServer().getBindingAddress()).isEqualTo("http://127.0.0.1:9001");
 
-            final SslConfig sslConfig = result.getP2PServerConfig().getSslConfig();
-            assertThat(sslConfig).isNotNull();
             assertThat(sslConfig.getClientTlsKeyPath()).isEqualTo(Paths.get("data/tls-client-key.pem"));
             assertThat(sslConfig.getClientTrustMode()).isEqualTo(SslTrustMode.CA_OR_TOFU);
+
         }
     }
 
     @Test
     public void urlPortNotSetInConfig() {
+
         InputStream template = getClass().getResourceAsStream("/sample-all-values-urlport-not-present.conf");
 
         Config result = tomlConfigFactory.create(template, null).build();
 
-        assertThat(result.getP2PServerConfig().getServerAddress()).isEqualTo("http://127.0.0.1");
+        assertThat(result.getServer().getHostName()).isEqualTo("http://127.0.0.1");
     }
 
     @Test
@@ -74,21 +76,24 @@ public class TomlConfigFactoryTest {
     @Test
     public void createConfigFromSampleFileOnly() throws IOException {
 
-        try (InputStream configData = getClass().getResourceAsStream("/sample.conf")) {
-            final Config result = tomlConfigFactory.create(configData, null).build();
+        Path passwordFile = Files.createTempFile("password", ".txt");
+        InputStream template = getClass().getResourceAsStream("/sample.conf");
+
+        try (InputStream configData = template) {
+            Config result = tomlConfigFactory.create(configData, null).build();
             assertThat(result).isNotNull();
+            assertThat(result.getUnixSocketFile()).isEqualTo(Paths.get("data", "constellation.ipc"));
+            assertThat(result.getServer()).isNotNull();
+            assertThat(result.getServer().getSslConfig()).isNotNull();
 
-            final ServerConfig q2tConfig = result.getServerConfigs().stream().filter(ServerConfig::isUnixSocket).findAny().get();
-            assertThat(q2tConfig.getServerAddress()).isEqualTo("unix:data/constellation.ipc");
-
-            assertThat(result.getP2PServerConfig()).isNotNull();
-            assertThat(result.getP2PServerConfig().getSslConfig()).isNotNull();
-
-            final SslConfig sslConfig = result.getP2PServerConfig().getSslConfig();
+            SslConfig sslConfig = result.getServer().getSslConfig();
 
             assertThat(sslConfig.getClientTlsKeyPath()).isEqualTo(Paths.get("data/tls-client-key.pem"));
             assertThat(sslConfig.getClientTrustMode()).isEqualTo(SslTrustMode.CA_OR_TOFU);
+
         }
+
+        Files.deleteIfExists(passwordFile);
     }
 
     @Test
@@ -117,6 +122,7 @@ public class TomlConfigFactoryTest {
 
             }
         }
+
     }
 
     @Test(expected = UnsupportedOperationException.class)
