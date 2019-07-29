a975f4de6235a1ad3744986a90c860bb7016333b
Peter Fox <peter.rd.fox@gmail.com>
2019-07-29 14:01:30 +0100

    Add parameterized test suites that allow for lots of Tessera configurations for the same test(s).

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/grpc/GrpcSuiteHSQL.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/grpc/GrpcSuiteHSQL.java
@@ -3,15 +3,10 @@ package com.quorum.tessera.test.grpc;
 import com.quorum.tessera.config.CommunicationType;
 import com.quorum.tessera.test.DBType;
 import org.junit.runner.RunWith;
+import suite.ProcessConfig;
 import suite.SocketType;
 import suite.TestSuite;
 
 @RunWith(TestSuite.class)
-@TestSuite.ProcessConfig(
-        communicationType = CommunicationType.GRPC,
-        dbType = DBType.HSQL,
-        socketType = SocketType.HTTP)
-public class GrpcSuiteHSQL extends GrpcSuite {
-
-
-}
+@ProcessConfig(communicationType = CommunicationType.GRPC, dbType = DBType.HSQL, socketType = SocketType.HTTP)
+public class GrpcSuiteHSQL extends GrpcSuite {}
--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuiteRemoteEnclaveH2.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuiteRemoteEnclaveH2.java
@@ -1,21 +1,17 @@
 package com.quorum.tessera.test.rest;
 
-
 import com.quorum.tessera.config.CommunicationType;
-import com.quorum.tessera.test.*;
+import com.quorum.tessera.test.DBType;
 import org.junit.runner.RunWith;
 import suite.EnclaveType;
+import suite.ProcessConfig;
 import suite.SocketType;
 import suite.TestSuite;
 
 @RunWith(TestSuite.class)
-@TestSuite.ProcessConfig(
+@ProcessConfig(
         communicationType = CommunicationType.REST,
         dbType = DBType.H2,
         socketType = SocketType.HTTP,
         enclaveType = EnclaveType.REMOTE)
-
-
-public class RestSuiteRemoteEnclaveH2 extends RestSuite {
-
-}
+public class RestSuiteRemoteEnclaveH2 extends RestSuite {}
