bf90e6df33c1c3fa83e4ad9a982bc1d3c56d9b3f
Mark Lowe <melowe.quorum@gmail.com>
2019-07-31 14:56:39 +0100

    Remove unrequired logger and log message

--- tessera-sync/src/main/java/com/quorum/tessera/sync/WebsocketPayloadPublisherFactory.java
+++ tessera-sync/src/main/java/com/quorum/tessera/sync/WebsocketPayloadPublisherFactory.java
@@ -4,16 +4,11 @@ import com.quorum.tessera.config.CommunicationType;
 import com.quorum.tessera.config.Config;
 import com.quorum.tessera.partyinfo.PayloadPublisher;
 import com.quorum.tessera.partyinfo.PayloadPublisherFactory;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class WebsocketPayloadPublisherFactory implements PayloadPublisherFactory {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(WebsocketPayloadPublisherFactory.class);
-
     @Override
     public PayloadPublisher create(Config config) {
-        LOGGER.info("Creating PayloadPublisher from {}",config);
         return new WebsocketPayloadPublisher(SessionStore.create());
     }
 
