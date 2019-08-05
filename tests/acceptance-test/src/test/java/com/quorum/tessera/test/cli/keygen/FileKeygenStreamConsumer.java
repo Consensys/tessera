818fce72153595ed061d0695a0d753eb72adcbcd
Peter Fox <peter.rd.fox@gmail.com>
2019-08-05 10:57:25 +0100

    Add file-based key generation acceptance tests for unlocked keys and locked keys with default Argon2 options.

---
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/cli/keygen/FileKeygenStreamConsumer.java
@@ -0,0 +1,40 @@
+package com.quorum.tessera.test.cli.keygen;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class FileKeygenStreamConsumer implements Runnable {
+
+    private static final List<String> REQUEST_STRINGS =
+            Arrays.asList(
+                    "Enter a password if you want to lock the private key or leave blank",
+                    "Please re-enter the password (or lack of) to confirm");
+
+    private final InputStream inputStream;
+
+    private final OutputStream outputStream;
+
+    private final byte[] response;
+
+    public FileKeygenStreamConsumer(final InputStream is, final OutputStream os, final String response) {
+        this.inputStream = is;
+        this.outputStream = os;
+        this.response = (response + System.lineSeparator()).getBytes();
+    }
+
+    @Override
+    public void run() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (REQUEST_STRINGS.contains(line)) {
+                    outputStream.write(response);
+                    outputStream.flush();
+                }
+            }
+        } catch (final IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
