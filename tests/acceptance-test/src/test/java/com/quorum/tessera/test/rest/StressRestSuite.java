c1d99d26b01b563c6aa4904feb55fe9580f3c54a
nicolae <nicolae.leonte.go@gmail.com>
2019-09-13 08:56:15 +0100

    ensure jaxrs Responses are closed

---
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/StressRestSuite.java
@@ -0,0 +1,30 @@
+package com.quorum.tessera.test.rest;
+
+import com.quorum.tessera.test.DBType;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import suite.EnclaveType;
+import suite.ParameterizedTestSuiteRunnerFactory;
+import suite.ProcessConfiguration;
+import suite.TestSuite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.quorum.tessera.config.CommunicationType.REST;
+import static suite.SocketType.HTTP;
+
+@TestSuite.SuiteClasses({StressSendIT.class})
+@RunWith(Parameterized.class)
+@Parameterized.UseParametersRunnerFactory(ParameterizedTestSuiteRunnerFactory.class)
+public class StressRestSuite {
+
+    @Parameterized.Parameters
+    public static List<ProcessConfiguration> configurations() {
+        final List<ProcessConfiguration> configurations = new ArrayList<>();
+
+        configurations.add(new ProcessConfiguration(DBType.H2, REST, HTTP, EnclaveType.LOCAL, false, ""));
+
+        return configurations;
+    }
+}
