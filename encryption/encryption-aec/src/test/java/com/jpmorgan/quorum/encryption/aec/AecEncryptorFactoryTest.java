9792eec3df7572c4e149b34205bb2fc8ce0dc4cd
Mark Lowe <melowe.quorum@gmail.com>
2019-10-24 10:37:48 +0100

    Add initial AEC encryptor module, pending config integration.

---
+++ encryption/encryption-aec/src/test/java/com/jpmorgan/quorum/encryption/aec/AecEncryptorFactoryTest.java
@@ -0,0 +1,24 @@
+package com.jpmorgan.quorum.encryption.aec;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import com.quorum.tessera.encryption.Encryptor;
+
+public class AecEncryptorFactoryTest {
+
+    private AecEncryptorFactory encryptorFactory;
+
+    @Before
+    public void setUp() {
+        this.encryptorFactory = new AecEncryptorFactory();
+    }
+
+    @Test
+    public void createInstance() {
+        final Encryptor result = encryptorFactory.create();
+
+        assertThat(result).isNotNull().isExactlyInstanceOf(AecEncryptor.class);
+    }
+}
