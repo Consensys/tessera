5c3e3de693f751a0b7946c5a61736e0c8ba3e4b0
Mark Lowe <melowe.quorum@gmail.com>
2019-10-24 10:40:16 +0100

    Ensure that JACL encryptor is returned despite AEC being available in classloader. Temporary measure until config changes for alternative encryptors is added.

--- encryption/encryption-api/src/main/java/com/quorum/tessera/encryption/EncryptorFactory.java
+++ encryption/encryption-api/src/main/java/com/quorum/tessera/encryption/EncryptorFactory.java
@@ -1,8 +1,10 @@
 package com.quorum.tessera.encryption;
 
 import com.quorum.tessera.ServiceLoaderUtil;
+import java.util.Arrays;
+import java.util.List;
 
-/** *  A factory for providing the implementation of the {@link Encryptor} with all its dependencies set up */
+/** * A factory for providing the implementation of the {@link Encryptor} with all its dependencies set up */
 public interface EncryptorFactory {
 
     /**
@@ -19,6 +21,11 @@ public interface EncryptorFactory {
      */
     static EncryptorFactory newFactory() {
         // TODO: return the stream and let the caller deal with it
-        return ServiceLoaderUtil.loadAll(EncryptorFactory.class).findAny().get();
+        List<String> naclDefaults = Arrays.asList("KaliumFactory", "JnaclFactory");
+
+        return ServiceLoaderUtil.loadAll(EncryptorFactory.class)
+                .filter(f -> naclDefaults.contains(f.getClass().getSimpleName()))
+                .findAny()
+                .orElse(ServiceLoaderUtil.load(EncryptorFactory.class).get());
     }
 }
