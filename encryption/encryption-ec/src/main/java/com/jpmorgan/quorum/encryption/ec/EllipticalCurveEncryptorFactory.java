55deb670e0acc202dc3d2c1b227365df90184eae
Mark Lowe <melowe.quorum@gmail.com>
2019-11-01 14:16:51 +0000

    avoid null pointer

--- encryption/encryption-ec/src/main/java/com/jpmorgan/quorum/encryption/ec/EllipticalCurveEncryptorFactory.java
+++ encryption/encryption-ec/src/main/java/com/jpmorgan/quorum/encryption/ec/EllipticalCurveEncryptorFactory.java
@@ -2,7 +2,9 @@ package com.jpmorgan.quorum.encryption.ec;
 
 import com.quorum.tessera.encryption.Encryptor;
 import com.quorum.tessera.encryption.EncryptorFactory;
+import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 public class EllipticalCurveEncryptorFactory implements EncryptorFactory {
 
@@ -13,12 +15,12 @@ public class EllipticalCurveEncryptorFactory implements EncryptorFactory {
 
     @Override
     public Encryptor create(Map<String, String> properties) {
-
+        final Map<String, String> props = Optional.ofNullable(properties).orElse(Collections.emptyMap());
         String symmetricCipher = properties.getOrDefault("symmetricCipher", "AES/GCM/NoPadding");
         String ellipticCurve = properties.getOrDefault("ellipticCurve", "secp256r1");
         int nonceLength = Integer.parseInt(properties.getOrDefault("nonceLength", "24"));
         int sharedKeyLength = Integer.parseInt(properties.getOrDefault("sharedKeyLength", "32"));
-        
+
         return new EllipticalCurveEncryptor(symmetricCipher, ellipticCurve, nonceLength, sharedKeyLength);
     }
 }
