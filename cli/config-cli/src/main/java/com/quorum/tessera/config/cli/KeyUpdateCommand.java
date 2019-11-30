237209b023e8588f2f4e021eb85de0c622fe37c4
chris-j-h <chris.hounsom@gmail.com>
2019-11-30 11:56:59 +0000

    [KEYUPDATE-WIP] Replace DefaultCli Apache CLI with PicoCLI Add keyupdate as a subcommand to the main tessera command

---
+++ cli/config-cli/src/main/java/com/quorum/tessera/config/cli/KeyUpdateCommand.java
@@ -0,0 +1,208 @@
+package com.quorum.tessera.config.cli;
+
+import com.quorum.tessera.cli.CliResult;
+import com.quorum.tessera.config.*;
+import com.quorum.tessera.config.keys.KeyEncryptor;
+import com.quorum.tessera.config.keys.KeyEncryptorFactory;
+import com.quorum.tessera.config.util.JaxbUtil;
+import com.quorum.tessera.encryption.PrivateKey;
+import com.quorum.tessera.io.SystemAdapter;
+import com.quorum.tessera.passwords.PasswordReader;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+import java.util.concurrent.Callable;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+
+@CommandLine.Command(
+        name = "keyupdate",
+        aliases = {"-updatepassword"},
+        headerHeading = "Usage:%n%n",
+        synopsisHeading = "%n",
+        descriptionHeading = "%nDescription:%n%n",
+        parameterListHeading = "%nParameters:%n",
+        optionListHeading = "%nOptions:%n",
+        header = "Update the password for a key",
+        description =
+                "Change the password or update the encryption options for an already locked key, or apply a new password to an unlocked key",
+        subcommands = {CommandLine.HelpCommand.class},
+        abbreviateSynopsis = true)
+public class KeyUpdateCommand implements Callable<CliResult> {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KeyUpdateCommand.class);
+
+    // TODO(cjh) don't hardcode these options (?)
+
+    @CommandLine.Option(names = "--keys.keyData.privateKeyPath", required = true)
+    public Path privateKeyPath;
+
+    //    @Pattern(regexp = "^(id|i|d)$")
+    //    @XmlAttribute(name = "variant")
+    // TODO(cjh) validation on the CLI values - the above is the validation applied to the Config ArgonOptions object
+    // fields
+    @CommandLine.Option(names = "--keys.keyData.config.data.aopts.algorithm", defaultValue = "i")
+    public String algorithm;
+
+    @CommandLine.Option(names = "--keys.keyData.config.data.aopts.iterations", defaultValue = "10")
+    public Integer iterations;
+
+    @CommandLine.Option(names = "--keys.keyData.config.data.aopts.memory", defaultValue = "1048576")
+    public Integer memory;
+
+    @CommandLine.Option(names = "--keys.keyData.config.data.aopts.parallelism", defaultValue = "4")
+    public Integer parallelism;
+
+    @CommandLine.Option(names = {"--keys.passwords"})
+    public String password;
+
+    @CommandLine.Option(names = {"--keys.passwordFile"})
+    public Path passwordFile;
+
+    @CommandLine.Option(
+            names = {"--configfile", "-configfile"},
+            description = "Path to node configuration file")
+    public Config config;
+
+    // TODO(cjh) default value using enum instead of hardcoding
+    @CommandLine.Option(
+            names = {"--encryptor.type"},
+            defaultValue = "NACL")
+    public EncryptorType encryptorType;
+
+    @CommandLine.Option(names = {"--encryptor.symmetricCipher"})
+    public String encryptorSymmetricCipher;
+
+    @CommandLine.Option(names = {"--encryptor.ellipticCurve"})
+    public String encryptorEllipticCurve;
+
+    @CommandLine.Option(names = {"--encryptor.nonceLength"})
+    public String encryptorNonceLength;
+
+    @CommandLine.Option(names = {"--encryptor.sharedKeyLength"})
+    public String encryptorSharedKeyLength;
+
+    private KeyEncryptorFactory keyEncryptorFactory;
+
+    private KeyEncryptor keyEncryptor;
+
+    private PasswordReader passwordReader;
+
+    KeyUpdateCommand(KeyEncryptorFactory keyEncryptorFactory, PasswordReader passwordReader) {
+        this.keyEncryptorFactory = keyEncryptorFactory;
+        this.passwordReader = passwordReader;
+    }
+
+    @Override
+    public CliResult call() throws Exception {
+        final EncryptorConfig encryptorConfig;
+
+        if (Optional.ofNullable(config).map(Config::getEncryptor).isPresent()) {
+            encryptorConfig = config.getEncryptor();
+        } else {
+            encryptorConfig = new EncryptorConfig();
+            encryptorConfig.setType(encryptorType);
+
+            if (encryptorType == EncryptorType.EC) {
+                Map<String, String> properties = new HashMap<>();
+
+                Optional.ofNullable(encryptorSymmetricCipher).ifPresent(v -> properties.put("symmetricCipher", v));
+
+                Optional.ofNullable(encryptorEllipticCurve).ifPresent(v -> properties.put("ellipticCurve", v));
+
+                Optional.ofNullable(encryptorNonceLength).ifPresent(v -> properties.put("nonceLength", v));
+
+                Optional.ofNullable(encryptorSharedKeyLength).ifPresent(v -> properties.put("sharedKeyLength", v));
+
+                encryptorConfig.setProperties(properties);
+            }
+        }
+
+        this.keyEncryptor = keyEncryptorFactory.create(encryptorConfig);
+
+        return execute();
+    }
+
+    public CliResult execute() throws IOException {
+        final ArgonOptions argonOptions = argonOptions();
+        final List<String> passwords = passwords();
+        final Path keypath = privateKeyPath();
+
+        final KeyDataConfig keyDataConfig = JaxbUtil.unmarshal(Files.newInputStream(keypath), KeyDataConfig.class);
+        final PrivateKey privateKey = this.getExistingKey(keyDataConfig, passwords);
+
+        final String newPassword = passwordReader.requestUserPassword();
+
+        final KeyDataConfig updatedKey;
+        if (newPassword.isEmpty()) {
+            final PrivateKeyData privateKeyData =
+                    new PrivateKeyData(privateKey.encodeToBase64(), null, null, null, null);
+            updatedKey = new KeyDataConfig(privateKeyData, PrivateKeyType.UNLOCKED);
+        } else {
+            final PrivateKeyData privateKeyData = keyEncryptor.encryptPrivateKey(privateKey, newPassword, argonOptions);
+            updatedKey = new KeyDataConfig(privateKeyData, PrivateKeyType.LOCKED);
+        }
+
+        // write the key to file
+        Files.write(keypath, JaxbUtil.marshalToString(updatedKey).getBytes(UTF_8));
+        SystemAdapter.INSTANCE.out().println("Private key at " + keypath.toString() + " updated.");
+
+        //        return Optional.empty();
+        // TODO(cjh) compare with existing behaviour
+        return new CliResult(1, true, null);
+    }
+
+    private PrivateKey getExistingKey(final KeyDataConfig kdc, final List<String> passwords) {
+
+        if (kdc.getType() == PrivateKeyType.UNLOCKED) {
+            byte[] privateKeyData = Base64.getDecoder().decode(kdc.getValue().getBytes(UTF_8));
+            return PrivateKey.from(privateKeyData);
+        } else {
+
+            for (final String pass : passwords) {
+                try {
+                    return PrivateKey.from(keyEncryptor.decryptPrivateKey(kdc.getPrivateKeyData(), pass).getKeyBytes());
+                } catch (final Exception e) {
+                    LOGGER.debug("Password failed to decrypt. Trying next if available.");
+                }
+            }
+
+            throw new IllegalArgumentException("Locked key but no valid password given");
+        }
+    }
+
+    private Path privateKeyPath() {
+        ////      TODO(cjh)shouldn't need this as the option should be marked as required - CHECK!
+        //        if (privateKeyPath == null) {
+        //            throw new IllegalArgumentException("Private key path cannot be null when updating key password");
+        //        }
+
+        if (Files.notExists(privateKeyPath)) {
+            throw new IllegalArgumentException("Private key path must exist when updating key password");
+        }
+
+        return privateKeyPath;
+    }
+
+    private List<String> passwords() throws IOException {
+        if (password != null) {
+            return singletonList(password);
+        } else if (passwordFile != null) {
+            return Files.readAllLines(passwordFile);
+        } else {
+            return emptyList();
+        }
+    }
+
+    private ArgonOptions argonOptions() {
+        return new ArgonOptions(
+                algorithm, Integer.valueOf(iterations), Integer.valueOf(memory), Integer.valueOf(parallelism));
+    }
+}
--- tessera-dist/tessera-launcher/src/main/java/com/quorum/tessera/launcher/PicoCliDelegate.java
+++ tessera-dist/tessera-launcher/src/main/java/com/quorum/tessera/launcher/PicoCliDelegate.java
@@ -4,6 +4,8 @@ import com.quorum.tessera.cli.CLIExceptionCapturer;
 import com.quorum.tessera.cli.CliResult;
 import com.quorum.tessera.cli.parsers.ConfigConverter;
 import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.cli.KeyUpdateCommand;
+import com.quorum.tessera.config.cli.KeyUpdateCommandFactory;
 import com.quorum.tessera.config.cli.OverrideUtil;
 import com.quorum.tessera.key.generation.KeyGenCommand;
 import com.quorum.tessera.key.generation.TesseraCommand;
@@ -14,7 +16,7 @@ import picocli.CommandLine.Model.OptionSpec;
 import java.util.Map;
 
 public class PicoCliDelegate {
-    public CliResult execute(String[] args) {
+    public CliResult execute(String[] args) throws Exception {
         final CommandSpec command = CommandSpec.forAnnotatedObject(TesseraCommand.class);
 
         // TODO(cjh) add options and positional parameters
@@ -41,10 +43,11 @@ public class PicoCliDelegate {
                     command.addOption(optionBuilder.build());
                 });
 
-        CommandLine keyGenCommandLine = new CommandLine(KeyGenCommand.class);
-
         command.addSubcommand(null, new CommandLine(CommandLine.HelpCommand.class));
-        command.addSubcommand(null, keyGenCommandLine);
+        command.addSubcommand(null, new CommandLine(KeyGenCommand.class));
+
+        final CommandLine.IFactory keyUpdateCommandFactory = new KeyUpdateCommandFactory();
+        command.addSubcommand(null, new CommandLine(KeyUpdateCommand.class, keyUpdateCommandFactory));
 
         final CommandLine commandLine = new CommandLine(command);
         final CLIExceptionCapturer mapper = new CLIExceptionCapturer();
@@ -57,53 +60,56 @@ public class PicoCliDelegate {
 
         commandLine.execute(args);
 
-//        try {
-//            CommandLine.ParseResult pr = commandLine.parseArgs(args);
-//            if (CommandLine.printHelpIfRequested(pr)) {
-//                return new CliResult(0, true, null);
-//            }
-//
-//            if (pr.hasSubcommand()) {
-//                CommandLine.ParseResult subPr = pr.subcommand();
-//                CommandSpec subCommand = subPr.commandSpec();
-//
-//                List<String> matchedStr = new ArrayList<>();
-//                List<CommandLine.Model.ArgSpec> matchedArgs = subPr.matchedArgs();
-//
-//                matchedArgs
-//                    .stream()
-//                    .map(CommandLine.Model.ArgSpec::originalStringValues)
-//                    .forEachOrdered(matchedStr::addAll);
-//
-////                List<CommandLine> commands = subPr.asCommandLineList();
-////                if (commands.size() != 1) {
-////                    throw new RuntimeException("at the moment exactly 1 subcommand has to be specified");
-////                }
-////
-////                commands.get(0).execute();
-//
-//                List<String> originalArgs = subPr.originalArgs();
-//
-//                if ("keygen".equals(subCommand.name())) {
-//                    System.out.println("name = " + subCommand.name());
-//                    keyGenCommandLine.execute(originalArgs.toArray(new String[0]));
-//                }
-//            }
+        // if an exception occurred, throw it to to the upper levels where it gets handled
+        if (mapper.getThrown() != null) {
+            throw mapper.getThrown();
+        }
 
-//            int count = pr.matchedOptionValue('c', 1);
-//            List<File> files = pr.matchedPositionalValue(0, Collections.<File>emptyList());
-//            for (File f : files) {
-//                for (int i = 0; i < count; i++) {
-//                    System.out.printf("%d: %s%n", i, f);
-//                }
-//            }
-//        } catch (CommandLine.ParameterException invalidInput) {
-//            System.err.println(invalidInput.getMessage());
-//            invalidInput.getCommandLine().usage(System.err);
-//        }
+        //        try {
+        //            CommandLine.ParseResult pr = commandLine.parseArgs(args);
+        //            if (CommandLine.printHelpIfRequested(pr)) {
+        //                return new CliResult(0, true, null);
+        //            }
+        //
+        //            if (pr.hasSubcommand()) {
+        //                CommandLine.ParseResult subPr = pr.subcommand();
+        //                CommandSpec subCommand = subPr.commandSpec();
+        //
+        //                List<String> matchedStr = new ArrayList<>();
+        //                List<CommandLine.Model.ArgSpec> matchedArgs = subPr.matchedArgs();
+        //
+        //                matchedArgs
+        //                    .stream()
+        //                    .map(CommandLine.Model.ArgSpec::originalStringValues)
+        //                    .forEachOrdered(matchedStr::addAll);
+        //
+        ////                List<CommandLine> commands = subPr.asCommandLineList();
+        ////                if (commands.size() != 1) {
+        ////                    throw new RuntimeException("at the moment exactly 1 subcommand has to be specified");
+        ////                }
+        ////
+        ////                commands.get(0).execute();
+        //
+        //                List<String> originalArgs = subPr.originalArgs();
+        //
+        //                if ("keygen".equals(subCommand.name())) {
+        //                    System.out.println("name = " + subCommand.name());
+        //                    keyGenCommandLine.execute(originalArgs.toArray(new String[0]));
+        //                }
+        //            }
+
+        //            int count = pr.matchedOptionValue('c', 1);
+        //            List<File> files = pr.matchedPositionalValue(0, Collections.<File>emptyList());
+        //            for (File f : files) {
+        //                for (int i = 0; i < count; i++) {
+        //                    System.out.printf("%d: %s%n", i, f);
+        //                }
+        //            }
+        //        } catch (CommandLine.ParameterException invalidInput) {
+        //            System.err.println(invalidInput.getMessage());
+        //            invalidInput.getCommandLine().usage(System.err);
+        //        }
 
         return new CliResult(1, true, null);
     }
-
-
 }
