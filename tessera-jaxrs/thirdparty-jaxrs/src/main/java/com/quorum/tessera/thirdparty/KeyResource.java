ebc1e456934f86e4940892c97124a907a167b8b8
Nam Truong <nam.p.truong@outlook.com>
2019-12-03 11:55:21 +0000

    Add api to thirdparty to return list of public keys managed by the enclave (#936)

---
+++ tessera-jaxrs/thirdparty-jaxrs/src/main/java/com/quorum/tessera/thirdparty/KeyResource.java
@@ -0,0 +1,48 @@
+package com.quorum.tessera.thirdparty;
+
+import com.quorum.tessera.admin.ConfigService;
+import com.quorum.tessera.encryption.PublicKey;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Set;
+
+@Api
+@Path("/keys")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class KeyResource {
+
+    private final ConfigService configService;
+
+    public KeyResource(ConfigService configService) {
+        this.configService = configService;
+    }
+
+    @GET
+    @ApiOperation(value = "Fetch local public keys managed by the enclave")
+    @ApiResponses({@ApiResponse(code = 200, message = "Managed public keys")})
+    public Response getPublicKeys() {
+        Set<PublicKey> publicKeys = configService.getPublicKeys();
+
+        final JsonArrayBuilder keyBuilder = Json.createArrayBuilder();
+
+        publicKeys.stream()
+                .map(key -> Json.createObjectBuilder().add("key", key.encodeToBase64()).build())
+                .forEach(keyBuilder::add);
+
+        final String output = Json.createObjectBuilder().add("keys", keyBuilder.build()).build().toString();
+
+        return Response.status(Response.Status.OK).entity(output).build();
+    }
+}
