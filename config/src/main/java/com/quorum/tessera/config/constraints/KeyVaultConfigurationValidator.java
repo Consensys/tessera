cc529a382e1f8528288696fecb185ef62f528c97
chris-j-h <39617426+chris-j-h@users.noreply.github.com>
2020-01-10 15:32:01 +0000

    Fix bug where validation fails even when defaultKeyVaultConfig is set (#963)
    Co-authored-by: Mark Lowe <melowe@users.noreply.github.com>

--- config/src/main/java/com/quorum/tessera/config/constraints/KeyVaultConfigurationValidator.java
+++ config/src/main/java/com/quorum/tessera/config/constraints/KeyVaultConfigurationValidator.java
@@ -1,42 +1,55 @@
 package com.quorum.tessera.config.constraints;
 
 import com.quorum.tessera.config.KeyConfiguration;
+import com.quorum.tessera.config.KeyVaultType;
 import com.quorum.tessera.config.keypairs.AzureVaultKeyPair;
 import com.quorum.tessera.config.keypairs.HashicorpVaultKeyPair;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.util.Optional;
 
-public class KeyVaultConfigurationValidator implements ConstraintValidator<ValidKeyVaultConfiguration, KeyConfiguration> {
+public class KeyVaultConfigurationValidator
+        implements ConstraintValidator<ValidKeyVaultConfiguration, KeyConfiguration> {
 
     @Override
     public boolean isValid(KeyConfiguration keyConfiguration, ConstraintValidatorContext cvc) {
 
-        //return true so that only NotNull annotation creates violation
-        if(keyConfiguration == null) {
+        // return true so that only NotNull annotation creates violation
+        if (keyConfiguration == null) {
             return true;
         }
 
-        boolean isUsingAzureVaultKeys = keyConfiguration.getKeyData()
-            .stream()
-            .anyMatch(keyPair -> keyPair instanceof AzureVaultKeyPair);
+        boolean isUsingAzureVaultKeys =
+                keyConfiguration.getKeyData().stream().anyMatch(keyPair -> keyPair instanceof AzureVaultKeyPair);
 
-        if(isUsingAzureVaultKeys && keyConfiguration.getAzureKeyVaultConfig() == null) {
+        boolean hasAzureKeyVaultConfig =
+                keyConfiguration.getAzureKeyVaultConfig() != null
+                        || Optional.ofNullable(keyConfiguration.getKeyVaultConfig())
+                                .filter((c) -> c.getKeyVaultType().equals(KeyVaultType.AZURE))
+                                .isPresent();
+
+        if (isUsingAzureVaultKeys && !hasAzureKeyVaultConfig) {
             cvc.disableDefaultConstraintViolation();
             cvc.buildConstraintViolationWithTemplate("{ValidKeyVaultConfiguration.azure.message}")
-                .addConstraintViolation();
+                    .addConstraintViolation();
 
             return false;
         }
 
-        boolean isUsingHashicorpVaultKeys = keyConfiguration.getKeyData()
-            .stream()
-            .anyMatch(keyPair -> keyPair instanceof HashicorpVaultKeyPair);
+        boolean isUsingHashicorpVaultKeys =
+                keyConfiguration.getKeyData().stream().anyMatch(keyPair -> keyPair instanceof HashicorpVaultKeyPair);
 
-        if(isUsingHashicorpVaultKeys && keyConfiguration.getHashicorpKeyVaultConfig() == null) {
+        boolean hasHashicorpKeyVaultConfig =
+                keyConfiguration.getHashicorpKeyVaultConfig() != null
+                        || Optional.ofNullable(keyConfiguration.getKeyVaultConfig())
+                                .filter((c) -> c.getKeyVaultType().equals(KeyVaultType.HASHICORP))
+                                .isPresent();
+
+        if (isUsingHashicorpVaultKeys && !hasHashicorpKeyVaultConfig) {
             cvc.disableDefaultConstraintViolation();
             cvc.buildConstraintViolationWithTemplate("{ValidKeyVaultConfiguration.hashicorp.message}")
-                .addConstraintViolation();
+                    .addConstraintViolation();
 
             return false;
         }
