c13fc44ee82154c1acd1cc8e4a29555cec6ede42
Peter Fox <peter.rd.fox@gmail.com>
2020-01-24 15:53:37 +0000

    Add a configurable timeout for "resend"
    Adds a new property called "resendWaitTime" to the P2P server
    configuration. This property denotes, in milliseconds, the timeout
    period for reading a response when calling the "/resend" API endpoint.
    
    By default, a 2 hour period was chosen as long enough for a medium
    sized database (about 500k transactions).

--- tessera-jaxrs/sync-jaxrs/src/main/java/com/quorum/tessera/p2p/RestResendClientFactory.java
+++ tessera-jaxrs/sync-jaxrs/src/main/java/com/quorum/tessera/p2p/RestResendClientFactory.java
@@ -2,6 +2,7 @@ package com.quorum.tessera.p2p;
 
 import com.quorum.tessera.config.CommunicationType;
 import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.util.IntervalPropertyHelper;
 import com.quorum.tessera.jaxrs.client.ClientFactory;
 import com.quorum.tessera.ssl.context.ClientSSLContextFactory;
 import com.quorum.tessera.ssl.context.SSLContextFactory;
@@ -13,12 +14,15 @@ import javax.ws.rs.client.Client;
 public class RestResendClientFactory implements ResendClientFactory {
 
     public ResendClient create(final Config config) {
+        final String resendWaitTime =
+                new IntervalPropertyHelper(config.getP2PServerConfig().getProperties()).resendWaitTime();
+
         final SSLContextFactory clientSSLContextFactory = ClientSSLContextFactory.create();
 
         final ClientFactory clientFactory = new ClientFactory(clientSSLContextFactory);
         final Client client = clientFactory.buildFrom(config.getP2PServerConfig());
 
-        client.property("jersey.config.client.readTimeout", "36000000"); // 10 hours
+        client.property("jersey.config.client.readTimeout", resendWaitTime);
         return new RestResendClient(client);
     }
 
