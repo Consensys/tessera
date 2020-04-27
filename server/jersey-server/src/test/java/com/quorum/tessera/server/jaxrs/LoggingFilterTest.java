02a51f82a16f108088a065d810a9ea986b27359f
Satpal <28562234+SatpalSandhu61@users.noreply.github.com>
2020-04-27 16:07:10 +0100

    Fix NPE on logged error (#1042)
    * Fix issue where Null Pointer Exception thrown after error logged for invalid resource request.
    Also correction to a couple of LOGGER class names.
    
    * Adding unit test.

--- server/jersey-server/src/test/java/com/quorum/tessera/server/jaxrs/LoggingFilterTest.java
+++ server/jersey-server/src/test/java/com/quorum/tessera/server/jaxrs/LoggingFilterTest.java
@@ -6,18 +6,17 @@ import org.junit.Test;
 
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Response;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class LoggingFilterTest {
 
     private LoggingFilter loggingFilter;
 
-    public LoggingFilterTest() {
-    }
+    public LoggingFilterTest() {}
 
     @Before
     public void setUp() {
@@ -33,9 +32,8 @@ public class LoggingFilterTest {
     public void filterRequest() {
         ContainerRequestContext request = mock(ContainerRequestContext.class);
         loggingFilter.filter(request);
-        //Very silly test 
+        // Very silly test
         assertThat(loggingFilter).isNotNull();
-
     }
 
     @Test
@@ -44,10 +42,26 @@ public class LoggingFilterTest {
         ContainerResponseContext response = mock(ContainerResponseContext.class);
         Response.StatusType statusInfo = mock(Response.StatusType.class);
         when(response.getStatusInfo()).thenReturn(statusInfo);
-        loggingFilter.filter(request,response);
-        
-        //Very silly test 
-        assertThat(loggingFilter).isNotNull();
+        loggingFilter.filter(request, response);
 
+        // Very silly test
+        assertThat(loggingFilter).isNotNull();
+    }
+
+    @Test
+    public void filterNullResourceInfo() {
+        ContainerRequestContext request = mock(ContainerRequestContext.class);
+        loggingFilter.setResourceInfo(null);
+        loggingFilter.filter(request); // if this doesn't throw exception then test passed
+    }
+
+    @Test
+    public void filterNullResourceInfoClass() {
+        ResourceInfo resourceInfo = mock(ResourceInfo.class);
+        loggingFilter.setResourceInfo(resourceInfo);
+        when(resourceInfo.getResourceClass()).thenReturn(null);
+
+        ContainerRequestContext request = mock(ContainerRequestContext.class);
+        loggingFilter.filter(request); // if this doesn't throw exception then test passed
     }
 }
