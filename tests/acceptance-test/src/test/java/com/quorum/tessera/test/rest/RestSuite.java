45b99745f71495853328e055e092d1a55e9d4352
Mark Lowe <melowe.quorum@gmail.com>
2020-06-03 13:06:01 +0100

    Reactivate commented tests

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuite.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/rest/RestSuite.java
@@ -1,21 +1,23 @@
 package com.quorum.tessera.test.rest;
 
 
+import com.quorum.tessera.test.CucumberRawIT;
+import com.quorum.tessera.test.CucumberRestIT;
 import suite.TestSuite;
 
 @TestSuite.SuiteClasses({
-//    MultipleKeyNodeIT.class,
-//    DeleteIT.class,
-//    PushIT.class,
-//    ReceiveIT.class,
-//    ReceiveRawIT.class,
-//    ResendAllIT.class,
-//    ResendIndividualIT.class,
+    MultipleKeyNodeIT.class,
+    DeleteIT.class,
+    PushIT.class,
+    ReceiveIT.class,
+    ReceiveRawIT.class,
+    ResendAllIT.class,
+    ResendIndividualIT.class,
     SendIT.class,
-//    SendRawIT.class,
-//    P2PRestAppIT.class,
-//    TransactionForwardingIT.class,
-//    CucumberRestIT.class,
-//    CucumberRawIT.class
+    SendRawIT.class,
+    P2PRestAppIT.class,
+    TransactionForwardingIT.class,
+    CucumberRestIT.class,
+    CucumberRawIT.class
 })
 public abstract class RestSuite {}
