605d2731159506dc33bbfcf367ca410d1aa9dd09
Mark Lowe <melowe.quorum@gmail.com>
2020-06-09 16:24:47 +0100

    additional teststo cover different builder scenerios.

--- tessera-core/src/test/java/com/quorum/tessera/transaction/ReceiveResponseTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/transaction/ReceiveResponseTest.java
@@ -3,6 +3,7 @@ package com.quorum.tessera.transaction;
 import com.quorum.tessera.enclave.PrivacyMode;
 import org.junit.Test;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ReceiveResponseTest {
@@ -18,4 +19,28 @@ public class ReceiveResponseTest {
         assertThat(result.getPrivacyMode()).isEqualTo(PrivacyMode.STANDARD_PRIVATE);
     }
 
+    @Test(expected = NullPointerException.class)
+    public void execHashRequiredFromPrivacyStateValidation() {
+        byte[] someData = "SomeData".getBytes();
+        ReceiveResponse.Builder.create()
+            .withPrivacyMode(PrivacyMode.PRIVATE_STATE_VALIDATION)
+            .withUnencryptedTransactionData(someData).build();
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void privacyModeIsRequired() {
+        byte[] someData = "SomeData".getBytes();
+        ReceiveResponse.Builder.create()
+            .withUnencryptedTransactionData(someData).build();
+
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void unencryptedTransactionDataIsRequired() {
+        ReceiveResponse.Builder.create()
+            .withPrivacyMode(PrivacyMode.STANDARD_PRIVATE)
+            .build();
+
+    }
 }
