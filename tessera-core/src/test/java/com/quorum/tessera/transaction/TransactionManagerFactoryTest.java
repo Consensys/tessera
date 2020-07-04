f72e53e2d543660b90a03804bb737ca7e52e7f9c
Mark Lowe <melowe.quorum@gmail.com>
2020-07-04 17:53:42 +0100

    provide TransactionManagerFactory . Make PartyInfoServiceFactory work the same way.

---
+++ tessera-core/src/test/java/com/quorum/tessera/transaction/TransactionManagerFactoryTest.java
@@ -0,0 +1,38 @@
+package com.quorum.tessera.transaction;
+
+import com.quorum.tessera.config.CommunicationType;
+import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.JdbcConfig;
+import com.quorum.tessera.config.ServerConfig;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class TransactionManagerFactoryTest {
+
+    @Test
+    public void create() {
+
+        TransactionManagerFactory result = TransactionManagerFactory.create();
+        assertThat(result).isNotNull();
+
+        Config config = mock(Config.class);
+        ServerConfig serverConfig = mock(ServerConfig.class);
+        when(serverConfig.getCommunicationType()).thenReturn(CommunicationType.REST);
+        when(config.getP2PServerConfig()).thenReturn(serverConfig);
+
+        JdbcConfig jdbcConfig = mock(JdbcConfig.class);
+        when(jdbcConfig.getUsername()).thenReturn("junit");
+        when(jdbcConfig.getPassword()).thenReturn("junit");
+        when(jdbcConfig.getUrl()).thenReturn("jdbc:h2:mem:junit");
+        when(config.getJdbcConfig()).thenReturn(jdbcConfig);
+
+        TransactionManager transactionManager = result.create(config);
+        assertThat(transactionManager).isNotNull();
+
+
+    }
+
+}
--- tessera-jaxrs/sync-jaxrs/src/test/java/com/quorum/tessera/p2p/MockPartyInfoServiceFactory.java
+++ tessera-jaxrs/sync-jaxrs/src/test/java/com/quorum/tessera/p2p/MockPartyInfoServiceFactory.java
@@ -1,6 +1,7 @@
 package com.quorum.tessera.p2p;
 
 
+import com.quorum.tessera.config.Config;
 import com.quorum.tessera.partyinfo.PartyInfoService;
 import com.quorum.tessera.partyinfo.PartyInfoServiceFactory;
 
@@ -8,7 +9,7 @@ import static org.mockito.Mockito.mock;
 
 public class MockPartyInfoServiceFactory implements PartyInfoServiceFactory {
     @Override
-    public PartyInfoService partyInfoService() {
+    public PartyInfoService create(Config config) {
         return mock(PartyInfoService.class);
     }
 
