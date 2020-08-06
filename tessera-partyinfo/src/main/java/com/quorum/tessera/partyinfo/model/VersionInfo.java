a0f17179a1bef9013f87669fb0175ba9bee63a70
namtruong <nam.p.truong@outlook.com>
2020-08-06 01:11:02 +0100

    Introduce VersionInfo object where versions information can be stored against a party

---
+++ tessera-partyinfo/src/main/java/com/quorum/tessera/partyinfo/model/VersionInfo.java
@@ -0,0 +1,12 @@
+package com.quorum.tessera.partyinfo.model;
+
+import java.util.Set;
+
+public interface VersionInfo {
+
+    Set<String> supportedApiVersions();
+
+    static VersionInfo from(Set<String> versions) {
+        return () -> versions;
+    }
+}
