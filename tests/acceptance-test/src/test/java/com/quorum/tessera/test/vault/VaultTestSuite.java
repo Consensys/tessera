7ea3780e72df1462c7281e4aef15995a2ea79edd
chris-j-h <chrishounsom@icloud.com>
2020-08-06 09:27:48 +0100

    Disable Azure  key vault ITs Disabled as Azure tests do not currently work due to challenge mocking the authentication protocol used by new Azure SDK libs

--- tests/acceptance-test/src/test/java/com/quorum/tessera/test/vault/VaultTestSuite.java
+++ tests/acceptance-test/src/test/java/com/quorum/tessera/test/vault/VaultTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     RunAwsIT.class,
- //   RunAzureIT.class,
+ //   RunAzureIT.class, // disabled as Azure tests do not currently work due to challenge mocking the authentication protocol used by new Azure SDK libs
 })
 public class VaultTestSuite {
 
