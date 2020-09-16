f7f53f3895f067e67137af29c73c629ac213f386
namtruong <nam.p.truong@outlook.com>
2020-09-16 11:26:01 +0100

    Include version v2 in ApiVersion

--- shared/src/test/java/com/quorum/tessera/version/EnhancedPrivacyVersionTest.java
+++ shared/src/test/java/com/quorum/tessera/version/EnhancedPrivacyVersionTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class VersionTwoTest {
+public class EnhancedPrivacyVersionTest {
 
     private EnhancedPrivacyVersion version = new EnhancedPrivacyVersion();
 
