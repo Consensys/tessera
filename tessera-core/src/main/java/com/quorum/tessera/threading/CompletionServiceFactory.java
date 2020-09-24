f2578d6218b0979f231e3349214812f3ce6de6e6
chris-j-h <chrishounsom@icloud.com>
2020-09-24 17:05:34 +0100

    Cleanup the CompletionService if there is a batch publish exception

--- tessera-core/src/main/java/com/quorum/tessera/threading/CompletionServiceFactory.java
+++ tessera-core/src/main/java/com/quorum/tessera/threading/CompletionServiceFactory.java
@@ -3,14 +3,11 @@ package com.quorum.tessera.threading;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorCompletionService;
-import java.util.concurrent.Executors;
 
 public class CompletionServiceFactory {
 
-    private final Executor executor = Executors.newCachedThreadPool();
-
     // TODO(cjh) make generic
-    public CompletionService<Void> create() {
+    public CompletionService<Void> create(Executor executor) {
         return new ExecutorCompletionService<>(executor);
     }
 
--- tessera-jaxrs/transaction-jaxrs/src/main/java/com/quorum/tessera/q2t/AsyncBatchPayloadPublisher.java
+++ tessera-jaxrs/transaction-jaxrs/src/main/java/com/quorum/tessera/q2t/AsyncBatchPayloadPublisher.java
@@ -14,12 +14,14 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 import java.util.concurrent.CompletionService;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class AsyncBatchPayloadPublisher implements BatchPayloadPublisher {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(AsyncBatchPayloadPublisher.class);
+
+    private final Executor executor = Executors.newCachedThreadPool();
 
     private final CompletionServiceFactory completionServiceFactory;
 
@@ -36,7 +38,8 @@ public class AsyncBatchPayloadPublisher implements BatchPayloadPublisher {
     /**
      * Asynchronously strips (leaving data intended only for that particular recipient) and publishes the payload to
      * each recipient identified by the provided keys.
-     * This method blocks until all pushes return successfully; if a push fails with an exception, the method exits
+     *
+     * <p>This method blocks until all pushes return successfully; if a push fails with an exception, the method exits
      * immediately and does not wait for the remaining responses.
      *
      * @param payload the payload object to be stripped and pushed
@@ -44,37 +47,73 @@ public class AsyncBatchPayloadPublisher implements BatchPayloadPublisher {
      */
     @Override
     public void publishPayload(EncodedPayload payload, List<PublicKey> recipientKeys) {
-        final CompletionService<Void> completionService = completionServiceFactory.create();
+        final CompletionService<Void> completionService = completionServiceFactory.create(executor);
 
-        // asynchronously submit all publishes
-        List<Future<Void>> futures =
-                recipientKeys.stream()
-                        .map(
-                                recipient ->
-                                        completionService.submit(
-                                                () -> {
-                                                    final EncodedPayload outgoing =
-                                                            encoder.forRecipient(payload, recipient);
-                                                    publisher.publishPayload(outgoing, recipient);
-                                                    return null;
-                                                }))
-                        .collect(Collectors.toList());
+        long notCompletedCount = recipientKeys
+            .stream()
+            .map(recipient -> completionService.submit(
+                () -> {
+                    final EncodedPayload outgoing = encoder.forRecipient(payload, recipient);
+                    publisher.publishPayload(outgoing, recipient);
+                    return null;
+                }))
+            .count();
 
-        // wait for publishes to complete, exiting if at least one returns an error
-        for (int i = 0; i < futures.size(); i++) {
-            try {
-                completionService.take().get();
-            } catch (ExecutionException e) {
-                Throwable cause = e.getCause();
-                LOGGER.debug("Unable to publish payload, exiting async publish", cause);
-                if (cause instanceof KeyNotFoundException) {
-                    throw (KeyNotFoundException) cause;
-                }
-                throw new BatchPublishPayloadException(cause);
-            } catch (InterruptedException e) {
-                LOGGER.debug("Async payload publish interrupted", e);
-                throw new BatchPublishPayloadException(e);
+        try {
+            while (notCompletedCount > 0) {
+                notCompletedCount--;
+                waitForNextCompletion(completionService);
             }
+        } catch(Exception e) {
+            LOGGER.debug("Async batch public exited early, cleaning up", e);
+            long n = notCompletedCount;
+            executor.execute(() -> drainN(completionService, n));
+        }
+    }
+
+    /**
+     * Waits until a task submitted to the CompletionService completes.
+     *
+     * <p>If an {@link ExecutionException} or {@link InterruptedException} is encountered,
+     * the method throws an exception and exits without waiting for the remaining tasks to complete.
+     *
+     * <p>To prevent a memory leak, the caller should remove the remaining tasks from the CompletionService after this
+     * method exits.
+     *
+     * @param completionService a service with submitted tasks
+     */
+    private void waitForNextCompletion(CompletionService<Void> completionService) {
+        try {
+            completionService.take().get();
+        } catch (ExecutionException e) {
+            Throwable cause = e.getCause();
+            LOGGER.debug("Unable to publish payload, exiting async publish", cause);
+            if (cause instanceof KeyNotFoundException) {
+                throw (KeyNotFoundException) cause;
+            }
+            throw new BatchPublishPayloadException(cause);
+        } catch (InterruptedException e) {
+            LOGGER.debug("Async payload publish interrupted", e);
+            throw new BatchPublishPayloadException(e);
+        }
+    }
+
+    /**
+     * Removes n tasks from the CompletionService, waiting for each to complete in turn.
+     *
+     * <p>If an exception is encountered, it is ignored so that the remaining tasks can be removed.
+     *
+     * @param completionService a service with submitted tasks
+     * @param n the number of tasks to drain from the completionService
+     */
+    private void drainN(CompletionService<Void> completionService, long n) {
+        while (n > 0) {
+            try {
+                waitForNextCompletion(completionService);
+            } catch (Exception e) {
+                LOGGER.debug("Unable to drain task from CompletionService", e);
+            }
+            n--;
         }
     }
 }
