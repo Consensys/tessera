2cf1620f8ab2ae9de4e4957831713b6aa1b1b17d
Mark Lowe <melowe.quorum@gmail.com>
2020-10-05 15:58:01 +0100

    use v2 as psv supporting version. Use values defined by apiversion lookup to provide supported versions as well as current version. Add integration test to unsure it all works

--- tessera-jaxrs/common-jaxrs/src/main/java/com/quorum/tessera/api/Version.java
+++ tessera-jaxrs/common-jaxrs/src/main/java/com/quorum/tessera/api/Version.java
@@ -1,8 +1,16 @@
 package com.quorum.tessera.api;
 
+import com.quorum.tessera.version.ApiVersion;
+
+import java.util.Comparator;
+
 public class Version {
 
     public static String getVersion() {
-        return "1.0.0";
+        return ApiVersion.versions()
+            .stream()
+            .sorted(Comparator.comparing(String::toString).reversed())
+            .findFirst()
+            .get();
     }
 }
