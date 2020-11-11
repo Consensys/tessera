a31c603cd139ec097676197f5680fd5f0dfa7058
Mark Lowe <melowe.quorum@gmail.com>
2020-11-11 12:36:14 +0000

    Addional tests for DAO providers

---
+++ tessera-data/src/test/java/com/quorum/tessera/data/EncryptedTransactionDAOProviderTest.java
@@ -0,0 +1,71 @@
+package com.quorum.tessera.data;
+
+import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.ConfigFactory;
+import com.quorum.tessera.config.JdbcConfig;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Collection;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(Parameterized.class)
+public class EncryptedTransactionDAOProviderTest {
+
+    private boolean autocreateTables;
+
+    public EncryptedTransactionDAOProviderTest(boolean autocreateTables) {
+        this.autocreateTables = autocreateTables;
+    }
+
+    @Test
+    public void defaultConstructorForCoverage() {
+        assertThat(new EncryptedTransactionDAOProvider()).isNotNull();
+    }
+
+    @Test
+    public void provides() {
+        try(
+            var mockedConfigFactory = mockStatic(ConfigFactory.class);
+            var mockedDataSourceFactory = mockStatic(DataSourceFactory.class);
+            var mockedPersistence = mockStatic(Persistence.class)
+        ) {
+
+            mockedPersistence.when(() -> Persistence.createEntityManagerFactory(anyString(), anyMap())).thenReturn(mock(EntityManagerFactory.class));
+
+            Config config = mock(Config.class);
+            JdbcConfig jdbcConfig = mock(JdbcConfig.class);
+            when(jdbcConfig.isAutoCreateTables()).thenReturn(autocreateTables);
+            when(config.getJdbcConfig()).thenReturn(jdbcConfig);
+
+            ConfigFactory configFactory = mock(ConfigFactory.class);
+            when(configFactory.getConfig()).thenReturn(config);
+
+            mockedConfigFactory.when(ConfigFactory::create).thenReturn(configFactory);
+
+            mockedDataSourceFactory
+                .when(DataSourceFactory::create).thenReturn(mock(DataSourceFactory.class));
+
+
+            EncryptedTransactionDAO result = EncryptedTransactionDAOProvider.provider();
+            assertThat(result).isNotNull().isExactlyInstanceOf(EncryptedTransactionDAOImpl.class);
+
+            mockedPersistence.verify(() -> Persistence.createEntityManagerFactory(anyString(), anyMap()));
+            mockedPersistence.verifyNoMoreInteractions();
+            EncryptedTransactionDAOProvider.provider();
+        }
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Boolean> autoCreateTables() {
+        return List.of(true,false);
+    }
+}
