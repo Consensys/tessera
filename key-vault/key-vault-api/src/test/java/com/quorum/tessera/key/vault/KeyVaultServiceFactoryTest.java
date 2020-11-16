bbd5681e28926b605124adb62676d52f13b0a3f6
Mark Lowe <melowe.quorum@gmail.com>
2020-11-16 22:05:52 +0000

    Reactivate tests and add required add open stuff to module-info test

--- key-vault/key-vault-api/src/test/java/com/quorum/tessera/key/vault/KeyVaultServiceFactoryTest.java
+++ key-vault/key-vault-api/src/test/java/com/quorum/tessera/key/vault/KeyVaultServiceFactoryTest.java
@@ -1,24 +1,74 @@
 package com.quorum.tessera.key.vault;
 
 import com.quorum.tessera.config.KeyVaultType;
-import org.junit.Ignore;
 import org.junit.Test;
 
+import java.util.ServiceLoader;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
 public class KeyVaultServiceFactoryTest {
 
-    @Ignore
     @Test
     public void getInstance() {
-        KeyVaultServiceFactory keyVaultServiceFactory = KeyVaultServiceFactory.getInstance(KeyVaultType.AZURE);
 
-        assertThat(keyVaultServiceFactory).isExactlyInstanceOf(MockAzureKeyVaultServiceFactory.class);
+        for(KeyVaultType keyVaultType : KeyVaultType.values()) {
+
+            KeyVaultServiceFactory otherKeyVaultServiceFactory = mock(KeyVaultServiceFactory.class);
+            when(otherKeyVaultServiceFactory.getType())
+                .thenReturn(Stream.of(KeyVaultType.values()).filter(k -> k != keyVaultType).findAny().get());
+
+            KeyVaultServiceFactory expected = mock(KeyVaultServiceFactory.class);
+            when(expected.getType()).thenReturn(keyVaultType);
+
+            KeyVaultServiceFactory keyVaultServiceFactory;
+            try (var mockedStaticServiceLoader = mockStatic(ServiceLoader.class)) {
+
+                ServiceLoader<KeyVaultServiceFactory> serviceLoader = mock(ServiceLoader.class);
+                ServiceLoader.Provider<KeyVaultServiceFactory> provider = mock(ServiceLoader.Provider.class);
+                when(provider.get()).thenReturn(expected);
+
+                ServiceLoader.Provider<KeyVaultServiceFactory> otherProvider = mock(ServiceLoader.Provider.class);
+                when(otherProvider.get()).thenReturn(otherKeyVaultServiceFactory);
+
+                when(serviceLoader.stream()).thenReturn(Stream.of(provider,otherProvider).unordered());
+
+                mockedStaticServiceLoader.when(() -> ServiceLoader.load(KeyVaultServiceFactory.class))
+                    .thenReturn(serviceLoader);
+
+                keyVaultServiceFactory = KeyVaultServiceFactory.getInstance(keyVaultType);
+
+                verify(serviceLoader).stream();
+                verifyNoMoreInteractions(serviceLoader);
+
+                verify(provider).get();
+                verifyNoMoreInteractions(provider);
+
+                mockedStaticServiceLoader.verify(() -> ServiceLoader.load(KeyVaultServiceFactory.class));
+                mockedStaticServiceLoader.verifyNoMoreInteractions();
+            }
+
+            assertThat(keyVaultServiceFactory).isSameAs(expected);
+        }
+
     }
 
     @Test(expected = NoKeyVaultServiceFactoryException.class)
     public void instanceNotFound() {
-        KeyVaultServiceFactory.getInstance(null);
+        try (var mockedStaticServiceLoader = mockStatic(ServiceLoader.class)) {
+            ServiceLoader<KeyVaultServiceFactory> serviceLoader = mock(ServiceLoader.class);
+            ServiceLoader.Provider<KeyVaultServiceFactory> provider = mock(ServiceLoader.Provider.class);
+            when(provider.get()).thenReturn(mock(KeyVaultServiceFactory.class));
+            when(serviceLoader.stream()).thenReturn(Stream.of(provider));
+
+            mockedStaticServiceLoader.when(() -> ServiceLoader.load(KeyVaultServiceFactory.class))
+                .thenReturn(serviceLoader);
+
+            KeyVaultServiceFactory.getInstance(KeyVaultType.AZURE);
+        }
     }
 
 }
