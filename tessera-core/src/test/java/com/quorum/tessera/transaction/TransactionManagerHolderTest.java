7498121bfe662e6038a027c08cb9b1f7575c3adf
Mark Lowe <melowe.quorum@gmail.com>
2020-11-17 08:36:12 +0000

    Test that holder holds.

---
+++ tessera-core/src/test/java/com/quorum/tessera/transaction/TransactionManagerHolderTest.java
@@ -0,0 +1,25 @@
+package com.quorum.tessera.transaction;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+
+public class TransactionManagerHolderTest {
+    @Before
+    @After
+    public void clear() {
+        TransactionManagerHolder.INSTANCE.store(null);
+    }
+
+    @Test
+    public void storeAndGet() {
+        TransactionManagerHolder transactionManagerHolder = TransactionManagerHolder.INSTANCE;
+        TransactionManager transactionManager = mock(TransactionManager.class);
+        assertThat(transactionManagerHolder.store(transactionManager)).isSameAs(transactionManager);
+        assertThat(transactionManagerHolder.getTransactionManager()).containsSame(transactionManager);
+    }
+
+}
