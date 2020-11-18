80ef043fbfb24b000584da758b84c83673323ea2
Mark Lowe <melowe.quorum@gmail.com>
2020-11-18 10:40:52 +0000

    format to be readable.

--- tessera-jaxrs/sync-jaxrs/src/main/java/com/quorum/tessera/p2p/resend/RestResendClient.java
+++ tessera-jaxrs/sync-jaxrs/src/main/java/com/quorum/tessera/p2p/resend/RestResendClient.java
@@ -1,8 +1,5 @@
 package com.quorum.tessera.p2p.resend;
 
-import com.quorum.tessera.p2p.resend.ResendClient;
-import com.quorum.tessera.p2p.resend.ResendRequest;
-
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.MediaType;
@@ -21,7 +18,10 @@ public class RestResendClient implements ResendClient {
     public boolean makeResendRequest(final String targetUrl, final ResendRequest request) {
         final Entity<ResendRequest> outboundEntity = Entity.entity(request, MediaType.APPLICATION_JSON);
 
-        try (Response response = client.target(targetUrl).path("/resend").request().post(outboundEntity)) {
+        try (Response response = client.target(targetUrl)
+            .path("/resend")
+            .request()
+            .post(outboundEntity)) {
             return Response.Status.OK.getStatusCode() == response.getStatus();
         }
     }
