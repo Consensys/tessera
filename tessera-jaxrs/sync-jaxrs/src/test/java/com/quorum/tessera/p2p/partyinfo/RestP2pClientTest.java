264241ec4ab4b20fe06bbfd2b004241108d07f5d
Mark Lowe <melowe.quorum@gmail.com>
2020-11-18 12:39:58 +0000

    RestP2pClientTest

--- tessera-jaxrs/sync-jaxrs/src/test/java/com/quorum/tessera/p2p/partyinfo/RestP2pClientTest.java
+++ tessera-jaxrs/sync-jaxrs/src/test/java/com/quorum/tessera/p2p/partyinfo/RestP2pClientTest.java
@@ -1,91 +1,85 @@
-//package com.quorum.tessera.p2p.partyinfo;
-//
-//import com.quorum.tessera.jaxrs.mock.MockClient;
-//import com.quorum.tessera.jaxrs.mock.MockWebTarget;
-//import com.quorum.tessera.p2p.partyinfo.RestP2pClient;
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import javax.ws.rs.client.Entity;
-//import javax.ws.rs.client.Invocation;
-//import javax.ws.rs.core.Response;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.*;
-//
-//public class RestP2pClientTest {
-//
-//    private MockClient restClient;
-//
-//    private RestP2pClient client;
-//
-//    @Before
-//    public void onSetUp() {
-//        restClient = new MockClient();
-//        client = new RestP2pClient(restClient);
-//    }
-//
-//    @Test
-//    public void sendPartyInfo() {
-//
-//        MockWebTarget webTarget = restClient.getWebTarget();
-//        Invocation.Builder m = webTarget.getMockInvocationBuilder();
-//
-//        byte[] responseData = "Result".getBytes();
-//        Response response = mock(Response.class);
-//        when(response.readEntity(byte[].class)).thenReturn(responseData);
-//        when(response.getStatus()).thenReturn(200);
-//
-//        List<Entity> postedEntities = new ArrayList<>();
-//        doAnswer(
-//            (invocation) -> {
-//                postedEntities.add(invocation.getArgument(0));
-//                return response;
-//            })
-//            .when(m)
-//            .post(any(Entity.class));
-//
-//        String targetUrl = "http://somedomain.com";
-//        byte[] data = "Some Data".getBytes();
-//
-//        boolean outcome = client.sendPartyInfo(targetUrl, data);
-//
-//        assertThat(outcome).isTrue();
-//        assertThat(postedEntities).hasSize(1);
-//
-//        Entity entity = postedEntities.get(0);
-//        assertThat(entity.getMediaType()).isEqualTo(javax.ws.rs.core.MediaType.APPLICATION_OCTET_STREAM_TYPE);
-//        assertThat(entity.getEntity()).isSameAs(data);
-//
-//        verify(response).readEntity(byte[].class);
-//    }
-//
-//
-//
-//    @Test
-//    public void sendPartyInfoReturns400() {
-//        MockWebTarget webTarget = restClient.getWebTarget();
-//
-//        Invocation.Builder m = webTarget.getMockInvocationBuilder();
-//        byte[] responseData = "Result".getBytes();
-//
-//        Response response = mock(Response.class);
-//        when(response.readEntity(byte[].class)).thenReturn(responseData);
-//        when(response.getStatus()).thenReturn(400);
-//
-//        when(m.post(any(Entity.class))).thenReturn(response);
-//
-//        String targetUrl = "http://somedomain.com";
-//        byte[] data = "Some Data".getBytes();
-//
-//        boolean outcome = client.sendPartyInfo(targetUrl, data);
-//
-//        assertThat(outcome).isFalse();
-//    }
-//
-//
-//
-//}
+package com.quorum.tessera.p2p.partyinfo;
+
+import com.quorum.tessera.p2p.resend.ResendRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@RunWith(Parameterized.class)
+public class RestP2pClientTest {
+
+    private Response.Status expectedResponseStatus;
+
+    public RestP2pClientTest(Response.Status expectedResponseStatus) {
+        this.expectedResponseStatus = expectedResponseStatus;
+    }
+
+    @Test
+    public void sendPartyInfo() {
+        try(var entityMockedStatic = mockStatic(Entity.class)) {
+
+            Entity<ResendRequest> outboundEntity = mock(Entity.class);
+            byte[] partyinfoData = "SomeEncodedPartyInfoData".getBytes();
+
+            entityMockedStatic.when(() -> Entity.entity(partyinfoData, MediaType.APPLICATION_OCTET_STREAM_TYPE))
+                .thenReturn(outboundEntity);
+
+            String targetUrl = "targetUrl";
+            Client client = mock(Client.class);
+            WebTarget webTarget = mock(WebTarget.class);
+            when(client.target(targetUrl)).thenReturn(webTarget);
+            when(webTarget.path("/partyinfo")).thenReturn(webTarget);
+
+            Invocation.Builder invocationBuilder = mock(Invocation.Builder.class);
+            when(webTarget.request())
+                .thenReturn(invocationBuilder);
+
+            Response response = mock(Response.class);
+            when(response.getStatus()).thenReturn(expectedResponseStatus.getStatusCode());
+            when(response.readEntity(byte[].class)).thenReturn("Success".getBytes());
+
+            when(invocationBuilder.post(outboundEntity)).thenReturn(response);
+
+            RestP2pClient restP2pClient = new RestP2pClient(client);
+
+
+            boolean outcome = restP2pClient.sendPartyInfo(targetUrl,partyinfoData);
+            if(Set.of(Response.Status.OK,Response.Status.CREATED).contains(expectedResponseStatus)) {
+                assertThat(outcome).isTrue();
+            } else {
+                assertThat(outcome).isFalse();
+            }
+
+            entityMockedStatic.verify(() -> Entity.entity(partyinfoData,MediaType.APPLICATION_OCTET_STREAM_TYPE));
+            entityMockedStatic.verifyNoMoreInteractions();
+
+            verify(client).target(targetUrl);
+            verify(webTarget).path("/partyinfo");
+            verify(webTarget).request();
+            verify(invocationBuilder).post(outboundEntity);
+
+            verifyNoMoreInteractions(outboundEntity,client,webTarget,invocationBuilder);
+
+        }
+    }
+
+    @Parameterized.Parameters(name = "ResponseStatus {0}")
+    public static Collection<Response.Status> statuses() {
+        return Arrays.asList(Response.Status.values());
+    }
+
+
+}
