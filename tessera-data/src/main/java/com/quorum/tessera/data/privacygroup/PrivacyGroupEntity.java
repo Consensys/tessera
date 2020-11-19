c7bc3f2b66afe00e404ab4aa5ccc8f3ff8b55b00
namtruong <nam.p.truong@outlook.com>
2020-11-19 19:38:25 +0000

    Add PrivacyGroup entity for database and unit tests

---
+++ tessera-data/src/main/java/com/quorum/tessera/data/privacygroup/PrivacyGroupEntity.java
@@ -0,0 +1,83 @@
+package com.quorum.tessera.data.privacygroup;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Arrays;
+
+/** The JPA entity that contains the privacy group information */
+@NamedQueries({
+    @NamedQuery(name = "PrivacyGroup.FindById", query = "SELECT pg FROM PrivacyGroupEntity pg WHERE pg.id = :id"),
+    @NamedQuery(
+            name = "PrivacyGroup.FindByLookupId",
+            query = "select pg from PrivacyGroupEntity pg WHERE pg.lookupId = :lookupId")
+})
+@Entity
+@Table(name = "PRIVACY_GROUP")
+public class PrivacyGroupEntity implements Serializable {
+
+    @Id
+    @Column(name = "ID")
+    private byte[] id;
+
+    @Lob
+    @Column(name = "LOOKUP_ID", nullable = false)
+    private byte[] lookupId;
+
+    @Lob
+    @Column(name = "DATA", nullable = false)
+    private byte[] data;
+
+    @Column(name = "TIMESTAMP", updatable = false)
+    private long timestamp;
+
+    public PrivacyGroupEntity(final byte[] id, final byte[] lookupId, final byte[] data) {
+        this.id = id;
+        this.lookupId = lookupId;
+        this.data = data;
+    }
+
+    public PrivacyGroupEntity() {}
+
+    @PrePersist
+    public void onPersist() {
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public byte[] getId() {
+        return id;
+    }
+
+    public void setId(byte[] id) {
+        this.id = id;
+    }
+
+    public byte[] getLookupId() {
+        return lookupId;
+    }
+
+    public void setLookupId(byte[] lookupId) {
+        this.lookupId = lookupId;
+    }
+
+    public byte[] getData() {
+        return data;
+    }
+
+    public void setData(byte[] data) {
+        this.data = data;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof PrivacyGroupEntity) && Arrays.equals(id, ((PrivacyGroupEntity) obj).id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(id);
+    }
+}
