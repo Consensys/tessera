9aaf40667188a91cac658e3224e35cfc069b21d6
namtruong <nam.p.truong@outlook.com>
2020-11-20 01:19:15 +0000

    Implements PrivacyGroupManager and unit tests

--- tessera-core/src/main/java/com/quorum/tessera/privacygroup/PrivacyGroupManagerImpl.java
+++ tessera-core/src/main/java/com/quorum/tessera/privacygroup/PrivacyGroupManagerImpl.java
@@ -1,36 +1,99 @@
 package com.quorum.tessera.privacygroup;
 
 import com.quorum.tessera.data.privacygroup.PrivacyGroupDAO;
+import com.quorum.tessera.data.privacygroup.PrivacyGroupEntity;
 import com.quorum.tessera.enclave.PrivacyGroup;
 import com.quorum.tessera.enclave.PrivacyGroupUtil;
 import com.quorum.tessera.encryption.PublicKey;
+import com.quorum.tessera.privacygroup.publish.PrivacyGroupPublisher;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PrivacyGroupManagerImpl implements PrivacyGroupManager {
 
     private final PrivacyGroupDAO privacyGroupDAO;
 
+    private final PrivacyGroupPublisher publisher;
+
     private final PrivacyGroupUtil privacyGroupUtil;
 
-    public PrivacyGroupManagerImpl(final PrivacyGroupDAO privacyGroupDAO) {
+    public PrivacyGroupManagerImpl(PrivacyGroupDAO privacyGroupDAO, PrivacyGroupPublisher publisher) {
+        this(privacyGroupDAO, publisher, PrivacyGroupUtil.create());
+    }
+
+    PrivacyGroupManagerImpl(PrivacyGroupDAO privacyGroupDAO, PrivacyGroupPublisher publisher, PrivacyGroupUtil privacyGroupUtil) {
         this.privacyGroupDAO = privacyGroupDAO;
-        this.privacyGroupUtil = PrivacyGroupUtil.create();
+        this.publisher = publisher;
+        this.privacyGroupUtil = privacyGroupUtil;
     }
 
     @Override
     public PrivacyGroup createPrivacyGroup(String name, String description, List<PublicKey> members, byte[] seed) {
-        return null;
+
+        final byte[] privacyGroupId = privacyGroupUtil.generatePrivacyGroupId(members, seed);
+
+        final PrivacyGroup created =
+                PrivacyGroup.Builder.create()
+                        .withPrivacyGroupId(PublicKey.from(privacyGroupId))
+                        .withName(name)
+                        .withDescription(description)
+                        .withMembers(members)
+                        .withSeed(seed)
+                        .withType(PrivacyGroup.Type.PANTHEON)
+                        .withState(PrivacyGroup.State.ACTIVE)
+                        .build();
+
+        final byte[] lookupId = privacyGroupUtil.generateLookupId(members);
+
+        final byte[] encodedData = privacyGroupUtil.encode(created);
+
+        privacyGroupDAO.save(
+                new PrivacyGroupEntity(privacyGroupId, lookupId, encodedData),
+                () -> {
+                    publisher.publishPrivacyGroup(encodedData, members);
+                    return null;
+                });
+
+        return created;
     }
 
     @Override
     public PrivacyGroup createLegacyPrivacyGroup(List<PublicKey> members) {
-        return null;
+
+        String name = "legacy";
+        String description = "Privacy groups to support the creation of groups by privateFor and privateFrom";
+
+        final byte[] privacyGroupId = privacyGroupUtil.generatePrivacyGroupId(members, null);
+
+        final byte[] lookupId = privacyGroupUtil.generateLookupId(members);
+
+        final PrivacyGroup created =
+            PrivacyGroup.Builder.create()
+                .withPrivacyGroupId(PublicKey.from(privacyGroupId))
+                .withName(name)
+                .withDescription(description)
+                .withMembers(members)
+                .withType(PrivacyGroup.Type.LEGACY)
+                .withState(PrivacyGroup.State.ACTIVE)
+                .build();
+
+        final byte[] encodedData = privacyGroupUtil.encode(created);
+
+        privacyGroupDAO.save(new PrivacyGroupEntity(privacyGroupId, lookupId, encodedData));
+
+        return created;
     }
 
     @Override
     public List<PrivacyGroup> findPrivacyGroup(List<PublicKey> members) {
-        return null;
+
+        final byte[] lookupId = privacyGroupUtil.generateLookupId(members);
+
+        return privacyGroupDAO.findByLookupId(lookupId).stream()
+            .map(PrivacyGroupEntity::getData)
+            .map(privacyGroupUtil::decode)
+            .collect(Collectors.toUnmodifiableList());
     }
 
     @Override
@@ -39,7 +102,5 @@ public class PrivacyGroupManagerImpl implements PrivacyGroupManager {
     }
 
     @Override
-    public void storePrivacyGroup(byte[] encodedData) {
-
-    }
+    public void storePrivacyGroup(byte[] encodedData) {}
 }
--- tessera-core/src/test/java/com/quorum/tessera/privacygroup/PrivacyGroupManagerTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/privacygroup/PrivacyGroupManagerTest.java
@@ -1,16 +1,23 @@
 package com.quorum.tessera.privacygroup;
 
 import com.quorum.tessera.data.privacygroup.PrivacyGroupDAO;
+import com.quorum.tessera.data.privacygroup.PrivacyGroupEntity;
 import com.quorum.tessera.enclave.PrivacyGroup;
+import com.quorum.tessera.enclave.PrivacyGroupUtil;
 import com.quorum.tessera.encryption.PublicKey;
 import com.quorum.tessera.privacygroup.exception.PrivacyGroupNotFoundException;
+import com.quorum.tessera.privacygroup.publish.PrivacyGroupPublisher;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 
 import java.util.List;
+import java.util.concurrent.Callable;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 public class PrivacyGroupManagerTest {
 
@@ -18,41 +25,123 @@ public class PrivacyGroupManagerTest {
 
     private PrivacyGroupDAO privacyGroupDAO;
 
+    private PrivacyGroupUtil privacyGroupUtil;
+
+    private PrivacyGroupPublisher publisher;
+
     @Before
     public void setUp() {
         privacyGroupDAO = mock(PrivacyGroupDAO.class);
-        privacyGroupManager = new PrivacyGroupManagerImpl(privacyGroupDAO);
+        publisher = mock(PrivacyGroupPublisher.class);
+        privacyGroupUtil = mock(PrivacyGroupUtil.class);
+        privacyGroupManager = new PrivacyGroupManagerImpl(privacyGroupDAO, publisher, privacyGroupUtil);
+    }
+
+    @After
+    public void tearDown() {
+        verifyNoMoreInteractions(privacyGroupDAO, publisher);
     }
 
     @Test
     public void testCreatePrivacyGroup() {
 
-        final PrivacyGroup privacyGroup = privacyGroupManager.createPrivacyGroup(
-            "name", "description", List.of(mock(PublicKey.class)), new byte[1]
-        );
+        when(privacyGroupUtil.generatePrivacyGroupId(anyList(), any(byte[].class)))
+            .thenReturn("generatedId".getBytes());
+        when(privacyGroupUtil.generateLookupId(anyList())).thenReturn("lookup".getBytes());
+        when(privacyGroupUtil.encode(any())).thenReturn("encoded".getBytes());
 
+        final List<PublicKey> members = List.of(mock(PublicKey.class), mock(PublicKey.class));
+
+        doAnswer(
+            invocation -> {
+                Callable callable = invocation.getArgument(1);
+                callable.call();
+                return mock(PrivacyGroupEntity.class);
+            }).when(privacyGroupDAO).save(any(), any());
+
+        final PrivacyGroup privacyGroup =
+            privacyGroupManager.createPrivacyGroup("name", "description", members, new byte[1]);
+
+        // Verify entity being saved has the correct values
+        ArgumentCaptor<PrivacyGroupEntity> argCaptor = ArgumentCaptor.forClass(PrivacyGroupEntity.class);
+        verify(privacyGroupDAO).save(argCaptor.capture(), any());
+        PrivacyGroupEntity savedEntity = argCaptor.getValue();
+        assertThat(savedEntity).isNotNull();
+        assertThat(savedEntity.getId()).isEqualTo("generatedId".getBytes());
+        assertThat(savedEntity.getLookupId()).isEqualTo("lookup".getBytes());
+        assertThat(savedEntity.getData()).isEqualTo("encoded".getBytes());
+
+        // Verify payload being distributed has the correct values
+        ArgumentCaptor<byte[]> payloadCaptor = ArgumentCaptor.forClass(byte[].class);
+        ArgumentCaptor<List<PublicKey>> recipientsCaptor = ArgumentCaptor.forClass(List.class);
+        verify(publisher).publishPrivacyGroup(payloadCaptor.capture(), recipientsCaptor.capture());
+        assertThat(payloadCaptor.getValue()).isEqualTo("encoded".getBytes());
+        assertThat(recipientsCaptor.getValue()).containsAll(members);
+
+        // Verify generated privacy group has the correct values
         assertThat(privacyGroup).isNotNull();
+        assertThat(privacyGroup.getPrivacyGroupId().getKeyBytes()).isEqualTo("generatedId".getBytes());
+        assertThat(privacyGroup.getName()).isEqualTo("name");
+        assertThat(privacyGroup.getDescription()).isEqualTo("description");
+        assertThat(privacyGroup.getMembers()).containsAll(members);
         assertThat(privacyGroup.getType()).isEqualTo(PrivacyGroup.Type.PANTHEON);
+        assertThat(privacyGroup.getState()).isEqualTo(PrivacyGroup.State.ACTIVE);
     }
 
     @Test
     public void testCreateLegacyPrivacyGroup() {
 
-        PublicKey member = mock(PublicKey.class);
+        final List<PublicKey> members = List.of(mock(PublicKey.class), mock(PublicKey.class));
+        when(privacyGroupUtil.generatePrivacyGroupId(anyList(), any()))
+            .thenReturn("generatedId".getBytes());
+        when(privacyGroupUtil.generateLookupId(anyList())).thenReturn("lookup".getBytes());
+        when(privacyGroupUtil.encode(any())).thenReturn("encoded".getBytes());
 
-        final PrivacyGroup privacyGroup = privacyGroupManager.createLegacyPrivacyGroup(List.of(member));
+        final PrivacyGroup privacyGroup = privacyGroupManager.createLegacyPrivacyGroup(members);
 
+        // Verify entity being saved has the correct values
+        ArgumentCaptor<PrivacyGroupEntity> argCaptor = ArgumentCaptor.forClass(PrivacyGroupEntity.class);
+        verify(privacyGroupDAO).save(argCaptor.capture());
+        PrivacyGroupEntity savedEntity = argCaptor.getValue();
+        assertThat(savedEntity).isNotNull();
+        assertThat(savedEntity.getId()).isEqualTo("generatedId".getBytes());
+        assertThat(savedEntity.getLookupId()).isEqualTo("lookup".getBytes());
+        assertThat(savedEntity.getData()).isEqualTo("encoded".getBytes());
+
+        // Verify generated privacy group has the correct values
         assertThat(privacyGroup).isNotNull();
+        assertThat(privacyGroup.getPrivacyGroupId().getKeyBytes()).isEqualTo("generatedId".getBytes());
+        assertThat(privacyGroup.getName()).isEqualTo("legacy");
+        assertThat(privacyGroup.getDescription()).isEqualTo("Privacy groups to support the creation of groups by privateFor and privateFrom");
+        assertThat(privacyGroup.getMembers()).containsAll(members);
         assertThat(privacyGroup.getType()).isEqualTo(PrivacyGroup.Type.LEGACY);
+        assertThat(privacyGroup.getState()).isEqualTo(PrivacyGroup.State.ACTIVE);
     }
 
     @Test
     public void testFindPrivacyGroup() {
-        PublicKey member = mock(PublicKey.class);
 
-        final List<PrivacyGroup> privacyGroups = privacyGroupManager.findPrivacyGroup(List.of(member));
+        final PrivacyGroupEntity et1 = mock(PrivacyGroupEntity.class);
+        when(et1.getData()).thenReturn("data1".getBytes());
+        final PrivacyGroupEntity et2 = mock(PrivacyGroupEntity.class);
+        when(et2.getData()).thenReturn("data2".getBytes());
+        final List<PrivacyGroupEntity> dbResult = List.of(et1, et2);
+
+        final PrivacyGroup pg1 = mock(PrivacyGroup.class);
+        final PrivacyGroup pg2 = mock(PrivacyGroup.class);
+
+        when(privacyGroupDAO.findByLookupId("lookup".getBytes())).thenReturn(dbResult);
+        when(privacyGroupUtil.generateLookupId(anyList())).thenReturn("lookup".getBytes());
+        when(privacyGroupUtil.decode("data1".getBytes())).thenReturn(pg1);
+        when(privacyGroupUtil.decode("data2".getBytes())).thenReturn(pg2);
+
+
+        final List<PrivacyGroup> privacyGroups = privacyGroupManager.findPrivacyGroup(List.of());
 
         assertThat(privacyGroups).isNotEmpty();
+        assertThat(privacyGroups).contains(pg1, pg2);
+
+        verify(privacyGroupDAO).findByLookupId("lookup".getBytes());
     }
 
     @Test
@@ -71,8 +160,7 @@ public class PrivacyGroupManagerTest {
 
         try {
             final PrivacyGroup result = privacyGroupManager.retrievePrivacyGroup(groupId);
-        }
-        catch(Exception ex) {
+        } catch (Exception ex) {
             assertThat(ex).isInstanceOf(PrivacyGroupNotFoundException.class);
         }
     }
@@ -83,4 +171,9 @@ public class PrivacyGroupManagerTest {
         privacyGroupManager.storePrivacyGroup(encoded);
     }
 
+    @Test
+    public void construct() {
+        PrivacyGroupManager manager = new PrivacyGroupManagerImpl(privacyGroupDAO, publisher);
+        assertThat(manager).isNotNull();
+    }
 }
