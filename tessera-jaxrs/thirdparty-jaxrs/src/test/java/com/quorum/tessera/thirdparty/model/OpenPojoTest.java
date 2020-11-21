06963700c50a263db51c57da3244aac84354ad2b
Mark Lowe <melowe.quorum@gmail.com>
2020-11-21 16:32:33 +0000

    Merge/port tests from master. Kill dead service loader config files

---
+++ tessera-jaxrs/thirdparty-jaxrs/src/test/java/com/quorum/tessera/thirdparty/model/OpenPojoTest.java
@@ -0,0 +1,36 @@
+package com.quorum.tessera.thirdparty.model;
+
+import com.openpojo.reflection.PojoClass;
+import com.openpojo.reflection.impl.PojoClassFactory;
+import com.openpojo.validation.Validator;
+import com.openpojo.validation.ValidatorBuilder;
+import com.openpojo.validation.rule.impl.GetterMustExistRule;
+import com.openpojo.validation.rule.impl.NoPrimitivesRule;
+import com.openpojo.validation.rule.impl.SetterMustExistRule;
+import com.openpojo.validation.test.impl.GetterTester;
+import com.openpojo.validation.test.impl.SetterTester;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class OpenPojoTest {
+
+    @Test
+    public void test() {
+
+        final Validator validator = ValidatorBuilder.create()
+            .with(new GetterMustExistRule())
+            .with(new SetterMustExistRule())
+            .with(new SetterTester())
+            .with(new GetterTester())
+            .with(new NoPrimitivesRule())
+            .build();
+
+        List<PojoClass> pojoClasses = Stream.of(GetPublicKeysResponse.class,Key.class)
+            .map(PojoClassFactory::getPojoClass).collect(Collectors.toList());
+
+        validator.validate(pojoClasses);
+    }
+}
