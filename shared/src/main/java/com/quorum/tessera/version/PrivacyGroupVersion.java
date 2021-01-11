86ffdd70de986dc9dc5d9e6e21e40c71b6f20a03
namtruong <nam.p.truong@outlook.com>
2021-01-11 13:10:11 +0000

    Add api version that reflect privacygroup support

---
+++ shared/src/main/java/com/quorum/tessera/version/PrivacyGroupVersion.java
@@ -0,0 +1,11 @@
+package com.quorum.tessera.version;
+
+public class PrivacyGroupVersion implements ApiVersion{
+
+    public static final String API_VERSION_3 = "3";
+
+    @Override
+    public String getVersion() {
+        return API_VERSION_3;
+    }
+}
