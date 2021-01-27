d88a011ebe139ab5ea517906291ef9cffcafbdea
namtruong <nam.p.truong@outlook.com>
2021-01-27 16:00:23 +0000

    Introduce api version 3.0 supporting privacy group

--- shared/src/test/java/com/quorum/tessera/version/ApiVersionTest.java
+++ shared/src/test/java/com/quorum/tessera/version/ApiVersionTest.java
@@ -8,6 +8,6 @@ public class ApiVersionTest {
 
     @Test
     public void create() {
-        assertThat(ApiVersion.versions()).containsExactlyInAnyOrder("0.1", "1.0", "v1", "v2", "2.1");
+        assertThat(ApiVersion.versions()).containsExactlyInAnyOrder("0.1", "1.0", "v1", "v2", "2.1", "3.0");
     }
 }
