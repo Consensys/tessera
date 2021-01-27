ec3ada09b892c90452fe12cfea830d40bb1977e5
namtruong <nam.p.truong@outlook.com>
2021-01-27 15:43:56 +0000

    Privacy Group Manager to handle pg logics and publisher

---
+++ tessera-core/src/main/java/com/quorum/tessera/privacygroup/publish/BatchPrivacyGroupPublisher.java
@@ -0,0 +1,10 @@
+package com.quorum.tessera.privacygroup.publish;
+
+import com.quorum.tessera.encryption.PublicKey;
+
+import java.util.List;
+
+public interface BatchPrivacyGroupPublisher {
+
+    void publishPrivacyGroup(byte[] data, List<PublicKey> recipients);
+}
