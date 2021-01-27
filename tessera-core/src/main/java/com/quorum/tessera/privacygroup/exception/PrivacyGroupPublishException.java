f88f3b1f6fe2fe5f838964772674da19cc3a2991
namtruong <nam.p.truong@outlook.com>
2021-01-27 15:20:17 +0000

    Privacy group related exceptions and unit tests

---
+++ tessera-core/src/main/java/com/quorum/tessera/privacygroup/exception/PrivacyGroupPublishException.java
@@ -0,0 +1,10 @@
+package com.quorum.tessera.privacygroup.exception;
+
+import com.quorum.tessera.exception.TesseraException;
+
+public class PrivacyGroupPublishException extends TesseraException {
+
+    public PrivacyGroupPublishException(String message) {
+        super(message);
+    }
+}
