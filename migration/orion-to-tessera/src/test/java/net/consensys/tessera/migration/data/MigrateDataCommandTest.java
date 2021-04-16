190bdb73327646a57f75a029adff942f6d4cd8a1
Mark Lowe <melowe.quorum@gmail.com>
2021-04-16 13:20:58 +0100

    normalise enclave creation.

--- migration/orion-to-tessera/src/test/java/net/consensys/tessera/migration/data/MigrateDataCommandTest.java
+++ migration/orion-to-tessera/src/test/java/net/consensys/tessera/migration/data/MigrateDataCommandTest.java
@@ -2,13 +2,7 @@ package net.consensys.tessera.migration.data;
 
 import com.moandjiezana.toml.Toml;
 import com.moandjiezana.toml.TomlWriter;
-import com.quorum.tessera.config.Config;
-import com.quorum.tessera.config.EncryptorConfig;
-import com.quorum.tessera.config.EncryptorType;
-import com.quorum.tessera.config.KeyConfiguration;
-import com.quorum.tessera.config.KeyData;
 import com.quorum.tessera.enclave.Enclave;
-import com.quorum.tessera.enclave.EnclaveFactory;
 import com.quorum.tessera.enclave.EncodedPayload;
 import com.quorum.tessera.enclave.PayloadEncoder;
 import com.quorum.tessera.enclave.PrivacyGroup;
@@ -244,7 +238,7 @@ public class MigrateDataCommandTest {
                             .map(PublicKey::from)
                             .get();
 
-                        Enclave enclave = createEnclave(orionKeyHelper);
+                        Enclave enclave = TesseraEnclaveFactory.createEnclave(orionKeyHelper);
 
                         byte[] unencryptedTransaction = enclave.unencryptTransaction(encodedPayload,encryptionKey);
 
@@ -415,25 +409,7 @@ public class MigrateDataCommandTest {
         ORACLE
     }
 
-    private static Enclave createEnclave(OrionKeyHelper orionKeyHelper) {
-        Config tesseraConfig = new Config();
-        EncryptorConfig tesseraEncryptorConfig = new EncryptorConfig();
-        tesseraEncryptorConfig.setType(EncryptorType.NACL);
 
-        tesseraConfig.setKeys(new KeyConfiguration());
-
-        KeyData keyData = orionKeyHelper.getKeyPairs().stream().map(p -> {
-            KeyData keyData1 = new KeyData();
-            keyData1.setPrivateKey(Base64.getEncoder().encodeToString(p.secretKey().bytesArray()));
-            keyData1.setPublicKey(Base64.getEncoder().encodeToString(p.publicKey().bytesArray()));
-            return keyData1;
-        }).findFirst().get();
-
-        tesseraConfig.getKeys().setKeyData(List.of(keyData));
-        tesseraConfig.setEncryptor(tesseraEncryptorConfig);
-
-        return EnclaveFactory.create().create(tesseraConfig);
-    }
 
 
 }
