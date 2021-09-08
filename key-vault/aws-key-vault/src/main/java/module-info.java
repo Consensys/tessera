1d59dc3150ec6d8d02539385ac6e90b3b1d6a4cc
Mark Lowe <melowe.quorum@gmail.com>
2021-09-08 16:44:07 +0100

    force aws module to load commons logging from jcl bridge jar. remove commented code

--- key-vault/aws-key-vault/src/main/java/module-info.java
+++ key-vault/aws-key-vault/src/main/java/module-info.java
@@ -5,6 +5,8 @@ module tessera.keyvault.aws {
   requires tessera.keyvault.api;
   requires com.fasterxml.jackson.core;
   requires com.fasterxml.jackson.databind;
+  requires org.slf4j;
+  requires org.apache.commons.logging;
 
   provides com.quorum.tessera.key.vault.KeyVaultServiceFactory with
       com.quorum.tessera.key.vault.aws.AWSKeyVaultServiceFactory;
