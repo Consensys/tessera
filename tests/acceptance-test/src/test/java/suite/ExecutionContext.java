84f4faecb1dd4872887e883e97b82f111be81e07
Mark Lowe <melowe.quorum@gmail.com>
2021-09-08 13:12:39 +0100

    spotless formatting changes

--- tests/acceptance-test/src/test/java/suite/ExecutionContext.java
+++ tests/acceptance-test/src/test/java/suite/ExecutionContext.java
@@ -239,7 +239,7 @@ public class ExecutionContext {
 
     public ExecutionContext createAndSetupContext() {
 
-      Stream.of(dbType, communicationType, socketType, enclaveType, encryptorType,clientMode)
+      Stream.of(dbType, communicationType, socketType, enclaveType, encryptorType, clientMode)
           .forEach(Objects::requireNonNull);
 
       ExecutionContext executionContext = build();
