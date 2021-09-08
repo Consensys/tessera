4691a4f862578eb2992377878c77b0d959c01508
Nam Truong <nam.p.truong@outlook.com>
2021-09-08 23:19:33 +0100

    gradle spotlessApply

--- tessera-jaxrs/common-jaxrs/src/test/java/com/quorum/tessera/api/exception/MandatoryRecipientsNotAvailableExceptionMapperTest.java
+++ tessera-jaxrs/common-jaxrs/src/test/java/com/quorum/tessera/api/exception/MandatoryRecipientsNotAvailableExceptionMapperTest.java
@@ -1,19 +1,20 @@
 package com.quorum.tessera.api.exception;
 
-import com.quorum.tessera.transaction.exception.MandatoryRecipientsNotAvailableException;
-import org.junit.Test;
-
-import javax.ws.rs.core.Response;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import com.quorum.tessera.transaction.exception.MandatoryRecipientsNotAvailableException;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
 
 public class MandatoryRecipientsNotAvailableExceptionMapperTest {
 
-  private MandatoryRecipientsNotAvailableExceptionMapper instance = new MandatoryRecipientsNotAvailableExceptionMapper();
+  private MandatoryRecipientsNotAvailableExceptionMapper instance =
+      new MandatoryRecipientsNotAvailableExceptionMapper();
 
   @Test
   public void toResponse() {
-    final MandatoryRecipientsNotAvailableException ex = new MandatoryRecipientsNotAvailableException("OUCH");
+    final MandatoryRecipientsNotAvailableException ex =
+        new MandatoryRecipientsNotAvailableException("OUCH");
 
     final Response result = instance.toResponse(ex);
     assertThat(result).isNotNull();
--- tessera-jaxrs/common-jaxrs/src/test/java/com/quorum/tessera/api/exception/MandatoryRecipientsNotSupportedExceptionMapperTest.java
+++ tessera-jaxrs/common-jaxrs/src/test/java/com/quorum/tessera/api/exception/MandatoryRecipientsNotSupportedExceptionMapperTest.java
@@ -1,19 +1,20 @@
 package com.quorum.tessera.api.exception;
 
-import com.quorum.tessera.transaction.exception.MandatoryRecipientsNotSupportedException;
-import org.junit.Test;
-
-import javax.ws.rs.core.Response;
-
 import static org.assertj.core.api.Assertions.assertThat;
+
+import com.quorum.tessera.transaction.exception.MandatoryRecipientsNotSupportedException;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
 
 public class MandatoryRecipientsNotSupportedExceptionMapperTest {
 
-  private MandatoryRecipientsNotSupportedExceptionMapper instance = new MandatoryRecipientsNotSupportedExceptionMapper();
+  private MandatoryRecipientsNotSupportedExceptionMapper instance =
+      new MandatoryRecipientsNotSupportedExceptionMapper();
 
   @Test
   public void toResponse() {
-    final MandatoryRecipientsNotSupportedException ex = new MandatoryRecipientsNotSupportedException("OUCH");
+    final MandatoryRecipientsNotSupportedException ex =
+        new MandatoryRecipientsNotSupportedException("OUCH");
 
     final Response result = instance.toResponse(ex);
     assertThat(result).isNotNull();
