7eb62a1586ef3236c95464d4e902635477c4c585
Mark Lowe <melowe.quorum@gmail.com>
2021-09-09 10:33:49 +0100

    change namespaces for latest classes

--- tessera-jaxrs/common-jaxrs/src/main/java/com/quorum/tessera/api/exception/MandatoryRecipientsNotSupportedExceptionMapper.java
+++ tessera-jaxrs/common-jaxrs/src/main/java/com/quorum/tessera/api/exception/MandatoryRecipientsNotSupportedExceptionMapper.java
@@ -1,10 +1,10 @@
 package com.quorum.tessera.api.exception;
 
 import com.quorum.tessera.transaction.exception.MandatoryRecipientsNotSupportedException;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.ext.ExceptionMapper;
-import javax.ws.rs.ext.Provider;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
