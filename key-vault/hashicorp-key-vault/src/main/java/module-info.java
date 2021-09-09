d342f9b153bfa92650de83bfc77757957694c4c8
Mark Lowe <melowe.quorum@gmail.com>
2021-09-09 11:58:26 +0100

    add jackson dependency to module info due to CI failure

--- key-vault/hashicorp-key-vault/src/main/java/module-info.java
+++ key-vault/hashicorp-key-vault/src/main/java/module-info.java
@@ -6,6 +6,7 @@ module tessera.keyvault.hashicorp {
   requires tessera.keyvault.api;
   requires org.slf4j;
   requires org.apache.commons.logging;
+  requires com.fasterxml.jackson.core;
 
   provides com.quorum.tessera.key.vault.KeyVaultServiceFactory with
       com.quorum.tessera.key.vault.hashicorp.HashicorpKeyVaultServiceFactory;
