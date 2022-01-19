372dc0f9c60a45875d44cce3f926994f0e034537
shashik90 <94975745+shashik90@users.noreply.github.com>
2022-01-19 17:04:42 +0530

    P2P tests

--- tessera-core/src/test/java/com/quorum/tessera/messaging/internal/InboxHolderTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/messaging/internal/InboxHolderTest.java
@@ -1,14 +1,19 @@
 package com.quorum.tessera.messaging.internal;
 
+import com.quorum.tessera.data.EncryptedMessageDAO;
+import com.quorum.tessera.enclave.Enclave;
+import com.quorum.tessera.messaging.Courier;
 import com.quorum.tessera.messaging.Inbox;
 import com.quorum.tessera.messaging.Messaging;
 import junit.framework.TestCase;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -17,21 +22,36 @@ public class InboxHolderTest {
 
   Inbox inbox;
   InboxHolder inboxHolder;
+  InboxImpl inboxImpl;
+  EncryptedMessageDAO dao;
 
   @Before
   public void setUp()  {
     inbox = mock(Inbox.class);
     inboxHolder = mock(InboxHolder.class);
+    dao = mock(EncryptedMessageDAO.class);
   }
 
   @Test
   public void testGetInbox() {
-    Optional<Inbox> optionalMessaging =  Optional.ofNullable(inbox);
-    when(inboxHolder.getInbox()).thenReturn(optionalMessaging);
+    Optional<Inbox> optionalInbox =  Optional.ofNullable(inbox);
+    when(inboxHolder.getInbox()).thenReturn(optionalInbox);
+    assertThat(InboxHolder.INSTANCE).isNotNull();
+    when(inboxHolder.getInbox()).thenReturn(optionalInbox);
+    InboxHolder holder =  InboxHolder.INSTANCE.INSTANCE;
+    assertThat(inboxHolder.getInbox()).isNotNull();
+    assertThat(holder.getInbox()).isNotNull();
+
   }
 
   @Test
   public void testStore() {
+    inboxImpl = new InboxImpl(dao);
     when(inboxHolder.store(any())).thenReturn(inbox);
+    InboxHolder holder =  InboxHolder.INSTANCE;
+    assertThat(inboxHolder.getInbox()).isNotNull();
+    assertThat(holder.getInbox()).isNotNull();
+    assertThat(inboxImpl).isNotNull();
+    Assert.assertNotNull(holder.store(inboxImpl));
   }
 }
