a3f7fcf213b0d76b77ee7ec6a78310e782ffa3a8
Chirag-786 <chirag17rock@gmail.com>
2022-01-19 13:29:08 +0530

    Changes for unit test

--- tessera-core/src/test/java/com/quorum/tessera/messaging/MessagingTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/messaging/MessagingTest.java
@@ -1,52 +1,35 @@
 package com.quorum.tessera.messaging;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ServiceLoader;
-import static org.mockito.ArgumentMatchers.anyString;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 public class MessagingTest {
-  private Messaging messaging;
-  private Message message;
-  private ServiceLoader serviceLoader;
-
-  @Before
-  public void setUp() {
-   message = mock(Message.class);
-   messaging = mock(Messaging.class);
-   serviceLoader = mock(ServiceLoader.class);
-  }
-
-  @Test
-  public void testSend(){
-   // when(messaging.send(message)).thenReturn("");
-    Assert.assertNotNull(messaging);
-  }
-
-  @Test
-  public void testReceived(){
-    when(messaging.received()).thenReturn(new ArrayList<>());
-  }
-
-  @Test
-  public void testRead() throws NoSuchMessageException {
-    when(messaging.read(anyString())).thenReturn(message);
-  }
-
-  @Test
-  public void testRemove() {
-    doNothing().when(messaging).remove(anyString());
-  }
 
   @Test
   public void testCreate() {
+    Messaging messaging = mock(Messaging.class);
+    ServiceLoader<Messaging> serviceLoader = mock(ServiceLoader.class);
     when(serviceLoader.findFirst()).thenReturn(Optional.of(messaging));
-    when(serviceLoader.findFirst().get()).thenReturn(Optional.of(messaging));
-    doReturn(Optional.ofNullable(Messaging.class)).when(serviceLoader).findFirst();
+
+    Messaging result;
+    try (var serviceLoaderMockedStatic = mockStatic(ServiceLoader.class)) {
+
+      serviceLoaderMockedStatic
+        .when(() -> ServiceLoader.load(Messaging.class))
+        .thenReturn(serviceLoader);
+
+      result = Messaging.create();
+
+      serviceLoaderMockedStatic.verify(() -> ServiceLoader.load(Messaging.class));
+      serviceLoaderMockedStatic.verifyNoMoreInteractions();
+    }
+    verify(serviceLoader).findFirst();
+    verifyNoMoreInteractions(serviceLoader);
+    assertThat(result).isNotNull().isSameAs(messaging);
+
   }
 }
