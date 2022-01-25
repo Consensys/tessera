14e45f8137066f51062833de4e339c220727aa55
shashik90 <94975745+shashik90@users.noreply.github.com>
2022-01-25 10:57:48 +0530

    Inbox and MessagingProvider
    Inbox & Messaging Provider

--- tessera-core/src/test/java/com/quorum/tessera/messaging/internal/InboxProviderTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/messaging/internal/InboxProviderTest.java
@@ -1,33 +1,68 @@
 package com.quorum.tessera.messaging.internal;
 
+import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.ConfigFactory;
 import com.quorum.tessera.data.EncryptedMessageDAO;
-import com.quorum.tessera.messaging.Inbox;
-import junit.framework.TestCase;
-import org.junit.Before;
-import org.junit.Test;
-import java.util.Optional;
 
-import static org.junit.Assert.assertEquals;
+import com.quorum.tessera.data.EncryptedRawTransactionDAO;
+import com.quorum.tessera.messaging.Inbox;
+import com.quorum.tessera.transaction.TransactionManager;
+import com.quorum.tessera.transaction.internal.TransactionManagerProvider;
+import org.junit.*;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 import static org.mockito.Mockito.*;
 
 public class InboxProviderTest {
-  InboxHolder inboxHolder;
-  Inbox inbox;
-  EncryptedMessageDAO encryptedMessageDAO;
-  InboxImpl inboxImpl;
 
   @Before
-  public void setUp() {
-    inboxHolder = mock(InboxHolder.class);
-    inbox = mock(Inbox.class);
-    encryptedMessageDAO = mock(EncryptedMessageDAO.class);
-    inboxImpl = new InboxImpl(encryptedMessageDAO);
+  @After
+  public void clearHolder() {
+    InboxHolder.INSTANCE.store(null);
+    assertThat(InboxHolder.INSTANCE.getInbox()).isNotPresent();
+  }
+
+
+  @Test
+  public void provider() {
+
+    try (var mockedStaticConfigFactory = mockStatic(ConfigFactory.class);
+         var mockedStaticEncryptedMessageDAO = mockStatic(EncryptedMessageDAO.class))
+         {
+
+      ConfigFactory configFactory = mock(ConfigFactory.class);
+      Config config = mock(Config.class);
+      when(configFactory.getConfig()).thenReturn(config);
+      mockedStaticConfigFactory.when(ConfigFactory::create).thenReturn(configFactory);
+
+      mockedStaticEncryptedMessageDAO
+        .when(EncryptedMessageDAO::create)
+        .thenReturn(mock(EncryptedMessageDAO.class));
+
+           Inbox inbox = InboxProvider.provider();
+           assertThat(inbox).isNotNull();
+
+           assertThat(InboxProvider.provider())
+             .describedAs("Second invocation should return same instance")
+             .isSameAs(inbox);
+
+           mockedStaticEncryptedMessageDAO.verify(EncryptedMessageDAO::create);
+           mockedStaticEncryptedMessageDAO.verifyNoMoreInteractions();
+    }
   }
 
   @Test
-  public void testInboxProvider() {
-    when(inboxHolder.getInbox()).thenReturn(Optional.ofNullable(inbox));
-    assertEquals(Optional.of(inbox),inboxHolder.getInbox());
-    doReturn(inbox).when(inboxHolder).store(inboxImpl);
+  public void defaultConstructorForCoverage() {
+    assertThat(new InboxProvider()).isNotNull();
   }
+
+  /*@Test
+  public void defaultConstructorForCoverage() {
+    assertThat(new TransactionManagerProvider()).isNotNull();
+  }*/
 }
+
+
+
--- tessera-core/src/test/java/com/quorum/tessera/messaging/internal/MessagingProviderTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/messaging/internal/MessagingProviderTest.java
@@ -1,31 +1,70 @@
 package com.quorum.tessera.messaging.internal;
 
+import com.quorum.tessera.config.Config;
+import com.quorum.tessera.config.ConfigFactory;
+import com.quorum.tessera.data.EncryptedMessageDAO;
+import com.quorum.tessera.enclave.Enclave;
+import com.quorum.tessera.messaging.Courier;
+import com.quorum.tessera.messaging.Inbox;
 import com.quorum.tessera.messaging.Messaging;
 import junit.framework.TestCase;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mockStatic;
 
 public class MessagingProviderTest {
 
-  private MessagingHolder holder;
-  private Messaging messaging;
-
-
   @Before
-  public void setUp()  {
-    holder = mock(MessagingHolder.class);
+  @After
+  public void clearHolder() {
+    MessagingHolder.INSTANCE.store(null);
+    assertThat(MessagingHolder.INSTANCE.getMessaging()).isNotPresent();
+  }
+
+
+  @Test
+  public void provider() {
+
+    try (var mockedStaticConfigFactory = mockStatic(ConfigFactory.class);
+         var mockedStaticEnclave = mockStatic(Enclave.class);
+         var mockedStaticCourier = mockStatic(Courier.class);
+         var mockedStaticInbox = mockStatic(Inbox.class)) {
+
+      ConfigFactory configFactory = mock(ConfigFactory.class);
+      Config config = mock(Config.class);
+      when(configFactory.getConfig()).thenReturn(config);
+      mockedStaticConfigFactory.when(ConfigFactory::create).thenReturn(configFactory);
+
+      mockedStaticEnclave.when(Enclave::create).thenReturn(mock(Enclave.class));
+      mockedStaticCourier.when(Courier::create).thenReturn(mock(Courier.class));
+      mockedStaticInbox.when(Inbox::create).thenReturn(mock(Inbox.class));
+
+      Messaging messaging = MessagingProvider.provider();
+      assertThat(messaging).isNotNull();
+
+      assertThat(MessagingProvider.provider())
+        .describedAs("Second invocation should return same instance")
+        .isSameAs(messaging);
+
+      mockedStaticEnclave.verify(Enclave::create);
+      mockedStaticEnclave.verifyNoMoreInteractions();
+      mockedStaticCourier.verify(Courier::create);
+      mockedStaticCourier.verifyNoMoreInteractions();
+      mockedStaticInbox.verify(Inbox::create);
+      mockedStaticInbox.verifyNoMoreInteractions();
+
+    }
   }
 
   @Test
-  public void testMessagingProvider() {
-    messaging = mock(Messaging.class);
-    when(holder.getMessaging()).thenReturn(Optional.ofNullable(messaging));
-    assertEquals(true,holder.getMessaging().isPresent());
+  public void defaultConstructorForCoverage() {
+    assertThat(new MessagingProvider()).isNotNull();
   }
 }
