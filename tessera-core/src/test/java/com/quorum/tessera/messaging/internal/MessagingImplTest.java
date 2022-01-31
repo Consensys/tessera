275c159852f6ebd162a2ad27a9ba021af8313541
Chirag-786 <chirag17rock@gmail.com>
2022-01-31 12:31:36 +0530

    Messaging Impl Test coverage

--- tessera-core/src/test/java/com/quorum/tessera/messaging/internal/MessagingImplTest.java
+++ tessera-core/src/test/java/com/quorum/tessera/messaging/internal/MessagingImplTest.java
@@ -3,6 +3,7 @@ package com.quorum.tessera.messaging.internal;
 import com.quorum.tessera.base64.Base64Codec;
 import com.quorum.tessera.config.Config;
 import com.quorum.tessera.config.ConfigFactory;
+import com.quorum.tessera.data.EncryptedMessageDAO;
 import com.quorum.tessera.enclave.*;
 import com.quorum.tessera.encryption.Encryptor;
 import com.quorum.tessera.encryption.KeyManager;
@@ -38,8 +39,8 @@ public class MessagingImplTest {
   private Message message;
   private MessageId messageId;
   private EncodedPayload encodedPayload;
-
-  private final PayloadEncoder payloadEncoder = PayloadEncoder.create(EncodedPayloadCodec.LEGACY);
+  private EncryptedMessageDAO dao;
+  private final PayloadEncoder payloadEncoder = new PayloadEncoderImpl();
   private final Base64Codec base64Codec = Base64Codec.create();
 
   @Before
@@ -51,6 +52,7 @@ public class MessagingImplTest {
     message = mock(Message.class);
     messageId = mock(MessageId.class);
     encodedPayload = mock(EncodedPayload.class);
+    dao = mock(EncryptedMessageDAO.class);
   }
 
   @Test
@@ -59,32 +61,22 @@ public class MessagingImplTest {
     assertThat(impl).isNotNull();
   }
 
-  @Test
-  public void testSendUnknownRecipientExceptionIsThrown() throws UnknownRecipientException{
+  @Test(expected = UnknownRecipientException.class)
+  public void testSendUnknownRecipientExceptionIsThrown(){
     PublicKey sender = PublicKey.from("sender".getBytes());
     PublicKey receiver  = PublicKey.from("receiver".getBytes());
 
     Message message = new Message(sender,receiver,"data".getBytes());
     MessagingImpl impl = new MessagingImpl(enclave,courier,inbox);
-
-    String msg = "";
-   try {
-     msg = impl.send(message);
-   }
-   catch(UnknownRecipientException ex){
-     assertThat(msg).isEmpty();
-   }
+    impl.send(message);
   }
 
   @Test
   public void testSendSuccess() {
     PublicKey sender = PublicKey.from("sender".getBytes());
     PublicKey receiver  = PublicKey.from("receiver".getBytes());
-    messagingImpl = mock(MessagingImpl.class);
     Message message = new Message(sender,receiver,"data".getBytes());
-    doReturn("doneTest").when(messagingImpl).send(message);
     doReturn(new MessageId("data".getBytes())).when(courier).push("data".getBytes(),receiver);
-
 
     courier = new Courier() {
       @Override
@@ -98,14 +90,77 @@ public class MessagingImplTest {
       }
     };
 
-    MessagingImpl impl = new MessagingImpl(enclave,courier,inbox);
-    try {
-      impl.send(message);
-    }
-    catch(Exception ex){
+    enclave = new Enclave() {
+      @Override
+      public PublicKey defaultPublicKey() {
+        return null;
+      }
 
-      assertThat(courier.push("data".getBytes(),receiver).toString()).isNotNull();
-    }
+      @Override
+      public Set<PublicKey> getForwardingKeys() {
+        return null;
+      }
+
+      @Override
+      public Set<PublicKey> getPublicKeys() {
+        return null;
+      }
+
+      @Override
+      public EncodedPayload encryptPayload(byte[] message, PublicKey senderPublicKey, List<PublicKey> recipientPublicKeys, PrivacyMetadata privacyMetadata) {
+
+        final EncodedPayload.Builder payloadBuilder = EncodedPayload.Builder.create();
+        return payloadBuilder
+          .withSenderKey(senderPublicKey)
+          .withCipherText("test".getBytes())
+          .withCipherTextNonce(new Nonce("test".getBytes()))
+          .withRecipientBoxes(new ArrayList<>())
+          .withRecipientNonce(new Nonce("test".getBytes()))
+          .withRecipientKeys(recipientPublicKeys)
+          .withPrivacyMode(privacyMetadata.getPrivacyMode())
+          .withAffectedContractTransactions(new HashMap<>())
+          .withExecHash(privacyMetadata.getExecHash())
+          .withMandatoryRecipients(privacyMetadata.getMandatoryRecipients())
+          .build();
+      }
+
+      @Override
+      public EncodedPayload encryptPayload(RawTransaction rawTransaction, List<PublicKey> recipientPublicKeys, PrivacyMetadata privacyMetadata) {
+        return null;
+      }
+
+      @Override
+      public Set<TxHash> findInvalidSecurityHashes(EncodedPayload encodedPayload, List<AffectedTransaction> affectedContractTransactions) {
+        return null;
+      }
+
+      @Override
+      public RawTransaction encryptRawPayload(byte[] message, PublicKey sender) {
+        return null;
+      }
+
+      @Override
+      public byte[] unencryptTransaction(EncodedPayload payload, PublicKey providedKey) {
+        return new byte[0];
+      }
+
+      @Override
+      public byte[] unencryptRawPayload(RawTransaction payload) {
+        return new byte[0];
+      }
+
+      @Override
+      public byte[] createNewRecipientBox(EncodedPayload payload, PublicKey recipientKey) {
+        return new byte[0];
+      }
+
+      @Override
+      public Status status() {
+        return null;
+      }
+    };
+    MessagingImpl impl = new MessagingImpl(enclave,courier,inbox);
+    impl.send(message);
 
   }
 
@@ -115,20 +170,10 @@ public class MessagingImplTest {
     assertThat(impl.received()).isNotNull();
   }
 
-  @Test
-  public void testReadThrowsNoSuchMessageException() throws NoSuchMessageException{
-    PublicKey sender = PublicKey.from("sender".getBytes());
-    PublicKey receiver  = PublicKey.from("receiver".getBytes());
-
-    Message message = new Message(sender,receiver,"data".getBytes());
+  @Test(expected = NoSuchMessageException.class)
+  public void testReadThrowsNoSuchMessageException(){
     MessagingImpl impl = new MessagingImpl(enclave,courier,inbox);
-
-    try {
-      impl.read("abcd");
-    }
-    catch(NoSuchMessageException ex){
-
-    }
+    impl.read("abcd");
   }
 
   @Test
@@ -137,8 +182,129 @@ public class MessagingImplTest {
     PublicKey receiver  = PublicKey.from("receiver".getBytes());
 
     Message message = new Message(sender,receiver,"data".getBytes());
+    PayloadEncoder payloadEncoder = new PayloadEncoderImpl() {
+      @Override
+      public byte[] encode(EncodedPayload payload) {
+        return new byte[0];
+      }
+
+      @Override
+      public EncodedPayload decode(byte[] input) {
+        final EncodedPayload.Builder payloadBuilder = EncodedPayload.Builder.create();
+        final PrivacyMetadata privacyMetadata =
+          PrivacyMetadata.Builder.create().withPrivacyMode(PrivacyMode.STANDARD_PRIVATE).build();
+        return payloadBuilder
+          .withSenderKey(sender)
+          .withCipherText("test".getBytes())
+          .withCipherTextNonce(new Nonce("test".getBytes()))
+          .withRecipientBoxes(new ArrayList<>())
+          .withRecipientNonce(new Nonce("test".getBytes()))
+          .withRecipientKeys(new ArrayList<>())
+          .withPrivacyMode(privacyMetadata.getPrivacyMode())
+          .withAffectedContractTransactions(new HashMap<>())
+          .withExecHash(privacyMetadata.getExecHash())
+          .withMandatoryRecipients(privacyMetadata.getMandatoryRecipients())
+          .build();
+      }
+
+      @Override
+      public EncodedPayloadCodec encodedPayloadCodec() {
+        return null;
+      }
+    };
+    enclave = new Enclave() {
+      @Override
+      public PublicKey defaultPublicKey() {
+        return sender;
+      }
+
+      @Override
+      public Set<PublicKey> getForwardingKeys() {
+        return null;
+      }
+
+      @Override
+      public Set<PublicKey> getPublicKeys() {
+        return null;
+      }
+
+      @Override
+      public EncodedPayload encryptPayload(byte[] message, PublicKey senderPublicKey, List<PublicKey> recipientPublicKeys, PrivacyMetadata privacyMetadata) {
+
+        final EncodedPayload.Builder payloadBuilder = EncodedPayload.Builder.create();
+        return payloadBuilder
+          .withSenderKey(senderPublicKey)
+          .withCipherText("test".getBytes())
+          .withCipherTextNonce(new Nonce("test".getBytes()))
+          .withRecipientBoxes(new ArrayList<>())
+          .withRecipientNonce(new Nonce("test".getBytes()))
+          .withRecipientKeys(recipientPublicKeys)
+          .withPrivacyMode(privacyMetadata.getPrivacyMode())
+          .withAffectedContractTransactions(new HashMap<>())
+          .withExecHash(privacyMetadata.getExecHash())
+          .withMandatoryRecipients(privacyMetadata.getMandatoryRecipients())
+          .build();
+      }
+
+      @Override
+      public EncodedPayload encryptPayload(RawTransaction rawTransaction, List<PublicKey> recipientPublicKeys, PrivacyMetadata privacyMetadata) {
+        return null;
+      }
+
+      @Override
+      public Set<TxHash> findInvalidSecurityHashes(EncodedPayload encodedPayload, List<AffectedTransaction> affectedContractTransactions) {
+        return null;
+      }
+
+      @Override
+      public RawTransaction encryptRawPayload(byte[] message, PublicKey sender) {
+        return null;
+      }
+
+      @Override
+      public byte[] unencryptTransaction(EncodedPayload payload, PublicKey providedKey) {
+        return "test".getBytes();
+      }
+
+      @Override
+      public byte[] unencryptRawPayload(RawTransaction payload) {
+        return new byte[0];
+      }
+
+      @Override
+      public byte[] createNewRecipientBox(EncodedPayload payload, PublicKey recipientKey) {
+        return new byte[0];
+      }
+
+      @Override
+      public Status status() {
+        return null;
+      }
+    };
+    inbox = new Inbox() {
+      @Override
+      public MessageId put(byte[] encoded) {
+        return null;
+      }
+
+      @Override
+      public byte[] get(MessageId messageId) {
+        return "1234567".getBytes();
+      }
+
+      @Override
+      public Stream<MessageId> stream() {
+        return null;
+      }
+
+      @Override
+      public void delete(MessageId messageId) {
+
+      }
+    };
 
     MessagingImpl impl = new MessagingImpl(enclave,courier,inbox);
+    impl.read("test");
 
   }
 
