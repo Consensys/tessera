7d41b89d6d2ffbe62dda10647f352872011f3403
Sally MacFarlane <sally.macfarlane@consensys.net>
2022-04-04 15:18:54 +1000

    removed todos and created issue see #1428
    Signed-off-by: Sally MacFarlane <sally.macfarlane@consensys.net>

--- tessera-jaxrs/thirdparty-jaxrs/src/main/java/com/quorum/tessera/thirdparty/model/Key.java
+++ tessera-jaxrs/thirdparty-jaxrs/src/main/java/com/quorum/tessera/thirdparty/model/Key.java
@@ -2,7 +2,6 @@ package com.quorum.tessera.thirdparty.model;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-// TODO(cjh) just used for swagger generation - should be used in the actual jaxrs methods
 public class Key {
 
   @Schema(description = "public key", format = "base64")
