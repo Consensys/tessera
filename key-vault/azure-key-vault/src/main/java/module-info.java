a45837c782e3971f9d1338b1ff56c2ea63d0c0d3
chris <chrishounsom@icloud.com>
2022-06-06 16:22:48 +0100

    switch azure http client from netty to okhttp resolves repeated cve dependency advisories from netty components & latest versions have java module issues that cannot be easily resolved

--- key-vault/azure-key-vault/src/main/java/module-info.java
+++ key-vault/azure-key-vault/src/main/java/module-info.java
@@ -3,13 +3,17 @@ module tessera.keyvault.azure {
   requires org.slf4j;
   requires tessera.config;
   requires tessera.keyvault.api;
-  requires jakarta.annotation;
+  //    requires jakarta.annotation;
   requires com.azure.identity;
   requires com.azure.security.keyvault.secrets;
-  requires com.azure.http.netty;
-  requires reactor.netty;
-  requires io.netty.handler;
-  requires io.netty.common;
+  //  requires com.azure.http.okhttp;
+  //  requires com.azure.http.netty;
+  //  requires reactor.netty;
+  //  requires io.netty.handler;
+  //  requires io.netty.common;
+  //  requires okio;
+
+  requires kotlin.stdlib;
 
   provides com.quorum.tessera.key.vault.KeyVaultServiceFactory with
       com.quorum.tessera.key.vault.azure.AzureKeyVaultServiceFactory;
